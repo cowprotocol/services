bcdd5f968fd34270fcaa933e4d6ee387044c55b4
josojo <josojo@hotmail.de>
2020-09-24 10:31:50 +0200

    Nicks comments

--- src/models/orders.rs
+++ src/models/orders.rs
@@ -1,21 +1,13 @@
-extern crate anyhow;
-extern crate ethabi;
-extern crate ethsign;
-extern crate rustc_hex;
-extern crate serde_json;
-extern crate web3;
-
 use anyhow::Result;
-use ethabi::encode;
-use ethsign::Signature;
+use ethcontract::common::abi::encode;
+use ethcontract::web3::contract::tokens::Tokenizable;
+use ethcontract::web3::signing::keccak256;
+use ethcontract::web3::signing::recover;
+use ethcontract::web3::types::{Address, H160, H256, U256};
 use serde::{Deserialize, Serialize};
 use std::cmp::Ord;
 use std::cmp::Ordering;
 use std::cmp::PartialOrd;
-use std::ops::Mul;
-use web3::contract::tokens::Tokenizable;
-use web3::signing::keccak256;
-use web3::types::{Address, H160, H256, U256};
 
 #[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize, Default)]
 pub struct Order {
@@ -36,10 +28,10 @@ pub struct Order {
 impl Order {
     #[allow(dead_code)]
     pub fn get_digest(&self) -> Result<[u8; 32]> {
-        let domain_separator: H256 = serde_json::from_str(
-            r#""0x24a654ed47680d6a76f087ec92b3a0f0fe4c9c82c26bff3bb22dffe0f120c7f0""#,
-        )
-        .unwrap();
+        let domain_separator: H256 =
+            "24a654ed47680d6a76f087ec92b3a0f0fe4c9c82c26bff3bb22dffe0f120c7f0"
+                .parse()
+                .unwrap();
         return Ok(keccak256(&encode(&[
             domain_separator.into_token(),
             self.sell_amount.into_token(),
@@ -53,13 +45,9 @@ impl Order {
     #[allow(dead_code)]
     pub fn validate_order(&self) -> Result<bool> {
         let message = self.get_digest()?;
-        let signature = Signature {
-            v: self.signature_v - 27 as u8,
-            r: *self.signature_r.as_fixed_bytes(),
-            s: *self.signature_s.as_fixed_bytes(),
-        };
-        let owner = signature.recover(&message)?;
-        return Ok(H160::from(owner.address()).eq(&self.owner));
+        let signature = encode(&[self.signature_r.into_token(), self.signature_s.into_token()]);
+        let owner = recover(&message, &signature, (self.signature_v - 27) as i32)?;
+        return Ok(H160::from(owner).eq(&self.owner));
     }
     #[cfg(test)]
     pub fn new_valid_test_order() -> Self {
@@ -68,21 +56,17 @@ impl Order {
             buy_amount: U256::from_dec_str("900000000000000000").unwrap(),
             current_sell_amount: U256::from_dec_str("1000000000000000000").unwrap(),
             current_buy_amount: U256::from_dec_str("900000000000000000").unwrap(),
-            sell_token: serde_json::from_str(r#""0xA193E42526F1FEA8C99AF609dcEabf30C1c29fAA""#)
-                .unwrap(),
-            buy_token: serde_json::from_str(r#""0xFDFEF9D10d929cB3905C71400ce6be1990EA0F34""#)
-                .unwrap(),
-            owner: serde_json::from_str(r#""0x63FC2aD3d021a4D7e64323529a55a9442C444dA0""#).unwrap(),
+            sell_token: "A193E42526F1FEA8C99AF609dcEabf30C1c29fAA".parse().unwrap(),
+            buy_token: "FDFEF9D10d929cB3905C71400ce6be1990EA0F34".parse().unwrap(),
+            owner: "63FC2aD3d021a4D7e64323529a55a9442C444dA0".parse().unwrap(),
             nonce: 1,
             signature_v: 27 as u8,
-            signature_r: serde_json::from_str(
-                r#""0x07cf23fa6f588cc3a91de8444b589e5afbf91c5d486c512a353d45d02fa58700""#,
-            )
-            .unwrap(),
-            signature_s: serde_json::from_str(
-                r#""0x53671e75b62b5bd64f91c80430aafb002040c35d1fcf25d0dc55d978946d5c11""#,
-            )
-            .unwrap(),
+            signature_r: "07cf23fa6f588cc3a91de8444b589e5afbf91c5d486c512a353d45d02fa58700"
+                .parse()
+                .unwrap(),
+            signature_s: "53671e75b62b5bd64f91c80430aafb002040c35d1fcf25d0dc55d978946d5c11"
+                .parse()
+                .unwrap(),
             valid_until: U256::from("0"),
         }
     }
@@ -90,14 +74,14 @@ impl Order {
 
 impl Ord for Order {
     fn cmp(&self, other: &Self) -> Ordering {
-        (self.sell_amount.mul(other.buy_amount)).cmp(&self.buy_amount.mul(other.sell_amount))
+        (self.sell_amount.full_mul(other.buy_amount))
+            .cmp(&(self.buy_amount.full_mul(other.sell_amount)))
     }
 }
 
 impl PartialOrd for Order {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        (self.sell_amount.mul(other.buy_amount))
-            .partial_cmp(&self.buy_amount.mul(other.sell_amount))
+        Some(self.cmp(other))
     }
 }
 
