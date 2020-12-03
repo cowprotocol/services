73ac32b97a8bacfeee5543a7aacac81aa2e398b0
Felix Leupold <felixleupold90@gmail.com>
2020-12-03 11:03:25 +0100

    Adding Dockerfile (gnosis/gp-v2-services#68)
    As a first step to get continuous development running. I went for using the same container for both binaries (as it's less code to maintain, faster to build on CI and I don't expect the binary to be that big in size). Looking forward to your thoughts if this is a bad idea. This decision will also affect if I have to request 1 or multiple dockerhub images
    
    The .dockerignore/Dockerfile is mostly taken from https://github.com/gnosis/rust-template however due to having multiple workspaces the trick of compiling dependencies before copying the code doesn't work.
    
    ### Test Plan
    
    ```sh
    docker build --tag gnosispm:oba-services -f docker/Dockerfile .
    docker run gnosispm:oba-services solver # panics due to "not implemented"
    docker run -p 8080:8080 gnosispm:oba-services orderbook
    ``` 
    
    and then fetching http://localhost:8080/api/v1/orders in the browser.

--- orderbook/src/main.rs
+++ orderbook/src/main.rs
@@ -8,6 +8,6 @@ use std::sync::Arc;
 async fn main() {
     let orderbook = Arc::new(OrderBook::default());
     let filter = api::handle_all_routes(orderbook);
-    let result = warp::serve(filter).bind(([127, 0, 0, 1], 8080)).await;
+    let result = warp::serve(filter).bind(([0, 0, 0, 0], 8080)).await;
     println!("warp exited: {:?}", result);
 }
