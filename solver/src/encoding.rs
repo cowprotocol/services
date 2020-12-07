03c714020e6526dc654621b76d08d1a65c64b701
Valentin <vakevk+git@gmail.com>
2020-12-07 09:52:22 +0100

    Encode uniswap interaction call (gnosis/gp-v2-services#71)
    Does not yet have the target address of the contract because I didn't
    know how it is supposed to be encoded.
    
    ### Test Plan
    unit test for encoding.

--- solver/src/encoding.rs
+++ solver/src/encoding.rs
@@ -1,7 +1,11 @@
+use std::convert::TryInto;
+
+use contracts::IUniswapV2Router02;
 use model::{OrderCreation, OrderKind};
-use primitive_types::U256;
+use primitive_types::{H160, U256};
 
 const TRADE_STRIDE: usize = 204;
+const UNISWAP_INTERACTION_SIZE: usize = 260;
 
 /// Creates the data which the smart contract's `decodeTrade` expects.
 pub fn encode_trade(
@@ -35,6 +39,49 @@ fn encode_order_flags(order: &OrderCreation) -> u8 {
         result |= 0b00000010;
     }
     result
+}
+
+fn encode_uniswap_call(
+    amount_in: U256,
+    amount_out_min: U256,
+    token_in: H160,
+    token_out: H160,
+    payout_to: H160,
+) -> [u8; UNISWAP_INTERACTION_SIZE] {
+    let uniswap = IUniswapV2Router02::at(&dummy::dummy_web3(), H160::zero());
+    let method = uniswap.swap_exact_tokens_for_tokens(
+        amount_in,
+        amount_out_min,
+        vec![token_in, token_out],
+        payout_to,
+        U256::MAX,
+    );
+    let data = method.tx.data.expect("call doesn't have calldata").0;
+    data.try_into().expect("unexpected data size")
+}
+
+// To create an ethcontract instance we need to provide a web3 even though we never use it. This
+// module provides a dummy transport and web3.
+mod dummy {
+    use jsonrpc_core::Call as RpcCall;
+    use serde_json::Value;
+    use web3::{api::Web3, Transport};
+
+    #[derive(Clone, Debug)]
+    pub struct DummyTransport;
+    impl Transport for DummyTransport {
+        type Out = futures::future::Pending<web3::Result<Value>>;
+        fn prepare(&self, _method: &str, _params: Vec<Value>) -> (web3::RequestId, RpcCall) {
+            unimplemented!()
+        }
+        fn send(&self, _id: web3::RequestId, _request: RpcCall) -> Self::Out {
+            unimplemented!()
+        }
+    }
+
+    pub fn dummy_web3() -> Web3<DummyTransport> {
+        Web3::new(DummyTransport)
+    }
 }
 
 #[cfg(test)]
@@ -80,5 +127,34 @@ mod tests {
         assert_eq!(encoded[139], 9);
         assert_eq!(encoded[140..172], u8_as_32_bytes_be(10));
         assert_eq!(encoded[172..204], u8_as_32_bytes_be(11));
+    }
+
+    #[test]
+    fn encode_uniswap_call_() {
+        let amount_in = 5;
+        let amount_out_min = 6;
+        let token_in = 7;
+        let token_out = 8;
+        let payout_to = 9;
+        let encoded = encode_uniswap_call(
+            U256::from(amount_in),
+            U256::from(amount_out_min),
+            H160::from_low_u64_be(token_in as u64),
+            H160::from_low_u64_be(token_out as u64),
+            H160::from_low_u64_be(payout_to as u64),
+        );
+        let signature = [0x38u8, 0xed, 0x17, 0x39];
+        let path_offset = 160;
+        let path_size = 2;
+        let deadline = [0xffu8; 32];
+        assert_eq!(encoded[0..4], signature);
+        assert_eq!(encoded[4..36], u8_as_32_bytes_be(amount_in));
+        assert_eq!(encoded[36..68], u8_as_32_bytes_be(amount_out_min));
+        assert_eq!(encoded[68..100], u8_as_32_bytes_be(path_offset));
+        assert_eq!(encoded[100..132], u8_as_32_bytes_be(payout_to));
+        assert_eq!(encoded[132..164], deadline);
+        assert_eq!(encoded[164..196], u8_as_32_bytes_be(path_size));
+        assert_eq!(encoded[196..228], u8_as_32_bytes_be(token_in));
+        assert_eq!(encoded[228..260], u8_as_32_bytes_be(token_out));
     }
 }
