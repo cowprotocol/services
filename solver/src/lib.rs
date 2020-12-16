e9c95474f92bc072cbe68328e5d367ed9e1199c7
Felix Leupold <felixleupold90@gmail.com>
2020-12-16 18:22:54 +0100

    Pass in Private Key for settlement (gnosis/gp-v2-services#144)
    This PR makes it so that we can specify a specific account when interacting with the settlement contract (requiring for any non local demo). We will have to make sure that any account used here is whitelisted on the smart contract level.
    
    ### Test Plan
    See that without specifying a private key we can no longer start the solver.
    
    cargo run --bin solver
    
    After exporting `PRIVATE_KEY` it works.

--- solver/src/lib.rs
+++ solver/src/lib.rs
@@ -4,3 +4,20 @@ pub mod interactions;
 pub mod naive_solver;
 pub mod orderbook;
 pub mod settlement;
+
+use anyhow::Result;
+use ethcontract::{contract::MethodDefaults, Account, Http, PrivateKey, Web3};
+
+pub async fn get_settlement_contract(
+    web3: &Web3<Http>,
+    chain_id: u64,
+    key: PrivateKey,
+) -> Result<contracts::GPv2Settlement> {
+    let mut settlement_contract = contracts::GPv2Settlement::deployed(&web3).await?;
+    *settlement_contract.defaults_mut() = MethodDefaults {
+        from: Some(Account::Offline(key, Some(chain_id))),
+        gas: None,
+        gas_price: None,
+    };
+    Ok(settlement_contract)
+}
--- solver/src/main.rs
+++ solver/src/main.rs
@@ -1,16 +1,18 @@
+use ethcontract::PrivateKey;
 use reqwest::Url;
 use solver::driver::Driver;
 use std::time::Duration;
 use structopt::StructOpt;
-
 #[derive(Debug, StructOpt)]
 struct Arguments {
     #[structopt(flatten)]
     shared: shared_arguments::Arguments,
 
+    /// The API endpoint to fetch the orderbook
     #[structopt(long, env = "ORDERBOOK_URL", default_value = "http://localhost:8080")]
     orderbook_url: Url,
 
+    /// The timeout for the API endpoint to fetch the orderbook
     #[structopt(
         long,
         env = "ORDERBOOK_TIMEOUT",
@@ -18,6 +20,10 @@ struct Arguments {
         parse(try_from_str = shared_arguments::duration_from_seconds),
     )]
     orderbook_timeout: Duration,
+
+    /// The private key used by the driver to sign transactions.
+    #[structopt(short = "k", long, env = "PRIVATE_KEY", hide_env_values = true)]
+    private_key: PrivateKey,
 }
 
 #[tokio::main]
@@ -29,12 +35,18 @@ async fn main() {
     let transport = web3::transports::Http::new(args.shared.node_url.as_str())
         .expect("transport creation failed");
     let web3 = web3::Web3::new(transport);
-    let settlement_contract = contracts::GPv2Settlement::deployed(&web3)
-        .await
-        .expect("couldn't load deployed settlement");
     let uniswap_contract = contracts::UniswapV2Router02::deployed(&web3)
         .await
         .expect("couldn't load deployed uniswap router");
+    let chain_id = web3
+        .eth()
+        .chain_id()
+        .await
+        .expect("Could not get chainId")
+        .as_u64();
+    let settlement_contract = solver::get_settlement_contract(&web3, chain_id, args.private_key)
+        .await
+        .expect("couldn't load deployed settlement");
     let orderbook =
         solver::orderbook::OrderBookApi::new(args.orderbook_url, args.orderbook_timeout);
     let mut driver = Driver::new(settlement_contract, uniswap_contract, orderbook);
