2cc0ba49d924eebc15e6944b16149b9d4f60794e
Felix Leupold <felixleupold90@gmail.com>
2020-12-22 09:30:01 +0100

    [Refactor] Move order related models in separate module

--- solver/src/encoding.rs
+++ solver/src/encoding.rs
@@ -1,6 +1,6 @@
 use std::convert::TryInto;
 
-use model::{OrderCreation, OrderKind};
+use model::order::{OrderCreation, OrderKind};
 use primitive_types::U256;
 
 pub const TRADE_STRIDE: usize = 206;
