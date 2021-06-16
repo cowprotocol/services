10a79674f6932bba9e1a7187719b59d747f057b3
Valentin <77051586+vkgnosis@users.noreply.github.com>
2021-06-16 11:26:02 +0200

    Test gas price stream (gnosis/gp-v2-services#729)
    With tokio::time it is possible to manipulate the current time and
    sleeps will automatically complete while advancing time. This way we can
    test the stream just by switching to the tokio time type.

--- solver/src/settlement_submission/gas_price_stream.rs
+++ solver/src/settlement_submission/gas_price_stream.rs
@@ -1,12 +1,11 @@
 use super::GAS_PRICE_REFRESH_INTERVAL;
 use futures::{stream, Stream, StreamExt};
 use gas_estimation::GasPriceEstimating;
-use std::time::Instant;
 
 // Create a never ending stream of gas prices based on checking the estimator in fixed intervals
 // and enforcing the minimum increase. Errors are ignored.
 pub fn gas_price_stream(
-    target_confirm_time: Instant,
+    target_confirm_time: std::time::Instant,
     gas_price_cap: f64,
     gas_limit: f64,
     estimator: &dyn GasPriceEstimating,
@@ -20,7 +19,8 @@ pub fn gas_price_stream(
         } else {
             tokio::time::sleep(GAS_PRICE_REFRESH_INTERVAL).await;
         }
-        let remaining_time = target_confirm_time.saturating_duration_since(Instant::now());
+        let remaining_time = tokio::time::Instant::from_std(target_confirm_time)
+            .saturating_duration_since(tokio::time::Instant::now());
         let estimate = estimator
             .estimate_with_limits(gas_limit, remaining_time)
             .await;
@@ -39,4 +39,43 @@ pub fn gas_price_stream(
         }
     });
     transaction_retry::gas_price_increase::enforce_minimum_increase_and_cap(gas_price_cap, stream)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::stream::StreamExt;
+    use std::time::Duration;
+    use tokio::time;
+
+    struct TestEstimator;
+
+    #[async_trait::async_trait]
+    impl GasPriceEstimating for TestEstimator {
+        async fn estimate_with_limits(&self, _: f64, time_limit: Duration) -> anyhow::Result<f64> {
+            Ok(20. - time_limit.as_secs_f64())
+        }
+    }
+
+    #[tokio::test]
+    async fn stream_uses_current_time() {
+        time::pause();
+
+        let estimator = TestEstimator;
+        let stream = gas_price_stream(
+            (tokio::time::Instant::now() + Duration::from_secs(20)).into_std(),
+            f64::INFINITY,
+            0.,
+            &estimator,
+            None,
+        );
+        futures::pin_mut!(stream);
+
+        let next = stream.next().await.unwrap();
+        assert_eq!(next as u32, 0);
+        let next = stream.next().await.unwrap();
+        assert_eq!(next as u32, 15);
+        let next = stream.next().await.unwrap();
+        assert_eq!(next as u32, 20);
+    }
 }
