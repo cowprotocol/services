745eecc5cd2825649a9cc0d1f05ee7527d74c385
Tamika Nomara <tamika.nomara@gnosis.pm>
2021-08-27 00:52:05 +0300

    Log gas estimation errors (gnosis/gp-v2-services#1023)
    Followup for #1022: log any error that occurs during gas estimation.

--- solver/src/driver.rs
+++ solver/src/driver.rs
@@ -339,6 +339,10 @@ impl Driver {
                     solver.account().clone(),
                 )
                 .await
+                .map_err(|err| {
+                    tracing::error!("Failed to estimate gas for solver {}: {:?}", solver.name(), err);
+                    err
+                })
                 .ok()?;
                 let rated_settlement = RatedSettlement {
                     settlement,
