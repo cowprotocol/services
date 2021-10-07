c2b8dec5d643940e35d15b434a25ba9d3c838efc
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2021-10-07 18:30:41 +0200

    Allow missing `fullFeeAmount` field (gnosis/gp-v2-services#1218)
    This PR makes the `fullFeeAmount` field of order metadata default to 0 if missing. This allows `main` to stay compatible with the current production orderbook API.
    
    This can be removed after the next release.
    
    ### Test Plan
    
    CI. Trivial change.

--- model/src/order.rs
+++ model/src/order.rs
@@ -408,7 +408,7 @@ pub struct OrderMetaData {
     pub status: OrderStatus,
     #[serde(with = "h160_hexadecimal")]
     pub settlement_contract: H160,
-    #[serde(with = "u256_decimal")]
+    #[serde(default, with = "u256_decimal")]
     pub full_fee_amount: U256,
 }
 
