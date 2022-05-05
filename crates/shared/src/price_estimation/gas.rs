c555e550b23af044b80a08d1d0a2fca4d6d094e1
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-05-05 09:45:35 +0200

    Use median for 0x

--- crates/shared/src/price_estimation/gas.rs
+++ crates/shared/src/price_estimation/gas.rs
@@ -46,9 +46,9 @@ pub static GAS_PER_ORDER: u64 = 66_315;
 // estimated with https://dune.com/queries/640717
 pub static GAS_PER_UNISWAP: u64 = 90_171;
 
-/// Average gas used per ZeroExInteraction.
+/// Median gas used per ZeroExInteraction.
 // estimated with https://dune.com/queries/639669
-pub static GAS_PER_ZEROEX_ORDER: u64 = 73_901;
+pub static GAS_PER_ZEROEX_ORDER: u64 = 66_358;
 
 /// Average gas used per BalancerSwapGivenOutInteraction.
 // estimated with https://dune.com/queries/639857
