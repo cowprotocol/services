58046f8d1c7065fe2def89926b4a6bcc8383c827
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-05-05 09:18:17 +0200

    Address comments about DB migration script

--- database/sql/V020__extend_orders_with_is_liquidity_order.sql
+++ database/sql/V020__extend_orders_with_is_liquidity_order.sql
@@ -1,5 +1,11 @@
 ALTER TABLE orders ADD is_liquidity_order boolean NOT NULL DEFAULT false;
--- So far every liquidity order was also a partially fillable order and all
--- partially fillable orders were liquidity orders.
--- We use this fact to initialize is_liquidity_order for existing orders correctly.
-UPDATE orders SET is_liquidity_order = partially_fillable WHERE is_liquidity_order = false;
+-- We know that every partially fillable order was also a liquidity order so we
+-- initialize the new column accordingly.
+-- There have been liquidity orders which were not partially fillable but there is
+-- no way to initialize the `is_liquidity_order` fields correctly for those.
+-- This is just our best effort to patch up existing orders.
+UPDATE orders SET is_liquidity_order = partially_fillable;
+-- We only wanted to have a default to make the migration easier. Ultimately
+-- we want every new insertion to supply the flag.
+ALTER TABLE orders ALTER COLUMN is_liquidity_order DROP DEFAULT;
+
