925067973f56a643fbedd7ef102b21af225a9cff
Nicholas Rodrigues Lordello <nick@cow.fi>
2022-05-16 17:27:30 +0200

    Add HTTP headers for external solver requests (#201)
    This PR just adds HTTP headers indicating that external solver `/solve` requests both send and only accept JSON.
    
    This is just to be more polite to solvers as some frameworks have friction if the header is missing or not set to `application/json`.
    
    ### Test Plan
    
    CI.

--- crates/shared/src/http_solver.rs
+++ crates/shared/src/http_solver.rs
@@ -1,5 +1,5 @@
 use anyhow::{anyhow, ensure, Context, Result};
-use reqwest::header::HeaderValue;
+use reqwest::header::{self, HeaderValue};
 use reqwest::{Client, Url};
 use std::time::Duration;
 
@@ -110,7 +110,12 @@ impl HttpSolverApi for DefaultHttpSolverApi {
                 .append_pair("auction_id", auction_id.to_string().as_str());
         }
         let query = url.query().map(ToString::to_string).unwrap_or_default();
-        let mut request = self.client.post(url).timeout(timeout);
+        let mut request = self
+            .client
+            .post(url)
+            .timeout(timeout)
+            .header(header::CONTENT_TYPE, "application/json")
+            .header(header::ACCEPT, "application/json");
         if let Some(api_key) = &self.config.api_key {
             let mut header = HeaderValue::from_str(api_key.as_str()).unwrap();
             header.set_sensitive(true);
