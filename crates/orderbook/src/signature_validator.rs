b31523ab4f4674e39cb3e251fb02c0dd2895bee8
Lachezar Lechev <lachezar@ambire.com>
2022-05-30 16:27:44 +0200

    orderbook - SignatureValidator - use anyhow error

--- crates/orderbook/src/signature_validator.rs
+++ crates/orderbook/src/signature_validator.rs
@@ -1,26 +1,18 @@
-use anyhow::Result;
-use ethcontract::{batch::CallBatch, Bytes};
+use anyhow::{Result, Context};
+use ethcontract::Bytes;
 use primitive_types::H160;
 use shared::Web3;
-
-#[derive(Debug)]
-pub enum IsValidSignatureError {
-    ContractCallFailed,
-}
 
 #[cfg_attr(test, mockall::automock)]
 #[async_trait::async_trait]
 /// <https://eips.ethereum.org/EIPS/eip-1271>
 pub trait SignatureValidator: Send + Sync {
-    /// The Magical value as defined by EIP-1271
-    const MAGICAL_VALUE: [u8; 4] = 0x1626ba7e_u32.to_le_bytes();
-
     async fn is_valid_signature(
         &self,
         contract_address: H160,
         hash: [u8; 32],
         signature: &[u8],
-    ) -> Result<bool, IsValidSignatureError>;
+    ) -> Result<bool>;
 }
 
 pub struct Web3SignatureValidator {
@@ -28,6 +20,9 @@ pub struct Web3SignatureValidator {
 }
 
 impl Web3SignatureValidator {
+    /// The Magical value as defined by EIP-1271
+    pub const MAGICAL_VALUE: [u8; 4] = 0x1626ba7e_u32.to_le_bytes();
+
     pub fn new(web3: Web3) -> Self {
         Self { web3 }
     }
@@ -40,14 +35,14 @@ impl SignatureValidator for Web3SignatureValidator {
         contract_address: H160,
         hash: [u8; 32],
         signature: &[u8],
-    ) -> Result<bool, IsValidSignatureError> {
+    ) -> Result<bool> {
         let instance = contracts::SignatureValidator::at(&self.web3, contract_address);
 
         let is_valid_signature = instance
             .is_valid_signature(Bytes(hash), Bytes(signature.to_vec()))
             .call()
             .await
-            .map_err(|_method_err| IsValidSignatureError::ContractCallFailed)?;
+            .context("isValidSignature")?;
 
         Ok(is_valid_signature.0 == Self::MAGICAL_VALUE)
     }
