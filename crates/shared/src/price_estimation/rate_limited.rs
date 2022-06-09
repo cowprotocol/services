4fbd7c24fa79b122880c22ad9741efd8e5d4fe41
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-06-09 15:52:41 +0200

    Add tests for RateLimitedPriceEstimator

--- crates/shared/src/price_estimation/rate_limited.rs
+++ crates/shared/src/price_estimation/rate_limited.rs
@@ -79,3 +79,81 @@ impl PriceEstimating for RateLimitedPriceEstimator {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::price_estimation::{Estimate, MockPriceEstimating, PriceEstimateResult};
+    use futures::{FutureExt, StreamExt};
+    use primitive_types::H160;
+    use tokio::time::{sleep, Duration};
+
+    #[tokio::test]
+    async fn backs_off_after_slow_response() {
+        const MAX_BACK_OFF: Duration = Duration::from_millis(200);
+        const HEALTHY_RESPONSE_TIME: Duration = Duration::from_millis(10);
+
+        fn estimate(amount: u64) -> Estimate {
+            Estimate {
+                out_amount: amount.into(),
+                ..Default::default()
+            }
+        }
+
+        fn is_rate_limit_error(result: &PriceEstimateResult) -> bool {
+            matches!(result, Err(e) if e.to_string().contains("backing off rate limit"))
+        }
+
+        fn query(sell_token: u8) -> Query {
+            Query {
+                sell_token: H160([sell_token; 20]),
+                ..Default::default()
+            }
+        }
+
+        let mut inner = MockPriceEstimating::new();
+        inner.expect_estimates().returning(move |queries| {
+            // RateLimitedPriceEstimator queries 1 price at a time
+            assert_eq!(queries.len(), 1);
+            let return_after_unhealthy_time = queries[0] == query(1);
+            futures::stream::once(async move {
+                if return_after_unhealthy_time {
+                    sleep(HEALTHY_RESPONSE_TIME * 2).await;
+                    (0, Ok(estimate(0)))
+                } else {
+                    (0, Ok(estimate(1)))
+                }
+            })
+            .boxed()
+        });
+
+        let estimator = inner.rate_limited(
+            RateLimitingStrategy::try_new(2.0, MAX_BACK_OFF, MAX_BACK_OFF, "rate_limited".into())
+                .unwrap(),
+            HEALTHY_RESPONSE_TIME,
+        );
+
+        let queries = &[query(1), query(2)];
+
+        // second query will return rate limited error
+        let results: Vec<_> = estimator.estimates(queries).collect().await;
+        assert_eq!(2, results.len());
+        assert!(results[0].1.is_ok());
+        // rate limits can start happening within one stream of queries
+        assert!(is_rate_limit_error(&results[1].1));
+
+        let results: Vec<_> = estimator
+            .estimates(queries)
+            .collect()
+            .now_or_never()
+            .expect("requests should return errors immediately while being rate limited");
+        assert_eq!(2, results.len());
+        assert!(is_rate_limit_error(&results[0].1));
+        assert!(is_rate_limit_error(&results[1].1));
+
+        sleep(MAX_BACK_OFF).await;
+        let results: Vec<_> = estimator.estimates(queries).collect().await;
+        assert_eq!(2, results.len());
+        assert!(results[0].1.is_ok());
+        assert!(is_rate_limit_error(&results[1].1));
+    }
+}
