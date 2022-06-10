5c2476ad274cc3aca7e32ee94b1eb970c26eed8c
Anuj Bansal <highlightanuj@gmail.com>
2022-06-10 19:54:40 +0530

    update test to consider fillable amount

--- crates/solver/src/liquidity/zeroex.rs
+++ crates/solver/src/liquidity/zeroex.rs
@@ -50,8 +50,8 @@ impl ZeroExLiquidity {
             .filter_map(|order| TokenPair::new(order.buy_token, order.sell_token));
         let relevant_pairs = self.base_tokens.relevant_pairs(user_order_pairs);
 
-        let order_buckets = ZeroExLiquidity::generate_order_buckets(zeroex_orders, relevant_pairs);
-        let filtered_zeroex_orders = ZeroExLiquidity::get_useful_orders(order_buckets, 5);
+        let order_buckets = generate_order_buckets(zeroex_orders, relevant_pairs);
+        let filtered_zeroex_orders = get_useful_orders(order_buckets, 5);
 
         let zeroex_liquidity_orders: Vec<_> = filtered_zeroex_orders
             .into_iter()
@@ -59,51 +59,6 @@ impl ZeroExLiquidity {
             .collect();
 
         Ok(zeroex_liquidity_orders)
-    }
-
-    fn generate_order_buckets(
-        zeroex_orders: impl Iterator<Item = OrderRecord>,
-        relevant_pairs: HashSet<TokenPair>,
-    ) -> OrderBuckets {
-        // divide orders in buckets
-        let mut buckets = OrderBuckets::default();
-        zeroex_orders
-            .filter(|record| {
-                match TokenPair::new(record.order.taker_token, record.order.maker_token) {
-                    Some(pair) => relevant_pairs.contains(&pair),
-                    None => false,
-                }
-            })
-            .for_each(|order| {
-                let bucket = buckets
-                    .entry((order.order.taker_token, order.order.maker_token))
-                    .or_default();
-                bucket.push(order);
-            });
-        buckets
-    }
-
-    /// Get the `orders_per_type` best priced and biggest volume orders.
-    fn get_useful_orders(order_buckets: OrderBuckets, orders_per_type: usize) -> Vec<OrderRecord> {
-        let mut filtered_zeroex_orders = vec![];
-        for mut orders in order_buckets.into_values() {
-            if orders.len() <= 2 * orders_per_type {
-                filtered_zeroex_orders.extend(orders);
-                continue;
-            }
-            // Sorting to have best priced orders at the end of the vector
-            // best priced orders are those that have the maximum maker_amount / taker_amount ratio
-            orders.sort_by(|order_1, order_2| {
-                let price_1 = order_1.order.maker_amount as f64 / order_1.order.taker_amount as f64;
-                let price_2 = order_2.order.maker_amount as f64 / order_2.order.taker_amount as f64;
-                price_1.partial_cmp(&price_2).unwrap()
-            });
-            filtered_zeroex_orders.extend(orders.drain(orders.len() - orders_per_type..));
-
-            orders.sort_by_key(|order| order.metadata.remaining_fillable_taker_amount);
-            filtered_zeroex_orders.extend(orders.into_iter().rev().take(orders_per_type));
-        }
-        filtered_zeroex_orders
     }
 
     /// Turns 0x OrderRecord into liquidity which solvers can use.
@@ -133,6 +88,51 @@ impl ZeroExLiquidity {
         };
         Some(Liquidity::LimitOrder(limit_order))
     }
+}
+
+fn generate_order_buckets(
+    zeroex_orders: impl Iterator<Item = OrderRecord>,
+    relevant_pairs: HashSet<TokenPair>,
+) -> OrderBuckets {
+    // divide orders in buckets
+    let mut buckets = OrderBuckets::default();
+    zeroex_orders
+        .filter(
+            |record| match TokenPair::new(record.order.taker_token, record.order.maker_token) {
+                Some(pair) => relevant_pairs.contains(&pair),
+                None => false,
+            },
+        )
+        .for_each(|order| {
+            let bucket = buckets
+                .entry((order.order.taker_token, order.order.maker_token))
+                .or_default();
+            bucket.push(order);
+        });
+    buckets
+}
+
+/// Get the `orders_per_type` best priced and biggest volume orders.
+fn get_useful_orders(order_buckets: OrderBuckets, orders_per_type: usize) -> Vec<OrderRecord> {
+    let mut filtered_zeroex_orders = vec![];
+    for mut orders in order_buckets.into_values() {
+        if orders.len() <= 2 * orders_per_type {
+            filtered_zeroex_orders.extend(orders);
+            continue;
+        }
+        // Sorting to have best priced orders at the end of the vector
+        // best priced orders are those that have the maximum maker_amount / taker_amount ratio
+        orders.sort_by(|order_1, order_2| {
+            let price_1 = order_1.order.maker_amount as f64 / order_1.order.taker_amount as f64;
+            let price_2 = order_2.order.maker_amount as f64 / order_2.order.taker_amount as f64;
+            price_1.partial_cmp(&price_2).unwrap()
+        });
+        filtered_zeroex_orders.extend(orders.drain(orders.len() - orders_per_type..));
+
+        orders.sort_by_key(|order| order.metadata.remaining_fillable_taker_amount);
+        filtered_zeroex_orders.extend(orders.into_iter().rev().take(orders_per_type));
+    }
+    filtered_zeroex_orders
 }
 
 struct OrderSettlementHandler {
@@ -183,10 +183,8 @@ pub mod tests {
         let order_1 = order_with_tokens(token_a, token_b);
         let order_2 = order_with_tokens(token_b, token_a);
         let order_3 = order_with_tokens(token_b, token_a);
-        let order_buckets = ZeroExLiquidity::generate_order_buckets(
-            [order_1, order_2, order_3].into_iter(),
-            relevant_pairs,
-        );
+        let order_buckets =
+            generate_order_buckets([order_1, order_2, order_3].into_iter(), relevant_pairs);
         assert_eq!(order_buckets.keys().len(), 2);
         assert_eq!(order_buckets[&(token_a, token_b)].len(), 1);
         assert_eq!(order_buckets[&(token_b, token_a)].len(), 2);
@@ -211,11 +209,9 @@ pub mod tests {
         let order_1 = order_with_tokens(token_ignore, token_b);
         let order_2 = order_with_tokens(token_a, token_ignore);
         let order_3 = order_with_tokens(token_ignore, token_ignore);
-        let order_buckets = ZeroExLiquidity::generate_order_buckets(
-            [order_1, order_2, order_3].into_iter(),
-            relevant_pairs,
-        );
-        let filtered_zeroex_orders = ZeroExLiquidity::get_useful_orders(order_buckets, 1);
+        let order_buckets =
+            generate_order_buckets([order_1, order_2, order_3].into_iter(), relevant_pairs);
+        let filtered_zeroex_orders = get_useful_orders(order_buckets, 1);
         assert_eq!(filtered_zeroex_orders.len(), 0);
     }
 
@@ -240,11 +236,9 @@ pub mod tests {
         let order_1 = order_with_fillable_amount(1_000);
         let order_2 = order_with_fillable_amount(100);
         let order_3 = order_with_fillable_amount(10_000);
-        let order_buckets = ZeroExLiquidity::generate_order_buckets(
-            [order_1, order_2, order_3].into_iter(),
-            relevant_pairs,
-        );
-        let filtered_zeroex_orders = ZeroExLiquidity::get_useful_orders(order_buckets, 1);
+        let order_buckets =
+            generate_order_buckets([order_1, order_2, order_3].into_iter(), relevant_pairs);
+        let filtered_zeroex_orders = get_useful_orders(order_buckets, 1);
         assert_eq!(filtered_zeroex_orders.len(), 2);
         assert_eq!(
             filtered_zeroex_orders[0]
@@ -265,8 +259,7 @@ pub mod tests {
         let token_a = H160([0x00; 20]);
         let token_b = H160([0xff; 20]);
         let relevant_pairs = get_relevant_pairs(token_a, token_b);
-        let metadata = OrderMetadata::default();
-        let order_with_taker_amount = |taker_amount| OrderRecord {
+        let order_with_amount = |taker_amount, remaining_fillable_taker_amount| OrderRecord {
             order: Order {
                 taker_token: token_a,
                 maker_token: token_b,
@@ -274,18 +267,21 @@ pub mod tests {
                 maker_amount: 100_000_000,
                 ..Default::default()
             },
-            metadata: metadata.clone(),
+            metadata: OrderMetadata {
+                remaining_fillable_taker_amount,
+                ..Default::default()
+            },
         };
-        let order_1 = order_with_taker_amount(10_000_000);
-        let order_2 = order_with_taker_amount(1_000);
-        let order_3 = order_with_taker_amount(100_000);
-        let order_buckets = ZeroExLiquidity::generate_order_buckets(
-            [order_1, order_2, order_3].into_iter(),
-            relevant_pairs,
-        );
-        let filtered_zeroex_orders = ZeroExLiquidity::get_useful_orders(order_buckets, 1);
+        let order_1 = order_with_amount(10_000_000, 1_000_000);
+        let order_2 = order_with_amount(1_000, 100);
+        let order_3 = order_with_amount(100_000, 1_000);
+        let order_buckets =
+            generate_order_buckets([order_1, order_2, order_3].into_iter(), relevant_pairs);
+        let filtered_zeroex_orders = get_useful_orders(order_buckets, 1);
         assert_eq!(filtered_zeroex_orders.len(), 2);
+        // First item in the list will be on the basis of maker_amount/taker_amount ratio
         assert_eq!(filtered_zeroex_orders[0].order.taker_amount, 1_000);
-        assert_eq!(filtered_zeroex_orders[1].order.taker_amount, 100_000);
+        // Second item in the list will be on the basis of remaining_fillable_taker_amount
+        assert_eq!(filtered_zeroex_orders[1].order.taker_amount, 10_000_000);
     }
 }
