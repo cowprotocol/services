aab48d4a4e1fa1bc79bd19e850ad4aabe3b0ba8a
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-06-29 15:27:07 +0200

    Setup basic server binary for driver (#323)

---
+++ crates/driver/src/api/execute.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use shared::api::{convert_json_response, error, extract_payload, ApiReply, IntoWarpReply};
+use std::convert::Infallible;
+use warp::{hyper::StatusCode, reply::with_status, Filter, Rejection};
+
+fn post_execute_request() -> impl Filter<Extract = (String,), Error = Rejection> + Clone {
+    warp::path!("execute")
+        .and(warp::post())
+        .and(extract_payload())
+}
+
+pub fn post_execute() -> impl Filter<Extract = (ApiReply,), Error = Rejection> + Clone {
+    post_execute_request().and_then(move |request| async move {
+        let result: Result<(), _> = Err(ExecuteError::NotImplemented);
+        if let Err(err) = &result {
+            tracing::warn!(?err, ?request, "post_execute error");
+        }
+        Result::<_, Infallible>::Ok(convert_json_response(result))
+    })
+}
+
+#[derive(thiserror::Error, Debug)]
+enum ExecuteError {
+    #[error("not implemented")]
+    NotImplemented,
+}
+
+impl IntoWarpReply for ExecuteError {
+    fn into_warp_reply(self) -> ApiReply {
+        match self {
+            Self::NotImplemented => with_status(
+                error("Route not yet implemented", "try again later"),
+                StatusCode::INTERNAL_SERVER_ERROR,
+            ),
+        }
+    }
+}
---
+++ crates/driver/src/main.rs
@@ -0,0 +1,55 @@
+use clap::Parser;
+use driver::api::serve_api;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    let args = driver::arguments::Arguments::parse();
+    shared::tracing::initialize(args.log_filter.as_str(), args.log_stderr_threshold);
+    tracing::info!("running driver with validated arguments:\n{}", args);
+
+    let (shutdown_sender, shutdown_receiver) = tokio::sync::oneshot::channel();
+    let serve_api = serve_api(args.bind_address, async {
+        let _ = shutdown_receiver.await;
+    });
+
+    futures::pin_mut!(serve_api);
+    tokio::select! {
+        result = &mut serve_api => tracing::error!(?result, "API task exited"),
+        _ = shutdown_signal() => {
+            tracing::info!("Gracefully shutting down API");
+            shutdown_sender.send(()).expect("failed to send shutdown signal");
+            match tokio::time::timeout(Duration::from_secs(10), serve_api).await {
+                Ok(inner) => inner.expect("API failed during shutdown"),
+                Err(_) => tracing::error!("API shutdown exceeded timeout"),
+            }
+        }
+    };
+}
+
+#[cfg(unix)]
+async fn shutdown_signal() {
+    // Intercept main signals for graceful shutdown
+    // Kubernetes sends sigterm, whereas locally sigint (ctrl-c) is most common
+    let sigterm = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .unwrap()
+            .recv()
+            .await
+    };
+    let sigint = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::interrupt())
+            .unwrap()
+            .recv()
+            .await;
+    };
+    futures::pin_mut!(sigint);
+    futures::pin_mut!(sigterm);
+    futures::future::select(sigterm, sigint).await;
+}
+
+#[cfg(windows)]
+async fn shutdown_signal() {
+    // We don't support signal handling on windows
+    std::future::pending().await
+}
