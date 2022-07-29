7687a8a6f1a205668f0b5ef9f4f39d72c221172b
sunce86 <dusan.stanivukovic@gmail.com>
2022-07-29 13:36:31 +0200

    fix univ3 unit test

--- crates/solver/src/interactions/uniswap_v3.rs
+++ crates/solver/src/interactions/uniswap_v3.rs
@@ -40,28 +40,34 @@ impl Interaction for UniswapV3Interaction {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use contracts::{GPv2Settlement, UniswapV3SwapRouter};
-    use shared::{dummy_contract, transport::dummy};
+    use contracts::UniswapV3SwapRouter;
+    use hex_literal::hex;
+    use shared::dummy_contract;
+
+    fn u8_as_32_bytes_be(u: u8) -> [u8; 32] {
+        let mut result = [0u8; 32];
+        result[31] = u;
+        result
+    }
 
     #[test]
     fn encode_uniswap_call() {
         let amount_out = 5;
         let amount_in_max = 6;
-        let token_in = H160::from_low_u64_be(7);
-        let token_out = H160::from_low_u64_be(8);
-        let fee = 3000;
-        let payout_to = 9u8;
+        let token_in = 7;
+        let token_out = 8;
+        let fee = 10000;
+        let payout_to = 9;
+        let deadline = U256::MAX;
         let router = dummy_contract!(UniswapV3SwapRouter, H160::from_low_u64_be(4));
-        let settlement =
-            GPv2Settlement::at(&dummy::web3(), H160::from_low_u64_be(payout_to as u64));
         let interaction = UniswapV3Interaction {
-            router,
+            router: router.clone(),
             params: ExactOutputSingleParams {
-                token_in,
-                token_out,
+                token_in: H160::from_low_u64_be(token_in as u64),
+                token_out: H160::from_low_u64_be(token_out as u64),
                 fee,
-                recipient: settlement.address(),
-                deadline: U256::MAX,
+                recipient: H160::from_low_u64_be(payout_to as u64),
+                deadline,
                 amount_out: amount_out.into(),
                 amount_in_max: amount_in_max.into(),
                 sqrt_price_limit_x96: U256::zero(),
@@ -73,21 +79,19 @@ mod tests {
         assert_eq!(interactions.len(), 1);
 
         // Verify Swap
-        // let swap_call = &interactions[0];
-        // assert_eq!(swap_call.0, router.address());
-        // let call = &swap_call.2 .0;
-        // let swap_signature = hex!("8803dbee");
-        // let path_offset = 160;
-        // let path_size = 2;
-        // let deadline = [0xffu8; 32];
-        // assert_eq!(call[0..4], swap_signature);
-        // assert_eq!(call[4..36], u8_as_32_bytes_be(amount_out));
-        // assert_eq!(call[36..68], u8_as_32_bytes_be(amount_in_max));
-        // assert_eq!(call[68..100], u8_as_32_bytes_be(path_offset));
-        // assert_eq!(call[100..132], u8_as_32_bytes_be(payout_to));
-        // assert_eq!(call[132..164], deadline);
-        // assert_eq!(call[164..196], u8_as_32_bytes_be(path_size));
-        // assert_eq!(&call[208..228], token_in.as_fixed_bytes());
-        // assert_eq!(call[228..260], u8_as_32_bytes_be(token_out));
+        let swap_call = &interactions[0];
+        assert_eq!(swap_call.0, router.address());
+        let call = &swap_call.2 .0;
+        let swap_signature = hex!("db3e2198");
+        let deadline = [0xffu8; 32];
+        assert_eq!(call[0..4], swap_signature);
+        assert_eq!(call[4..36], u8_as_32_bytes_be(token_in));
+        assert_eq!(call[36..68], u8_as_32_bytes_be(token_out));
+        assert_eq!(call[96..100], fee.to_be_bytes());
+        assert_eq!(call[100..132], u8_as_32_bytes_be(payout_to));
+        assert_eq!(call[132..164], deadline);
+        assert_eq!(call[164..196], u8_as_32_bytes_be(amount_out));
+        assert_eq!(call[196..228], u8_as_32_bytes_be(amount_in_max));
+        assert_eq!(call[228..260], u8_as_32_bytes_be(0));
     }
 }
