c423e2841715167e10f34f52602be0f60280e6b9
Felix Leupold <felixleupold90@gmail.com>
2022-08-11 20:36:45 +0200

    Update crates/solver/src/in_flight_orders.rs
    Co-authored-by: Nicholas Rodrigues Lordello <nick@cow.fi>

--- crates/solver/src/in_flight_orders.rs
+++ crates/solver/src/in_flight_orders.rs
@@ -75,7 +75,7 @@ impl InFlightOrders {
         });
         auction.orders.retain(|order| match order.data.kind {
             OrderKind::Sell => {
-                u256_to_big_uint(&order.data.sell_amount) > order.metadata.executed_sell_amount
+                u256_to_big_uint(&order.data.sell_amount) > order.metadata.executed_sell_amount_before_fees
             }
             OrderKind::Buy => {
                 u256_to_big_uint(&order.data.buy_amount) > order.metadata.executed_buy_amount
