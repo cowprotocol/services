a0175cd10dd4f50c1056a881e8f47670cce3c6e2
josojo <josojo@hotmail.de>
2022-09-02 16:04:04 +0200

    Valentin's comment: creating one further index

--- database/sql/V030__create_ethflow_orders.sql
+++ database/sql/V030__create_ethflow_orders.sql
@@ -1,11 +1,11 @@
--- table to store additional information only available for ethflow orders
+-- Table to store additional information available for ethflow orders
 -- uid is needed as unique identifier for the order
--- the user_valid_to is the only additional information
+-- The user_valid_to is the valid_to specified by the user
 
 CREATE TABLE ethflow_orders (
     uid bytea PRIMARY KEY,
     valid_to bigint NOT NULL
 );
 
--- to get all valid orders quickly, we create an index
+-- To get all valid orders quickly, we create an index
 CREATE INDEX user_valid_to ON orders USING BTREE (valid_to);
