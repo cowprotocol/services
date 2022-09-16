ebd1f0d347693aabd72bfa8fc3a07995262fd85e
Nicholas Rodrigues Lordello <nick@cow.fi>
2022-09-16 11:09:09 +0200

    Introduce TradeFinding Abstraction (#519)
    This PR introduces a new `TradeFinding` abstraction - and starts by implementing one for 0x.
    
    The `TradeFinding` abstraction is quite similar to `PriceEstimating`, but different in a few key ways:
    - It is not streamed
    - It returns calldata
    - It does not do things like rate limiting and request sharing.
    
    The idea behind the `TradeFinding` abstraction is to split out the "get me a quote for this query" and "optimize, stream, rate limit, and verify a quote" for price estimator implementations. This will allow, for every `TradeFinding`-wrapped price estimator to reuse the rate-limiting and request sharing logic, but also implement a "trade simulation" check where we simulate the calldata, compute the actual gas usage and verify the executed amounts match reported amounts.
    
    Wrapping a `TradeFinding` implementation to create a `PriceEstimation` implementation will come in a follow up PR - and start with the `ZeroExPriceEstimator`.
    
    Additionally, producing calldata for queries can be used in other places (like for bad token detection for example, where we want to simulate a round-trip with two settlements selling and buying a token).
    
    ### Test Plan
    
    Added unit tests and an integration test:
    ```
    % cargo test -p shared -- --ignored --nocapture trade_finding::zeroex
        Finished test [unoptimized + debuginfo] target(s) in 0.27s
         Running unittests src/lib.rs (target/debug/deps/shared-e14dee386a6fa988)
    
    running 1 test
    1.0 ETH buys 10.307734194885478 GNO
    gas:  231093
    data: 0x6af479b200000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000000000000000000000000000008f0c6da3354bc6700000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002bc02aaa39b223fe8d0a0e5c4f27ead9083c756cc20001f46810e776880c02933d47db1b9fc05908e5386b96000000000000000000000000000000000000000000869584cd0000000000000000000000009008d19f58aabd9ed0d60971565aa8510560ab410000000000000000000000000000000000000000000000f8bd44bbe56319a12c
    test trade_finding::zeroex::tests::real_estimate ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 316 filtered out; finished in 1.14s
    ```

---
+++ crates/shared/src/trade_finding.rs
@@ -0,0 +1,47 @@
+//! A module for abstracting a component that can produce a quote with calldata
+//! for a specified token pair and amount.
+
+pub mod zeroex;
+
+use crate::price_estimation::Query;
+use ethcontract::{H160, U256};
+use thiserror::Error;
+
+/// Find a trade for a token pair.
+///
+/// This is similar to the `PriceEstimating` interface, but it expects calldata
+/// to also be produced.
+#[mockall::automock]
+#[async_trait::async_trait]
+pub trait TradeFinding: Send + Sync + 'static {
+    async fn get_trade(&self, query: &Query) -> Result<Trade, TradeError>;
+}
+
+/// A trade.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Trade {
+    pub out_amount: U256,
+    pub gas_estimate: u64,
+    pub approval_spender: Option<H160>,
+    pub interaction: Interaction,
+}
+
+/// Data for a raw GPv2 interaction.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Interaction {
+    pub target: H160,
+    pub value: U256,
+    pub data: Vec<u8>,
+}
+
+#[derive(Error, Debug)]
+pub enum TradeError {
+    #[error("No liquidity")]
+    NoLiquidity,
+
+    #[error("Unsupported Order Type")]
+    UnsupportedOrderType,
+
+    #[error(transparent)]
+    Other(#[from] anyhow::Error),
+}
