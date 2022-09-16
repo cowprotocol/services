6edb813dfa4d5596d36647728b64f7e210460487
Nicholas Rodrigues Lordello <nick@cow.fi>
2022-09-16 17:22:51 +0200

    Expand TradeFinding Interface for Quoting

--- crates/shared/src/trade_finding/oneinch.rs
+++ crates/shared/src/trade_finding/oneinch.rs
@@ -1,6 +1,6 @@
 //! A 1Inch-based trade finder.
 
-use super::{Interaction, Query, Trade, TradeError, TradeFinding};
+use super::{Interaction, Query, Quote, Trade, TradeError, TradeFinding};
 use crate::{
     oneinch_api::{OneInchClient, OneInchError, ProtocolCache, SellOrderQuoteQuery, SwapQuery},
     price_estimation::gas,
@@ -18,7 +18,10 @@ pub struct OneInchTradeFinder {
 }
 
 impl OneInchTradeFinder {
-    async fn quote_and_swap(&self, query: &Query) -> Result<Trade, TradeError> {
+    async fn verify_query_and_get_protocols(
+        &self,
+        query: &Query,
+    ) -> Result<Option<Vec<String>>, TradeError> {
         if query.kind == OrderKind::Buy {
             return Err(TradeError::UnsupportedOrderType);
         }
@@ -28,15 +31,40 @@ impl OneInchTradeFinder {
             .get_allowed_protocols(&self.disabled_protocols, self.api.as_ref())
             .await?;
 
+        Ok(allowed_protocols)
+    }
+
+    async fn quote(&self, query: &Query) -> Result<Quote, TradeError> {
+        let allowed_protocols = self.verify_query_and_get_protocols(query).await?;
+        Ok(self.perform_quote(query, allowed_protocols).await?)
+    }
+
+    async fn perform_quote(
+        &self,
+        query: &Query,
+        allowed_protocols: Option<Vec<String>>,
+    ) -> Result<Quote, OneInchError> {
+        let quote = self
+            .api
+            .get_sell_order_quote(SellOrderQuoteQuery::with_default_options(
+                query.sell_token,
+                query.buy_token,
+                allowed_protocols,
+                query.in_amount,
+                self.referrer_address,
+            ))
+            .await?;
+
+        Ok(Quote {
+            out_amount: quote.to_token_amount,
+            gas_estimate: gas::SETTLEMENT_OVERHEAD + quote.estimated_gas,
+        })
+    }
+
+    async fn quote_and_swap(&self, query: &Query) -> Result<Trade, TradeError> {
+        let allowed_protocols = self.verify_query_and_get_protocols(query).await?;
         let (quote, spender, swap) = futures::try_join!(
-            self.api
-                .get_sell_order_quote(SellOrderQuoteQuery::with_default_options(
-                    query.sell_token,
-                    query.buy_token,
-                    allowed_protocols.clone(),
-                    query.in_amount,
-                    self.referrer_address,
-                )),
+            self.perform_quote(query, allowed_protocols.clone()),
             self.api.get_spender(),
             self.api.get_swap(SwapQuery::with_default_options(
                 query.sell_token,
@@ -50,8 +78,8 @@ impl OneInchTradeFinder {
         )?;
 
         Ok(Trade {
-            out_amount: quote.to_token_amount,
-            gas_estimate: gas::SETTLEMENT_OVERHEAD + quote.estimated_gas,
+            out_amount: quote.out_amount,
+            gas_estimate: quote.gas_estimate,
             approval: Some((query.sell_token, spender.address)),
             interaction: Interaction {
                 target: swap.tx.to,
@@ -86,6 +114,10 @@ impl From<OneInchError> for TradeError {
 
 #[async_trait::async_trait]
 impl TradeFinding for OneInchTradeFinder {
+    async fn get_quote(&self, query: &Query) -> Result<Quote, TradeError> {
+        self.quote(query).await
+    }
+
     async fn get_trade(&self, query: &Query) -> Result<Trade, TradeError> {
         self.quote_and_swap(query).await
     }
@@ -102,6 +134,49 @@ mod tests {
 
     fn create_trade_finder<T: OneInchClient + 'static>(api: T) -> OneInchTradeFinder {
         OneInchTradeFinder::new(Arc::new(api), Vec::default(), None)
+    }
+
+    #[tokio::test]
+    async fn quote_sell_order_succeeds() {
+        // How much GNO can you buy for 1 WETH
+        let mut one_inch = MockOneInchClient::new();
+
+        // Response was generated with:
+        //
+        // curl 'https://api.1inch.io/v4.0/1/quote?\
+        //     fromTokenAddress=0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2&\
+        //     toTokenAddress=0x6810e776880c02933d47db1b9fc05908e5386b96&\
+        //     amount=100000000000000000'
+        one_inch.expect_get_sell_order_quote().return_once(|_| {
+            Ok(SellOrderQuote {
+                from_token: Token {
+                    address: testlib::tokens::WETH,
+                },
+                to_token: Token {
+                    address: testlib::tokens::GNO,
+                },
+                to_token_amount: 808_069_760_400_778_577u128.into(),
+                from_token_amount: 100_000_000_000_000_000u128.into(),
+                protocols: Vec::default(),
+                estimated_gas: 189_386,
+            })
+        });
+
+        let estimator = create_trade_finder(one_inch);
+
+        let quote = estimator
+            .get_quote(&Query {
+                from: None,
+                sell_token: testlib::tokens::WETH,
+                buy_token: testlib::tokens::GNO,
+                in_amount: 1_000_000_000_000_000_000u128.into(),
+                kind: OrderKind::Sell,
+            })
+            .await
+            .unwrap();
+
+        assert_eq!(quote.out_amount, 808_069_760_400_778_577u128.into());
+        assert!(quote.gas_estimate > 189_386);
     }
 
     #[tokio::test]
