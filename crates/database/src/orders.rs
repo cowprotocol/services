43724c27630a61605ab993937638415eb592c3a5
josojo <josojo@hotmail.de>
2022-09-28 11:53:22 +0200

    Valentin's suggestion

--- crates/database/src/orders.rs
+++ crates/database/src/orders.rs
@@ -174,8 +174,8 @@ pub async fn read_order(
     id: &OrderUid,
 ) -> Result<Option<Order>, sqlx::Error> {
     const QUERY: &str = r#"
-        SELECT * FROM ORDERS
-        WHERE uid = $1
+SELECT * FROM ORDERS
+WHERE uid = $1
     "#;
     sqlx::query_as(QUERY).bind(id).fetch_optional(ex).await
 }
@@ -312,7 +312,8 @@ const ORDERS_SELECT: &str = r#"
 o.uid, o.owner, o.creation_timestamp, o.sell_token, o.buy_token, o.sell_amount, o.buy_amount,
 o.valid_to, o.app_data, o.fee_amount, o.full_fee_amount, o.kind, o.partially_fillable, o.signature,
 o.receiver, o.signing_scheme, o.settlement_contract, o.sell_token_balance, o.buy_token_balance,
-o.is_liquidity_order, (CASE WHEN eth_o.uid is NULL THEN false ELSE true END) as is_ethflow_order,
+o.is_liquidity_order,
+(SELECT EXISTS(SELECT 1 FROM ethflow_orders eo WHERE eo.uid = o.uid)) as is_ethflow_order,
 (SELECT COALESCE(SUM(t.buy_amount), 0) FROM trades t WHERE t.order_uid = o.uid) AS sum_buy,
 (SELECT COALESCE(SUM(t.sell_amount), 0) FROM trades t WHERE t.order_uid = o.uid) AS sum_sell,
 (SELECT COALESCE(SUM(t.fee_amount), 0) FROM trades t WHERE t.order_uid = o.uid) AS sum_fee,
@@ -328,7 +329,7 @@ o.is_liquidity_order, (CASE WHEN eth_o.uid is NULL THEN false ELSE true END) as 
 ), true)) AS presignature_pending
 "#;
 
-const ORDERS_FROM: &str = "orders o LEFT OUTER JOIN ethflow_orders eth_o on eth_o.uid = o.uid";
+const ORDERS_FROM: &str = "orders o";
 
 pub async fn single_full_order(
     ex: &mut PgConnection,
