285d9e157eb79342f98fdf3e49256cb9af2da414
josojo <josojo@hotmail.de>
2022-10-13 17:18:56 +0200

    Valentin's suggestions

--- database/sql/V033__new_table_pre_interactions.sql
+++ database/sql/V033__new_table_pre_interactions.sql
@@ -1,16 +1,10 @@
 -- Creates new table for pre_interactions of orders
--- Since one order can have many pre_interactions,
--- the primary key is not the uid, but the whole 
--- pre_interaction
 
 CREATE TABLE pre_interactions (
     order_uid bytea NOT NULL,
     target_to bytea NOT NULL,
     value numeric(78,0) NOT NULL,
-    data bytea,
-    PRIMARY KEY (order_uid, target_to, value, data)
+    data bytea NOT NULL,
 );
 
 CREATE INDEX uid ON pre_interactions USING HASH (order_uid);
-
-
