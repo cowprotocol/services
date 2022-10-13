0491a9f9880624f95230df573f63d0053ea63ae2
josojo <josojo@hotmail.de>
2022-10-13 17:21:37 +0200

    fixing error from before

--- database/sql/V033__new_table_pre_interactions.sql
+++ database/sql/V033__new_table_pre_interactions.sql
@@ -4,7 +4,7 @@ CREATE TABLE pre_interactions (
     order_uid bytea NOT NULL,
     target_to bytea NOT NULL,
     value numeric(78,0) NOT NULL,
-    data bytea NOT NULL,
+    data bytea NOT NULL
 );
 
 CREATE INDEX uid ON pre_interactions USING HASH (order_uid);
