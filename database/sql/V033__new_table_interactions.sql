cdcafd2afa3d7c1d4bd28bf4edbad3460bef6aaa
josojo <josojo@hotmail.de>
2022-10-18 17:13:11 +0200

    adding missin file

---
+++ database/sql/V033__new_table_interactions.sql
@@ -0,0 +1,12 @@
+-- Creates new table for pre_interactions of orders
+
+CREATE TABLE interactions (
+    order_uid bytea NOT NULL,
+    index int NOT NULL,
+    target bytea NOT NULL,
+    value numeric(78,0) NOT NULL,
+    data bytea NOT NULL,
+    PRIMARY KEY(order_uid, index)
+);
+
+CREATE INDEX uid ON interactions USING HASH (order_uid);
