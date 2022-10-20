cab122178fe9a57369156e174de5430de63adfe0
Dusan Stanivukovic <dusan.stanivukovic@gmail.com>
2022-10-20 16:35:17 +0200

    Add argument for MAX_POOLS_TO_INITIALIZE (#653)

--- crates/orderbook/src/main.rs
+++ crates/orderbook/src/main.rs
@@ -236,9 +236,14 @@ async fn main() {
     };
     let uniswap_v3_pool_fetcher = if baseline_sources.contains(&BaselineSource::UniswapV3) {
         let uniswap_v3_pool_fetcher = Arc::new(
-            UniswapV3PoolFetcher::new(chain_id, http_factory.create(), web3.clone())
-                .await
-                .expect("failed to create UniswapV3 pool fetcher in orderbook"),
+            UniswapV3PoolFetcher::new(
+                chain_id,
+                http_factory.create(),
+                web3.clone(),
+                args.shared.max_pools_to_initialize_cache,
+            )
+            .await
+            .expect("failed to create UniswapV3 pool fetcher in orderbook"),
         );
         Some(uniswap_v3_pool_fetcher)
     } else {
