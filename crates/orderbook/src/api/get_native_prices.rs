69e90f36bb230e3978eff5a051f4e8e46c0e156d
sunce86 <dusan.stanivukovic@gmail.com>
2022-10-20 13:00:18 +0200

    receive single value instead of array

--- crates/orderbook/src/api/get_native_prices.rs
+++ crates/orderbook/src/api/get_native_prices.rs
@@ -1,31 +1,21 @@
 use crate::orderbook::Orderbook;
 use anyhow::Result;
 use ethcontract::H160;
-use serde::Deserialize;
 use shared::api::{ApiReply, IntoWarpReply};
 use std::{convert::Infallible, sync::Arc};
 use warp::{hyper::StatusCode, reply::with_status, Filter, Rejection};
 
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct NativeTokens {
-    tokens: Vec<H160>,
-}
-
-fn get_native_prices_request() -> impl Filter<Extract = (NativeTokens,), Error = Rejection> + Clone
-{
-    warp::path!("nativePrices")
-        .and(warp::get())
-        .and(warp::query::<NativeTokens>())
+fn get_native_prices_request() -> impl Filter<Extract = (H160,), Error = Rejection> + Clone {
+    warp::path!("token" / H160 / "native_prices").and(warp::get())
 }
 
 pub fn get_native_prices(
     orderbook: Arc<Orderbook>,
 ) -> impl Filter<Extract = (ApiReply,), Error = Rejection> + Clone {
-    get_native_prices_request().and_then(move |tokens: NativeTokens| {
+    get_native_prices_request().and_then(move |token: H160| {
         let orderbook = orderbook.clone();
         async move {
-            let result = orderbook.get_native_prices(&tokens.tokens).await;
+            let result = orderbook.get_native_prices(&[token]).await;
             let reply = match result {
                 Ok(estimates) => with_status(warp::reply::json(&estimates), StatusCode::OK),
                 Err(err) => err.into_warp_reply(),
@@ -44,7 +34,7 @@ mod tests {
 
     #[test]
     fn native_prices_query() {
-        let path= "/nativePrices?tokens=0xdac17f958d2ee523a2206206994597c13d831ec7,0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+        let path = "/token/0xdac17f958d2ee523a2206206994597c13d831ec7/native_prices";
         let request = request().path(path).method("GET");
         let result = request
             .filter(&get_native_prices_request())
@@ -52,11 +42,8 @@ mod tests {
             .unwrap()
             .unwrap();
         assert_eq!(
-            result.tokens,
-            vec![
-                H160(hex!("dac17f958d2ee523a2206206994597c13d831ec7")),
-                H160(hex!("a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"))
-            ]
+            result,
+            H160(hex!("dac17f958d2ee523a2206206994597c13d831ec7"))
         );
     }
 }
