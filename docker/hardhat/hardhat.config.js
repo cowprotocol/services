88f30154895eff74487adfeda064448394441ed6
Niksa <enntheprogrammer@gmail.com>
2022-10-24 13:55:18 +0000

    add docker-compose file

---
+++ docker/hardhat/hardhat.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    networks: {
+        hardhat: {
+            initialBaseFeePerGas: 0,
+            accounts: {
+                accountsBalance: "1000000000000000000000000"
+            }
+        }
+    }
+};
