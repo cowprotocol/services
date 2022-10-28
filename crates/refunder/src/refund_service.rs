ba2071db1a213edb2d4c026014cd32d5c6f8350a
josojo <josojo@hotmail.de>
2022-10-28 21:13:10 +0200

    Martin's comments

--- crates/refunder/src/refund_service.rs
+++ crates/refunder/src/refund_service.rs
@@ -1,19 +1,15 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use contracts::CoWSwapEthFlow;
 use database::{
-    ethflow_orders::{mark_eth_orders_as_refunded, refundable_orders},
+    ethflow_orders::{mark_eth_orders_as_refunded, refundable_orders, EthOrderPlacement},
     OrderUid,
 };
-use lazy_static::lazy_static;
 use primitive_types::H160;
 use shared::{transport::MAX_BATCH_SIZE, Web3, Web3CallBatch};
 use sqlx::PgPool;
 use std::time::SystemTime;
 
-// The following address is used in the ethflow contract to mark invalidated orders
-lazy_static! {
-    static ref INVALIDATED_OWNER: H160 = H160([255u8; 20]);
-}
+const INVALIDATED_OWNER: H160 = H160([255u8; 20]);
 
 pub struct RefundService {
     pub db: PgPool,
@@ -43,23 +39,62 @@ impl RefundService {
     }
 
     pub async fn try_to_refund_all_eligble_orders(&self) -> Result<()> {
-        // Look for all refundable orders by calling the database
+        let refundable_order_uids = self.get_refundable_ethflow_orders_from_db().await?;
+
+        let (refunded, _to_be_refunded) = self
+            .identify_already_refunded_order_uids_via_web3_calls(refundable_order_uids)
+            .await?;
+
+        self.update_already_refunded_orders_in_db(refunded).await?;
+
+        // Send out tx to deleteOrders, and wait for 5 block for the tx
+        // to be mined
+        // todo()
+
+        Ok(())
+    }
+
+    async fn update_already_refunded_orders_in_db(
+        &self,
+        refunded: Vec<(OrderUid, bool)>,
+    ) -> Result<()> {
+        let mut ex = self.db.acquire().await?;
+        let refunded_uids: Vec<OrderUid> = refunded.into_iter().map(|(uid, _)| uid).collect();
+        mark_eth_orders_as_refunded(&mut ex, refunded_uids.as_slice())
+            .await
+            .map_err(|err| {
+                anyhow!(
+                    "Error while retrieving updating the already refunded orders:{:?}",
+                    err
+                )
+            })
+    }
+
+    async fn get_refundable_ethflow_orders_from_db(&self) -> Result<Vec<EthOrderPlacement>> {
         let unix_timestamp = SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
             .unwrap()
             .as_secs() as i64;
         let mut ex = self.db.acquire().await?;
-        let refundable_order_uids = refundable_orders(
+        refundable_orders(
             &mut ex,
             unix_timestamp,
             self.min_validity_duration,
             self.min_slippage,
         )
-        .await?;
+        .await
+        .map_err(|err| {
+            anyhow!(
+                "Error while retrieving the refundable ethflow orders from db: {:?}",
+                err
+            )
+        })
+    }
 
-        // Identify orderUids that are already refunded
-        // by making a batched web3 call.
-
+    async fn identify_already_refunded_order_uids_via_web3_calls(
+        &self,
+        refundable_order_uids: Vec<EthOrderPlacement>,
+    ) -> Result<SplittedOrderUids> {
         let mut batch = Web3CallBatch::new(self.web3.transport().clone());
         let futures = refundable_order_uids
             .iter()
@@ -76,7 +111,7 @@ impl RefundService {
                         Ok(order) => Some(order.0),
                         Err(err) => return Err(err),
                     };
-                    let is_refunded: bool = order_owner == Some(*INVALIDATED_OWNER);
+                    let is_refunded: bool = order_owner == Some(INVALIDATED_OWNER);
                     Ok((eth_order_placement.uid, is_refunded))
                 }
             })
@@ -85,18 +120,8 @@ impl RefundService {
         batch.execute_all(MAX_BATCH_SIZE).await;
         let uid_with_latest_refundablility = futures::future::try_join_all(futures).await?;
 
-        let (refunded, _to_be_refunded): SplittedOrderUids = uid_with_latest_refundablility
+        Ok(uid_with_latest_refundablility
             .into_iter()
-            .partition(|(_, is_refunded)| *is_refunded);
-
-        // Update refunded ethflow_orders in the database
-        let refunded_uids: Vec<OrderUid> = refunded.into_iter().map(|(uid, _)| uid).collect();
-        mark_eth_orders_as_refunded(&mut ex, refunded_uids.as_slice()).await?;
-
-        // Send out tx to deleteOrders, and wait for 5 block for the tx
-        // to be mined
-        // todo()
-
-        Ok(())
+            .partition(|(_, is_refunded)| *is_refunded))
     }
 }
