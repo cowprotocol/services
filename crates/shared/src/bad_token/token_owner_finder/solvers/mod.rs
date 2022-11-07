7beec12fd4f20e1ec7bc260705e4bcceda316dc0
sunce86 <dusan.stanivukovic@gmail.com>
2022-11-07 17:15:57 +0100

    fix wording

--- crates/shared/src/bad_token/token_owner_finder/solvers/mod.rs
+++ crates/shared/src/bad_token/token_owner_finder/solvers/mod.rs
@@ -11,6 +11,6 @@ type Owner = H160;
 
 #[async_trait::async_trait]
 pub trait TokenOwnerSolverApi: Send + Sync {
-    /// Get token owner pairs from spesific solver
+    /// Get token owner pairs from specific solver
     async fn get_token_owner_pairs(&self) -> Result<HashMap<Token, Vec<Owner>>>;
 }
