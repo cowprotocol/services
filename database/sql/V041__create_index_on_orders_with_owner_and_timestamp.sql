a82f8897268c2148510d2041e3b47b1bcd14bab7
josojo <josojo@hotmail.de>
2022-11-09 09:11:41 +0100

    Also adding old index to increase performance

---
+++ database/sql/V041__create_index_on_orders_with_owner_and_timestamp.sql
@@ -0,0 +1,3 @@
+-- The index can be traversed in both directions anyway but we usually fetch user orders with this
+-- ordering so using it here too.
+CREATE INDEX user_order_creation_timestamp ON orders USING BTREE (owner, creation_timestamp DESC);
