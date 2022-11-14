914ca88cb61e336724d3bc6294bc89ba2e27bfc2
Niksa <enntheprogrammer@gmail.com>
2022-11-14 18:14:07 +0100

    add new test

--- crates/e2e/tests/e2e/limit_orders.rs
+++ crates/e2e/tests/e2e/limit_orders.rs
@@ -1,4 +1,10 @@
-use crate::services::{deploy_mintable_token, to_wei, OrderbookServices, API_HOST};
+use std::sync::Arc;
+
+use crate::services::{
+    create_order_converter, create_orderbook_api, deploy_mintable_token, to_wei,
+    uniswap_pair_provider, wait_for_solvable_orders, OrderbookServices, API_HOST,
+};
+use contracts::IUniswapLikeRouter;
 use ethcontract::prelude::{Account, Address, PrivateKey, U256};
 use hex_literal::hex;
 use model::{
@@ -6,11 +12,27 @@ use model::{
     signature::EcdsaSigningScheme,
 };
 use secp256k1::SecretKey;
-use shared::{ethrpc::Web3, http_client::HttpClientFactory};
+use shared::{
+    ethrpc::Web3, http_client::HttpClientFactory, maintenance::Maintaining,
+    sources::uniswap_v2::pool_fetching::PoolFetcher,
+};
+use solver::{
+    liquidity::uniswap_v2::UniswapLikeLiquidity,
+    liquidity_collector::LiquidityCollector,
+    metrics::NoopMetrics,
+    settlement_access_list::{create_priority_estimator, AccessListEstimatorType},
+    settlement_submission::{
+        submitter::{public_mempool_api::PublicMempoolApi, Strategy},
+        GlobalTxPool, SolutionSubmitter, StrategyArgs,
+    },
+};
+use std::time::Duration;
 use web3::signing::SecretKeyRef;
 
-const TRADER_PK: [u8; 32] =
+const TRADER_A_PK: [u8; 32] =
     hex!("0000000000000000000000000000000000000000000000000000000000000001");
+const TRADER_B_PK: [u8; 32] =
+    hex!("0000000000000000000000000000000000000000000000000000000000000002");
 
 const ORDER_PLACEMENT_ENDPOINT: &str = "/api/v1/orders/";
 
@@ -33,20 +55,21 @@ async fn single_limit_order_test(web3: Web3) {
 
     let accounts: Vec<Address> = web3.eth().accounts().await.expect("get accounts failed");
     let solver_account = Account::Local(accounts[0], None);
-    let trader_account = Account::Offline(PrivateKey::from_raw(TRADER_PK).unwrap(), None);
+    let trader_a = Account::Offline(PrivateKey::from_raw(TRADER_A_PK).unwrap(), None);
+    let trader_b = Account::Offline(PrivateKey::from_raw(TRADER_B_PK).unwrap(), None);
 
-    // Create & Mint tokens to trade
+    // Create tokens to trade
     let token_a = deploy_mintable_token(&web3).await;
     let token_b = deploy_mintable_token(&web3).await;
 
-    // Fund trader and settlement accounts
+    // Fund trader accounts
     tx!(
         solver_account,
-        token_a.mint(trader_account.address(), to_wei(100))
+        token_a.mint(trader_a.address(), to_wei(1010))
     );
     tx!(
         solver_account,
-        token_b.mint(contracts.gp_settlement.address(), to_wei(100))
+        token_b.mint(trader_b.address(), to_wei(510))
     );
 
     // Create and fund Uniswap pool
@@ -119,13 +142,17 @@ async fn single_limit_order_test(web3: Web3) {
     }
 
     // Approve GPv2 for trading
-    tx!(
-        trader_account,
-        token_a.approve(contracts.allowance, to_wei(100))
-    );
+    tx!(trader_a, token_a.approve(contracts.allowance, to_wei(101)));
+    tx!(trader_b, token_b.approve(contracts.allowance, to_wei(51)));
 
     // Place Orders
-    let _services = OrderbookServices::new(&web3, &contracts, true).await;
+    let OrderbookServices {
+        maintenance,
+        block_stream,
+        solvable_orders_cache,
+        base_tokens,
+        ..
+    } = OrderbookServices::new(&web3, &contracts, true).await;
 
     let http_factory = HttpClientFactory::default();
     let client = http_factory.create();
@@ -134,13 +161,13 @@ async fn single_limit_order_test(web3: Web3) {
         .with_sell_token(token_a.address())
         .with_sell_amount(to_wei(100))
         .with_buy_token(token_b.address())
-        .with_buy_amount(to_wei(1200))
+        .with_buy_amount(to_wei(80))
         .with_valid_to(model::time::now_in_epoch_seconds() + 300)
         .with_kind(OrderKind::Sell)
         .sign_with(
             EcdsaSigningScheme::Eip712,
             &contracts.domain_separator,
-            SecretKeyRef::from(&SecretKey::from_slice(&TRADER_PK).unwrap()),
+            SecretKeyRef::from(&SecretKey::from_slice(&TRADER_A_PK).unwrap()),
         )
         .build()
         .into_order_creation();
@@ -153,7 +180,7 @@ async fn single_limit_order_test(web3: Web3) {
     assert_eq!(placement.status(), 201);
     let order_id: String = placement.json().await.unwrap();
 
-    let order: Order = client
+    let limit_order: Order = client
         .get(&format!("{API_HOST}{ORDER_PLACEMENT_ENDPOINT}{order_id}"))
         .json(&order)
         .send()
@@ -162,11 +189,105 @@ async fn single_limit_order_test(web3: Web3) {
         .json()
         .await
         .unwrap();
+    assert_eq!(limit_order.metadata.class, OrderClass::Limit);
 
-    assert_eq!(order.metadata.class, OrderClass::Limit);
+    wait_for_solvable_orders(&client, 1).await.unwrap();
 
-    // TODO #643 Extend this to actually simulate driving the solution. Look at other E2E tests for
-    // an example of how this can be done.
+    // Drive solution
+    let uniswap_pair_provider = uniswap_pair_provider(&contracts);
+    let uniswap_liquidity = UniswapLikeLiquidity::new(
+        IUniswapLikeRouter::at(&web3, contracts.uniswap_router.address()),
+        contracts.gp_settlement.clone(),
+        base_tokens,
+        web3.clone(),
+        Arc::new(PoolFetcher::uniswap(uniswap_pair_provider, web3.clone())),
+    );
+    let solver = solver::solver::naive_solver(solver_account);
+    let liquidity_collector = LiquidityCollector {
+        uniswap_like_liquidity: vec![uniswap_liquidity],
+        balancer_v2_liquidity: None,
+        zeroex_liquidity: None,
+        uniswap_v3_liquidity: None,
+    };
+    let network_id = web3.net().version().await.unwrap();
+    let submitted_transactions = GlobalTxPool::default();
+    let mut driver = solver::driver::Driver::new(
+        contracts.gp_settlement.clone(),
+        liquidity_collector,
+        vec![solver],
+        Arc::new(web3.clone()),
+        Duration::from_secs(30),
+        contracts.weth.address(),
+        Duration::from_secs(0),
+        Arc::new(NoopMetrics::default()),
+        web3.clone(),
+        network_id.clone(),
+        Duration::from_secs(30),
+        Default::default(),
+        block_stream,
+        SolutionSubmitter {
+            web3: web3.clone(),
+            contract: contracts.gp_settlement.clone(),
+            gas_price_estimator: Arc::new(web3.clone()),
+            target_confirm_time: Duration::from_secs(1),
+            gas_price_cap: f64::MAX,
+            max_confirm_time: Duration::from_secs(120),
+            retry_interval: Duration::from_secs(5),
+            transaction_strategies: vec![
+                solver::settlement_submission::TransactionStrategy::PublicMempool(StrategyArgs {
+                    submit_api: Box::new(PublicMempoolApi::new(vec![web3.clone()], false)),
+                    max_additional_tip: 0.,
+                    additional_tip_percentage_of_max_fee: 0.,
+                    sub_tx_pool: submitted_transactions.add_sub_pool(Strategy::PublicMempool),
+                }),
+            ],
+            access_list_estimator: Arc::new(
+                create_priority_estimator(
+                    &web3,
+                    &[AccessListEstimatorType::Web3],
+                    None,
+                    network_id,
+                )
+                .unwrap(),
+            ),
+        },
+        create_orderbook_api(),
+        create_order_converter(&web3, contracts.weth.address()),
+        0.0,
+        15000000u128,
+        1.0,
+        None,
+        None.into(),
+        None,
+        0,
+    );
+    driver.single_run().await.unwrap();
+
+    // Check matching
+    let balance = token_b
+        .balance_of(trader_a.address())
+        .call()
+        .await
+        .expect("Couldn't fetch TokenB's balance");
+    assert_eq!(balance, U256::from(99_600_698_103_990_321_649_u128));
+
+    let balance = token_a
+        .balance_of(trader_b.address())
+        .call()
+        .await
+        .expect("Couldn't fetch TokenA's balance");
+    // Didn't touch the balance of token_a
+    assert_eq!(balance, U256::zero());
+
+    // Drive orderbook in order to check the removal of settled order_b
+    maintenance.run_maintenance().await.unwrap();
+    solvable_orders_cache.update(0).await.unwrap();
+
+    let auction = create_orderbook_api().get_auction().await.unwrap();
+    assert!(auction.auction.orders.is_empty());
+
+    // Drive again to ensure we can continue solution finding
+    driver.single_run().await.unwrap();
 }
 
 async fn too_many_limit_orders_test(web3: Web3) {
@@ -176,7 +297,7 @@ async fn too_many_limit_orders_test(web3: Web3) {
 
     let accounts: Vec<Address> = web3.eth().accounts().await.expect("get accounts failed");
     let solver_account = Account::Local(accounts[0], None);
-    let trader_account = Account::Offline(PrivateKey::from_raw(TRADER_PK).unwrap(), None);
+    let trader_account = Account::Offline(PrivateKey::from_raw(TRADER_A_PK).unwrap(), None);
 
     // Create & Mint tokens to trade
     let token_a = deploy_mintable_token(&web3).await;
@@ -208,13 +329,13 @@ async fn too_many_limit_orders_test(web3: Web3) {
         .with_sell_token(token_a.address())
         .with_sell_amount(to_wei(100))
         .with_buy_token(token_b.address())
-        .with_buy_amount(to_wei(1200))
+        .with_buy_amount(to_wei(80))
         .with_valid_to(model::time::now_in_epoch_seconds() + 300)
         .with_kind(OrderKind::Sell)
         .sign_with(
             EcdsaSigningScheme::Eip712,
             &contracts.domain_separator,
-            SecretKeyRef::from(&SecretKey::from_slice(&TRADER_PK).unwrap()),
+            SecretKeyRef::from(&SecretKey::from_slice(&TRADER_A_PK).unwrap()),
         )
         .build()
         .into_order_creation();
@@ -238,7 +359,7 @@ async fn too_many_limit_orders_test(web3: Web3) {
         .sign_with(
             EcdsaSigningScheme::Eip712,
             &contracts.domain_separator,
-            SecretKeyRef::from(&SecretKey::from_slice(&TRADER_PK).unwrap()),
+            SecretKeyRef::from(&SecretKey::from_slice(&TRADER_A_PK).unwrap()),
         )
         .build()
         .into_order_creation();
