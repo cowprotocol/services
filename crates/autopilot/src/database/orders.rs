5e887e694bc1c86eb369ed9878011d8db3178bef
Nicholas Rodrigues Lordello <nick@cow.fi>
2022-11-17 13:23:37 +0100

    Include Full Fee Amount For Limit Orders (#792)
    Fixes #770 
    
    This PR makes sure limit orders also set their `full_fee_amount`. Specifically, CoW Protocol orders have 2 different fee fields:
    - `fee_amount`: This is the actual fee amount that we charge the user. In order to make trading more attractive on CoW Swap, we have a few "knobs" for configuring subsidies on orders used to compute this amount. For limit orders, this is the analogous to `surplus_fee` which is the actual fee we change the user (but instead of it being a pro-rated fee, it is taken from the order's surplus).
    - `full_fee_amount`: This is the actual expected "cost" of executing the order. We store this for a couple of reasons:
      1. Primary reason is that we want to use this value in the objective function and **not** the actual fee amount. The reason for this is we want competition to be done without taking subsidies into account.
      2. Secondary reason (and much less important) is to help in debugging and make sure our fee estimation is working as expected and somewhat accurate (we should compare these values with the actual on-chain gas costs of settlement executions).
      
    These fields should be accurate for limit orders now 🎉.
    
    ### Test Plan
    
    Unit test to check that this field is being set. It gets read in the same way that it gets read for regular orders during auction building.

--- crates/autopilot/src/database/orders.rs
+++ crates/autopilot/src/database/orders.rs
@@ -10,6 +10,17 @@ use model::{
 use number_conversions::u256_to_big_decimal;
 use shared::db_order_conversions::full_order_into_model_order;
 
+/// New fee data to update a limit order with.
+pub struct FeeUpdate {
+    /// The actual fee amount to charge the order from its surplus.
+    pub surplus_fee: U256,
+
+    /// The full unsubsidized fee amount that settling the order is expected to
+    /// actually cost. This is used for objective value computation so that fee
+    /// subsidies do not change the objective value.
+    pub full_fee_amount: U256,
+}
+
 impl Postgres {
     pub async fn limit_orders_with_outdated_fees(&self, age: Duration) -> Result<Vec<Order>> {
         let _timer = super::Metrics::get()
@@ -32,18 +43,25 @@ impl Postgres {
         .await
     }
 
-    pub async fn update_surplus_fee(&self, order_uid: &OrderUid, surplus_fee: U256) -> Result<()> {
+    pub async fn update_limit_order_fees(
+        &self,
+        order_uid: &OrderUid,
+        update: &FeeUpdate,
+    ) -> Result<()> {
         let _timer = super::Metrics::get()
             .database_queries
-            .with_label_values(&["update_surplus_fee"])
+            .with_label_values(&["update_limit_order_fees"])
             .start_timer();
 
         let mut ex = self.0.acquire().await?;
-        database::orders::update_surplus_fee(
+        database::orders::update_limit_order_fees(
             &mut ex,
             &database::byte_array::ByteArray(order_uid.0),
-            &u256_to_big_decimal(&surplus_fee),
-            Utc::now(),
+            &database::orders::FeeUpdate {
+                surplus_fee: u256_to_big_decimal(&update.surplus_fee),
+                surplus_fee_timestamp: Utc::now(),
+                full_fee_amount: u256_to_big_decimal(&update.full_fee_amount),
+            },
         )
         .await?;
         Ok(())
