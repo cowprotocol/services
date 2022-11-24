04e892d5c1f2128c3904fe82c65efc1544490212
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-11-24 15:17:46 +0100

    Make clippy happy

--- crates/orderbook/src/api/version.rs
+++ crates/orderbook/src/api/version.rs
@@ -5,14 +5,16 @@ use std::convert::Infallible;
 use warp::{reply::with_status, Filter, Rejection};
 
 pub fn version() -> impl Filter<Extract = (ApiReply,), Error = Rejection> + Clone {
-    warp::path!("v1" / "version").and(warp::get()).and_then(|| async {
-        Result::<_, Infallible>::Ok(with_status(
-            warp::reply::json(&json!({
-                "version": env!("VERGEN_GIT_SEMVER_LIGHTWEIGHT"),
-                "commit": env!("VERGEN_GIT_SHA"),
-                "branch": env!("VERGEN_GIT_BRANCH"),
-            })),
-            StatusCode::OK,
-        ))
-    })
+    warp::path!("v1" / "version")
+        .and(warp::get())
+        .and_then(|| async {
+            Result::<_, Infallible>::Ok(with_status(
+                warp::reply::json(&json!({
+                    "version": env!("VERGEN_GIT_SEMVER_LIGHTWEIGHT"),
+                    "commit": env!("VERGEN_GIT_SHA"),
+                    "branch": env!("VERGEN_GIT_BRANCH"),
+                })),
+                StatusCode::OK,
+            ))
+        })
 }
