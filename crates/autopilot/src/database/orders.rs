de41803978a65af3c4defc79d13ec3a0528c0e0c
Martin Beckmann <martin.beckmann@protonmail.com>
2022-12-06 10:35:49 +0100

    Store limit order quote to compute risk adjusted rewards later on (#908)

--- crates/autopilot/src/database/orders.rs
+++ crates/autopilot/src/database/orders.rs
@@ -1,6 +1,7 @@
 use super::Postgres;
 use anyhow::Result;
 use chrono::{Duration, Utc};
+use database::byte_array::ByteArray;
 use ethcontract::U256;
 use futures::{StreamExt, TryStreamExt};
 use model::{
@@ -8,7 +9,7 @@ use model::{
     time::now_in_epoch_seconds,
 };
 use number_conversions::u256_to_big_decimal;
-use shared::db_order_conversions::full_order_into_model_order;
+use shared::{db_order_conversions::full_order_into_model_order, fee_subsidy::FeeParameters};
 
 /// New fee data to update a limit order with.
 pub struct FeeUpdate {
@@ -19,6 +20,18 @@ pub struct FeeUpdate {
     /// actually cost. This is used for objective value computation so that fee
     /// subsidies do not change the objective value.
     pub full_fee_amount: U256,
+}
+
+/// Data required to compute risk adjusted rewards for limit orders.
+pub struct LimitOrderQuote {
+    /// Everything required to compute the fee amount in sell token
+    pub fee_parameters: FeeParameters,
+
+    /// The `sell_amount` of the quote associated with the `surplus_fee` estimation.
+    pub sell_amount: U256,
+
+    /// The `buy_amount` of the quote associated with the `surplus_fee` estimation.
+    pub buy_amount: U256,
 }
 
 impl Postgres {
@@ -43,17 +56,20 @@ impl Postgres {
         .await
     }
 
+    /// Updates the `surplus_fee` of a limit order together with the quote used to compute that
+    /// fee.
     pub async fn update_limit_order_fees(
         &self,
         order_uid: &OrderUid,
         update: &FeeUpdate,
+        quote: &LimitOrderQuote,
     ) -> Result<()> {
         let _timer = super::Metrics::get()
             .database_queries
             .with_label_values(&["update_limit_order_fees"])
             .start_timer();
 
-        let mut ex = self.0.acquire().await?;
+        let mut ex = self.0.begin().await?;
         database::orders::update_limit_order_fees(
             &mut ex,
             &database::byte_array::ByteArray(order_uid.0),
@@ -64,6 +80,20 @@ impl Postgres {
             },
         )
         .await?;
+
+        database::orders::insert_quote_and_update_on_conflict(
+            &mut ex,
+            &database::orders::Quote {
+                order_uid: ByteArray(order_uid.0),
+                gas_amount: quote.fee_parameters.gas_amount,
+                gas_price: quote.fee_parameters.gas_price,
+                sell_token_price: quote.fee_parameters.sell_token_price,
+                sell_amount: u256_to_big_decimal(&quote.sell_amount),
+                buy_amount: u256_to_big_decimal(&quote.buy_amount),
+            },
+        )
+        .await?;
+        ex.commit().await?;
         Ok(())
     }
 
