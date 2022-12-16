16d32ed71fa70190d987b1978c0d6cea83bc6050
Martin Beckmann <martin.beckmann@gnosis.pm>
2022-12-16 11:41:45 +0100

    update comment on migration file

--- database/sql/V045__store_refund_tx.sql
+++ database/sql/V045__store_refund_tx.sql
@@ -5,9 +5,9 @@ CREATE TABLE ethflow_refunds
     tx_hash bytea NOT NULL
 );
 
--- Ideally we would populate the `refund_tx` column with the correct value for already refunedn transactions in the migration.
--- However, we don't have the required information in the DB so we simply use the 0x00..00 hash as a sentinel value to easily
--- replace the placeholder values with the correct hash with a manual update.
+-- Ideally we would populate the ethflow_refunds table with the correct value for already refunded orders in the migration.
+-- However, we don't have the required information in the DB so we simply use placeholder values to easily replace them
+-- with the correct hash with a manual update.
 INSERT INTO ethflow_refunds (order_uid, block_number, tx_hash)
 SELECT uid, 0, '\x0000000000000000000000000000000000000000000000000000000000000000'
 FROM ethflow_orders
