0e21fd42c8861ad119cd64333610ce3bc9a1cf92
Nicholas Rodrigues Lordello <nick@cow.fi>
2023-01-12 22:15:57 +0100

    Hook up Baseline Solver to API

--- crates/solvers/src/main.rs
+++ crates/solvers/src/main.rs
@@ -1,22 +1,40 @@
 // TODO remove this once the crate stabilizes a bit.
 #![allow(dead_code)]
 
-#[cfg(unix)]
-use tokio::signal::unix::{self, SignalKind};
-
 mod api;
 mod boundary;
+mod cli;
 mod domain;
 mod util;
 
+use crate::domain::baseline;
+use clap::Parser;
+#[cfg(unix)]
+use tokio::signal::unix::{self, SignalKind};
+use tracing::level_filters::LevelFilter;
+
 #[tokio::main]
 async fn main() {
-    run().await;
+    let cli = cli::Cli::parse();
+
+    shared::tracing::initialize(&cli.log_filter, LevelFilter::ERROR);
+    shared::exit_process_on_panic::set_panic_hook();
+
+    // TODO implement Display for the arguments
+    tracing::info!("running solver engine with {cli:#?}");
+    run(cli.arguments, cli.command).await;
 }
 
-async fn run() {
+async fn run(arguments: cli::Arguments, command: cli::Command) {
+    let cli::Command::Baseline(baseline) = command;
+
     api::Api {
-        addr: "127.0.0.1:7872".parse().unwrap(),
+        addr: arguments.addr,
+        solver: baseline::Baseline {
+            weth: baseline.weth,
+            base_tokens: baseline.base_tokens.into_iter().collect(),
+            max_hops: baseline.max_hops,
+        },
     }
     .serve(shutdown_signal())
     .await
