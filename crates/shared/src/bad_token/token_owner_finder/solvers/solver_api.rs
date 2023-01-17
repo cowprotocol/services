ba268aebb475178cddb120df20ba5031c7294e82
sunce86 <dusan.stanivukovic@gmail.com>
2023-01-17 17:17:29 +0100

    format

--- crates/shared/src/bad_token/token_owner_finder/solvers/solver_api.rs
+++ crates/shared/src/bad_token/token_owner_finder/solvers/solver_api.rs
@@ -24,7 +24,7 @@ impl SolverConfiguration {
             .await?
             .text()
             .await?;
-        serde_json::from_str(&response).context(format!("bad query response: {}", response))
+        serde_json::from_str(&response).context(format!("bad query response: {:?}", response))
     }
 }
 
