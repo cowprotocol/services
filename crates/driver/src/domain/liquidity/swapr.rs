ee4573a0754261c2cd55fa551449f169483919a9
Nicholas Rodrigues Lordello <nick@cow.fi>
2023-01-18 07:52:52 +0100

    Split Swapr pool into its own module

---
+++ crates/driver/src/domain/liquidity/swapr.rs
@@ -0,0 +1,4 @@
+/// A Unswap V2 like constant product pool, with the notable difference that the
+/// pool fees are dynamic and can be changed by the protocol administrator.
+#[derive(Clone, Debug)]
+pub struct Pool {}
