bcc74d34f636b4b3afa23e74c6d3b5da365fe9b8
Nicholas Rodrigues Lordello <nick@cow.fi>
2023-01-19 20:32:51 +0100

    Introduce Driver Liquidity Domain Objects (#1083)
    This PR introduces "skeleton" types for the `driver` liquidity.
    
    The idea is to wrap the existing `LiquidityCollector` in a boundary
    module, and "baseline source" by "baseline source" introduce wrapping
    logic into the `driver`. As such, each of the liquidity data types are
    intentionally left empty and will be populated once the
    wrapping/conversion logic gets introduced.
    
    For now, I wanted to have this PR to:
    - Come to consensus on the type names and module layout for liquidity
    and liquidity fetching
    - Introduce documentation for each of the supported pool types
    
    In following PRs, I will slowly build these abstractions and fill in
    code and types as needed.
    
    I also proposed a module organization following the suggestion
    [here](https://github.com/cowprotocol/services/pull/1066#pullrequestreview-1251281875)
    - namely, using the names of the actual DEX sources they correspond to
    (i.e. `uniswap::v2::Pool` instead of `constant_product::Pool`). I'm not
    100% convinced about this:
    - I like how it looks and it fits better into my mental model of what
    these domain objects represent
    - HOWEVER, it feels a bit weird to have 4 different DEXs that use
    `uniswap::v2::Pool`:
        - Uniswap V2
        - SushiSwap
        - Honeyswap
        - Baoswap
    
    ### Test Plan
    
    Just types and documentation - so the Rust compiler 🙌.

--- crates/driver/src/domain/liquidity/mod.rs
+++ crates/driver/src/domain/liquidity/mod.rs
@@ -1,7 +1,14 @@
+pub use self::fetcher::Fetcher;
 use crate::domain::eth;
 
+pub mod balancer;
+pub mod fetcher;
+pub mod swapr;
+pub mod uniswap;
+pub mod zeroex;
+
 /// A source of liquidity which can be used by the solver.
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct Liquidity {
     pub id: Id,
     /// Depending on the liquidity provider, this can mean different things.
@@ -9,7 +16,7 @@ pub struct Liquidity {
     pub address: eth::Address,
     /// Estimation of gas needed to use this liquidity on-chain.
     pub gas: eth::Gas,
-    // TODO There will be plenty more data here in the future.
+    pub kind: Kind,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -31,4 +38,19 @@ impl PartialEq<usize> for Id {
     fn eq(&self, other: &usize) -> bool {
         self.0 == *other
     }
+}
+
+/// Data tied to a particular liquidity instance, specific to the kind of
+/// liquidity.
+///
+/// This contains relevant data for encoding interactions for the given
+/// liquidity, as well as state required by the solver engine.
+#[derive(Debug, Clone)]
+pub enum Kind {
+    UnswapV2(uniswap::v2::Pool),
+    UnswapV3(uniswap::v3::Pool),
+    BalancerV2Stable(balancer::stable::Pool),
+    BalancerV2Weighted(balancer::weighted::Pool),
+    Swapr(swapr::Pool),
+    ZeroEx(zeroex::LimitOrder),
 }
