98bcd4163138776f2559c32babbad456b84454aa
Valentin <77051586+vkgnosis@users.noreply.github.com>
2023-01-30 16:53:38 +0100

    Prioritize updates of market order native token prices

--- crates/autopilot/src/solvable_orders.rs
+++ crates/autopilot/src/solvable_orders.rs
@@ -482,6 +482,16 @@ fn get_orders_with_native_prices(
         .flat_map(|(token, price)| to_normalized_price(price).map(|price| (token, price)))
         .collect();
 
+    let high_priority_tokens = orders
+        .iter()
+        .filter_map(|order| match order.metadata.class {
+            OrderClass::Market => Some([order.data.sell_token, order.data.buy_token]),
+            OrderClass::Liquidity => None,
+            OrderClass::Limit(_) => None,
+        })
+        .flatten();
+    native_price_estimator.replace_high_priority(high_priority_tokens.collect());
+
     // Filter both orders and prices so that we only return orders that have prices and prices that
     // have orders.
     let mut filtered_market_orders = 0_i64;
