34cacdea1fa9b5579e3c581a2158f8788af51432
sunce86 <dusan.stanivukovic@gmail.com>
2023-02-07 22:52:16 +0100

    Add pre-interactions support for foreign liquidity orders

--- crates/shared/src/http_solver/model.rs
+++ crates/shared/src/http_solver/model.rs
@@ -7,7 +7,7 @@ use {
     ethcontract::{Bytes, H160},
     model::{
         auction::AuctionId,
-        order::{OrderData, OrderUid},
+        order::{Interactions, OrderData, OrderUid},
         ratio_as_decimal,
         signature::Signature,
         u256_decimal::{self, DecimalU256},
@@ -258,6 +258,7 @@ pub struct NativeLiquidityOrder {
     pub data: OrderData,
     #[serde(flatten)]
     pub signature: Signature,
+    pub interactions: Option<Interactions>,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -977,6 +978,7 @@ mod tests {
                         ..Default::default()
                     },
                     signature: Signature::Eip1271(vec![1, 2, 3, 4]),
+                    interactions: None,
                 },
                 exec_sell_amount: 50.into(),
                 exec_buy_amount: 51.into(),
