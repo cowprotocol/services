e42ecdecdc65797d6636e56023db0e53a69c3a3c
sunce86 <dusan.stanivukovic@gmail.com>
2023-02-14 16:22:34 +0100

    Implement auction_prices

--- crates/database/src/auction_prices.rs
+++ crates/database/src/auction_prices.rs
@@ -1,1 +1,47 @@
+use {
+    crate::{auction::AuctionId, Address},
+    bigdecimal::BigDecimal,
+    sqlx::PgConnection,
+};
 
+/// External prices for a given auction.
+#[derive(Debug, PartialEq, sqlx::FromRow)]
+pub struct Prices {
+    pub auction_id: AuctionId,
+    pub token: Address,
+    pub price: BigDecimal,
+}
+
+pub async fn insert(
+    ex: &mut PgConnection,
+    auction_id: AuctionId,
+    token: Address,
+    price: BigDecimal,
+) -> Result<(), sqlx::Error> {
+    const QUERY: &str =
+        r#"INSERT INTO auction_prices (auction_id, token, price) VALUES ($1, $2, $3);"#;
+    sqlx::query(QUERY)
+        .bind(auction_id)
+        .bind(token)
+        .bind(price)
+        .execute(ex)
+        .await?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, crate::byte_array::ByteArray, sqlx::Connection};
+
+    #[tokio::test]
+    #[ignore]
+    async fn postgres_roundtrip() {
+        let mut db = PgConnection::connect("postgresql://").await.unwrap();
+        let mut db = db.begin().await.unwrap();
+        crate::clear_DANGER_(&mut db).await.unwrap();
+
+        insert(&mut db, 1, ByteArray([2; 20]), 3.into())
+            .await
+            .unwrap();
+    }
+}
