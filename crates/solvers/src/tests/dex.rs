743e7fd6a8a749e5ff48b06e578b087476b1653a
Nicholas Rodrigues Lordello <nick@cow.fi>
2023-02-20 11:09:33 +0100

    comments

--- crates/solvers/src/tests/dex.rs
+++ crates/solvers/src/tests/dex.rs
@@ -59,7 +59,6 @@ pub async fn setup(expectations: Vec<Expectation>) -> SocketAddr {
     let server = axum::Server::bind(&"0.0.0.0:0".parse().unwrap()).serve(app.into_make_service());
     let addr = server.local_addr();
     tokio::spawn(async move { server.await.unwrap() });
-    tokio::time::sleep(tokio::time::Duration::from_millis(1_000)).await;
     addr
 }
 
@@ -67,18 +66,14 @@ pub async fn setup(expectations: Vec<Expectation>) -> SocketAddr {
 struct State(Arc<Mutex<Vec<Expectation>>>);
 
 fn get(state: State, path: Option<String>, query: Option<String>) -> serde_json::Value {
-    let mut state = state.0.lock().unwrap();
-    assert!(
-        !state.is_empty(),
-        "got another GET request, but didn't expect any more"
-    );
-
-    let full_path = full_path(path, query);
-    let (expected_path, res) = match state.pop().unwrap() {
-        Expectation::Get { path, res } => (path, res),
-        other => panic!("expected GET request but got {other:?}"),
+    let expectation = state.0.lock().unwrap().pop();
+    let (expected_path, res) = match expectation {
+        Some(Expectation::Get { path, res }) => (path, res),
+        Some(other) => panic!("expected GET request but got {other:?}"),
+        None => panic!("got another GET request, but didn't expect any more"),
     };
 
+    let full_path = full_path(path, query);
     assert_eq!(full_path, expected_path, "GET request has unexpected path");
     res
 }
@@ -89,18 +84,14 @@ fn post(
     query: Option<String>,
     req: serde_json::Value,
 ) -> serde_json::Value {
-    let mut state = state.0.lock().unwrap();
-    assert!(
-        !state.is_empty(),
-        "got another POST request, but didn't expect any more"
-    );
-
-    let full_path = full_path(path, query);
-    let (expected_path, expected_req, res) = match state.pop().unwrap() {
-        Expectation::Post { path, req, res } => (path, req, res),
-        other => panic!("expected POST request but got {other:?}"),
+    let expectation = state.0.lock().unwrap().pop();
+    let (expected_path, expected_req, res) = match expectation {
+        Some(Expectation::Post { path, req, res }) => (path, req, res),
+        Some(other) => panic!("expected POST request but got {other:?}"),
+        None => panic!("got another POST request, but didn't expect any more"),
     };
 
+    let full_path = full_path(path, query);
     assert_eq!(full_path, expected_path, "POST request has unexpected path");
     assert_eq!(req, expected_req, "POST request has unexpected body");
     res
