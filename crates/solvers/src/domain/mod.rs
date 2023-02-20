c0ea29182d421a2e705c47f12757b3198f49147c
Martin Beckmann <martin.beckmann@gnosis.pm>
2023-02-20 13:20:55 +0100

    Add comment about returning list of solutions.

--- crates/solvers/src/domain/mod.rs
+++ crates/solvers/src/domain/mod.rs
@@ -14,6 +14,9 @@ pub enum Solver {
 }
 
 impl Solver {
+    /// Solves a given auction and returns multiple solutions. We allow
+    /// returning multiple solutions to later merge multiple non-overlapping
+    /// solutions to get one big more gas efficient solution.
     pub async fn solve(&self, auction: auction::Auction) -> Vec<solution::Solution> {
         match self {
             Solver::Baseline(solver) => solver.solve(auction),
