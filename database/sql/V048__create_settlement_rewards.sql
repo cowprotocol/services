819a45d137990fb9e212a875d0731303bf648c69
sunce86 <dusan.stanivukovic@gmail.com>
2023-02-24 13:47:27 +0100

    comment for reference score

--- database/sql/V048__create_settlement_rewards.sql
+++ database/sql/V048__create_settlement_rewards.sql
@@ -3,6 +3,7 @@
 CREATE TABLE settlement_scores (
   auction_id bigint PRIMARY KEY,
   winning_score numeric(78,0) NOT NULL,
+  -- The score from a runner-up solver, if there is one, otherwise zero.
   reference_score numeric(78,0) NOT NULL,
   -- winning solver has the obligation to settle the transaction onchain before the deadline
   block_deadline bigint NOT NULL
