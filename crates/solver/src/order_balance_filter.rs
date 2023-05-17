ea44a18d91ffd3e13a62da9ee114ec8345e31cf8
Valentin <77051586+vkgnosis@users.noreply.github.com>
2023-05-17 10:20:14 +0200

    Return remaining balance back out (#1511)
    We are going to need to assign balances for FOK orders while letting
    solvers assign balances for partially fillable orders. To do this, we
    need to know what balances are remaining after assigning FOK orders.
    
    I also simplify the function slightly so that it iterates over all
    orders in one loop without grouping them by Query first.

--- crates/solver/src/order_balance_filter.rs
+++ crates/solver/src/order_balance_filter.rs
@@ -34,6 +34,8 @@ impl BalancedOrder {
 }
 
 /// Fetch the balances for `balance_orders`.
+///
+/// If a balance can't be fetched, it isn't part of the result.
 pub async fn fetch_balances(
     fetcher: &dyn BalanceFetching,
     orders: &[Order],
@@ -57,71 +59,68 @@ pub fn max_balance(orders: &[Order]) -> HashMap<Query, U256> {
 }
 
 /// See the `BalancedOrder` documentation.
+///
+/// After the call, `balances` contains the remaining unassigned balances.
 pub fn balance_orders(
     mut orders: Vec<Order>,
-    balances: &HashMap<Query, U256>,
+    balances: &mut HashMap<Query, U256>,
     ethflow_contract: Option<H160>,
 ) -> Vec<BalancedOrder> {
-    let mut orders_map = HashMap::<Query, Vec<Order>>::new();
     orders.sort_by_key(|order| std::cmp::Reverse(order.metadata.creation_date));
-    for order in orders {
-        let key = Query::from_order(&order);
-        orders_map.entry(key).or_default().push(order);
-    }
 
     let mut result: Vec<BalancedOrder> = Vec::new();
-    for (key, orders) in orders_map {
-        let mut remaining_balance = match balances.get(&key) {
-            Some(balance) => *balance,
+    for order in orders {
+        let key = Query::from_order(&order);
+        let remaining_balance = match balances.get_mut(&key) {
+            Some(balance) => balance,
             None => continue,
         };
-        for order in orders {
-            // For ethflow orders, there is no need to check the balance. The contract
-            // ensures that there will always be sufficient balance, after the wrapAll
-            // pre_interaction has been called.
-            if Some(order.metadata.owner) == ethflow_contract {
-                result.push(BalancedOrder::full(order));
+
+        // For ethflow orders, there is no need to check the balance. The contract
+        // ensures that there will always be sufficient balance, after the wrapAll
+        // pre_interaction has been called.
+        if Some(order.metadata.owner) == ethflow_contract {
+            result.push(BalancedOrder::full(order));
+            continue;
+        }
+
+        let needed_balance = match max_transfer_out_amount(&order) {
+            // Should only ever happen if a partially fillable order has been filled completely
+            Ok(balance) if balance.is_zero() => continue,
+            Ok(balance) => balance,
+            Err(err) => {
+                // This should only happen if we read bogus order data from
+                // the database (either we allowed a bogus order to be
+                // created or we updated a good order incorrectly), so raise
+                // the alarm!
+                tracing::error!(
+                    ?err,
+                    ?order,
+                    "error computing order max transfer out amount"
+                );
                 continue;
             }
+        };
 
-            let needed_balance = match max_transfer_out_amount(&order) {
-                // Should only ever happen if a partially fillable order has been filled completely
-                Ok(balance) if balance.is_zero() => continue,
-                Ok(balance) => balance,
-                Err(err) => {
-                    // This should only happen if we read bogus order data from
-                    // the database (either we allowed a bogus order to be
-                    // created or we updated a good order incorrectly), so raise
-                    // the alarm!
-                    tracing::error!(
-                        ?err,
-                        ?order,
-                        "error computing order max transfer out amount"
-                    );
-                    continue;
-                }
-            };
-
-            if order.data.partially_fillable {
-                if remaining_balance == 0.into() {
-                    continue;
-                }
-                result.push(BalancedOrder {
-                    order,
-                    available_sell_token_balance: needed_balance.min(remaining_balance),
-                });
-                remaining_balance = remaining_balance.saturating_sub(needed_balance);
-            } else {
-                match remaining_balance.checked_sub(needed_balance) {
-                    Some(balance) => {
-                        result.push(BalancedOrder::full(order));
-                        remaining_balance = balance;
-                    }
-                    None => continue,
-                };
+        if order.data.partially_fillable {
+            let balance_for_order = std::cmp::min(needed_balance, *remaining_balance);
+            if balance_for_order == 0.into() {
+                continue;
             }
+            result.push(BalancedOrder {
+                order,
+                available_sell_token_balance: balance_for_order,
+            });
+            *remaining_balance -= balance_for_order;
+        } else {
+            if needed_balance > *remaining_balance {
+                continue;
+            }
+            result.push(BalancedOrder::full(order));
+            *remaining_balance -= needed_balance;
         }
     }
+
     result
 }
 
@@ -191,22 +190,56 @@ mod tests {
             },
         ];
 
-        let balances = hashmap! {Query::from_order(&orders[0]) => U256::from(9)};
-        let orders_ = balance_orders(orders.clone(), &balances, None);
+        let mut balances = hashmap! {Query::from_order(&orders[0]) => U256::from(9)};
+        let orders_ = balance_orders(orders.clone(), &mut balances, None);
         assert_eq!(orders_.len(), 2);
         // Second order has lower timestamp so it isn't picked.
         assert_eq!(orders_[0].order.data, orders[0].data);
         // Third order is partially fillable so is picked with remaining balance.
         assert_eq!(orders_[1].order.data, orders[2].data);
         assert_eq!(orders_[1].available_sell_token_balance, 3.into());
+        assert_eq!(
+            balances.get(&Query::from_order(&orders[0])),
+            Some(&0.into())
+        );
 
         orders[1].metadata.creation_date = Utc.timestamp_opt(3, 0).unwrap();
-        let orders_ = balance_orders(orders.clone(), &balances, None);
+        let mut balances = hashmap! {Query::from_order(&orders[0]) => U256::from(9)};
+        let orders_ = balance_orders(orders.clone(), &mut balances, None);
         assert_eq!(orders_.len(), 2);
         assert_eq!(orders_[0].order.data, orders[1].data);
         // Remaining balance is different because previous order has changed.
         assert_eq!(orders_[1].order.data, orders[2].data);
         assert_eq!(orders_[1].available_sell_token_balance, 5.into());
+        assert_eq!(
+            balances.get(&Query::from_order(&orders[0])),
+            Some(&0.into())
+        );
+    }
+
+    #[tokio::test]
+    async fn records_remaining_balance() {
+        let orders = vec![Order {
+            data: OrderData {
+                sell_amount: 3.into(),
+                fee_amount: 3.into(),
+                ..Default::default()
+            },
+            metadata: OrderMetadata {
+                creation_date: Utc.timestamp_opt(2, 0).unwrap(),
+                ..Default::default()
+            },
+            ..Default::default()
+        }];
+
+        let mut balances = hashmap! {Query::from_order(&orders[0]) => U256::from(9)};
+        let orders_ = balance_orders(orders.clone(), &mut balances, None);
+        // 6 balance assigned to order, 3 remaining
+        assert_eq!(orders_.len(), 1);
+        assert_eq!(
+            balances.get(&Query::from_order(&orders[0])),
+            Some(&3.into())
+        );
     }
 
     #[tokio::test]
@@ -226,8 +259,8 @@ mod tests {
             ..Default::default()
         }];
 
-        let balances = hashmap! {Query::from_order(&orders[0]) => U256::from(0)};
-        let orders_ = balance_orders(orders.clone(), &balances, Some(ethflow_address));
+        let mut balances = hashmap! {Query::from_order(&orders[0]) => U256::from(0)};
+        let orders_ = balance_orders(orders.clone(), &mut balances, Some(ethflow_address));
         assert_eq!(orders_.len(), 1);
         assert_eq!(orders_[0].order, orders[0]);
     }
@@ -273,9 +306,9 @@ mod tests {
             },
         ];
 
-        let balances = hashmap! {Query::from_order(&orders[0]) => U256::MAX};
+        let mut balances = hashmap! {Query::from_order(&orders[0]) => U256::MAX};
         let expected_result = vec![orders[0].clone(), orders[1].clone()];
-        let mut filtered_orders = balance_orders(orders, &balances, None);
+        let mut filtered_orders = balance_orders(orders, &mut balances, None);
         // Deal with `solvable_orders()` sorting the orders.
         filtered_orders.sort_by_key(|order| order.order.metadata.creation_date);
         assert_eq!(expected_result.len(), filtered_orders.len());
