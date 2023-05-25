f7ade991da91d267cfdcc7031531ee24392397b0
cowfee <cowfeegit@gmail.com>
2023-05-25 12:01:37 +0200

    debug formatting nit

--- crates/solvers/src/domain/order.rs
+++ crates/solvers/src/domain/order.rs
@@ -202,5 +202,13 @@ impl EcdsaSignature {
 /// This is a hash allowing arbitrary user data to be associated with an order.
 /// While this type holds the hash, the data itself is uploaded to IPFS. This
 /// hash is signed along with the order.
-#[derive(Clone, Copy, Debug, Default)]
+#[derive(Clone, Copy, Default)]
 pub struct AppData(pub [u8; 32]);
+
+impl Debug for AppData {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("AppData")
+            .field(&util::fmt::Hex(&self.0))
+            .finish()
+    }
+}
