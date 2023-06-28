800f4570063b82c1c7633e4ca189753dbc649b27
Martin Beckmann <martin.beckmann@gnosis.pm>
2023-06-28 09:44:10 +0200

    Uppercase SQL keywords and builtin functions

--- crates/orderbook/src/database/total_surplus.rs
+++ crates/orderbook/src/database/total_surplus.rs
@@ -9,47 +9,46 @@ use {
 /// and **NOT** quoted price) since march 2023.
 async fn fetch_total_surplus(ex: &mut PgConnection, user: &Address) -> Result<f64, sqlx::Error> {
     const TOTAL_SURPLUS_QUERY: &str = r#"
-with trade_components as (
-    select
-       case kind
+WITH trade_components AS (
+    SELECT
+       CASE kind
           -- so much was actually bought
-          when 'sell' then t.buy_amount
+          WHEN 'sell' THEN t.buy_amount
           -- so much was actually converted to buy tokens
-          when 'buy' then t.sell_amount - t.fee_amount
-       end as trade_amount,
-       case kind
+          WHEN 'buy' THEN t.sell_amount - t.fee_amount
+       END AS trade_amount,
+       CASE kind
           -- so much had to be bought at least (given exeucted amount and limit price)
-          when 'sell' then (t.sell_amount - t.fee_amount) * o.buy_amount / o.sell_amount
+          WHEN 'sell' THEN (t.sell_amount - t.fee_amount) * o.buy_amount / o.sell_amount
           -- so much could be converted to buy_token at most (given executed amount and limit price)
-          when 'buy' then t.buy_amount * o.sell_amount / o.buy_amount
-       end as limit_amount,
+          WHEN 'buy' THEN t.buy_amount * o.sell_amount / o.buy_amount
+       END AS limit_amount,
        o.kind,
-       (select price from auction_prices ap where ap.token = o.sell_token and ap.auction_id = oe.auction_id) as native_sell_price,
-       (select price from auction_prices ap where ap.token = o.buy_token and ap.auction_id = oe.auction_id) as native_buy_price
-    from orders o
-    join trades t on o.uid = t.order_uid
-    join order_quotes oq on o.uid = oq.order_uid
-    left join onchain_placed_orders opo on opo.uid = o.uid
-    join order_execution oe on o.uid = oe.order_uid
-    where
+       (SELECT price FROM auction_prices ap WHERE ap.token = o.sell_token AND ap.auction_id = oe.auction_id) AS native_sell_price,
+       (SELECT price FROM auction_prices ap WHERE ap.token = o.buy_token AND ap.auction_id = oe.auction_id) AS native_buy_price
+    FROM orders o
+    JOIN trades t ON o.uid = t.order_uid
+    LEFT JOIN onchain_placed_orders opo ON opo.uid = o.uid
+    JOIN order_execution oe ON o.uid = oe.order_uid
+    WHERE
         o.owner = $1
         or opo.sender = $1
 ),
-trade_surplus as (
-    select
-        case kind
+trade_surplus AS (
+    SELECT
+        CASE kind
             -- amounts refer to tokens bought; more is better
-            when 'sell' then (trade_amount - limit_amount) * native_buy_price
+            WHEN 'sell' THEN (trade_amount - limit_amount) * native_buy_price
             -- amounts refer to tokens sold; less is better
-            when 'buy' then (limit_amount - trade_amount) * native_sell_price
-        end / power(10, 18) as surplus_in_wei,
+            WHEN 'buy' THEN (limit_amount - trade_amount) * native_sell_price
+        END / POWER(10, 18) AS surplus_in_wei,
         limit_amount,
         trade_amount
-    from trade_components
+    FROM trade_components
 )
-select
-   sum(surplus_in_wei) as total_surplus_in_wei
-from trade_surplus
+SELECT
+   SUM(surplus_in_wei) AS total_surplus_in_wei
+FROM trade_surplus
 "#;
 
     sqlx::query_scalar(TOTAL_SURPLUS_QUERY)
