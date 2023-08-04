350f81c34079a8fc5120396d9d1a841e7df6e6a4
Martin Beckmann <martin.beckmann@protonmail.com>
2023-08-04 10:51:24 +0200

    Make simulation reentrancy guard less fragile (#1735)

--- crates/contracts/solidity/Trader.sol
+++ crates/contracts/solidity/Trader.sol
@@ -20,9 +20,31 @@ contract Trader {
     /// the trader is actually a smart contract.
     address constant private TRADER_IMPL = address(0x10000);
 
-    /// @dev Flag that ensures that `prepareSwap` gets called exactly once to
-    /// prevent custom interaction from calling it.
-    bool private _alreadyCalled = false;
+    /// @dev Storage slot where we store the flag whether `prepareSwap` has
+    /// already been called to implement a reentrancy guard that does not rely
+    /// on node implementation specific behavior.
+    /// Note that we subtract 1 from the hash so that their is no known
+    /// pre-image, so hash collisions are not possible.
+    bytes32 constant private ALREADY_CALLED_SLOT =
+        bytes32(uint256(keccak256("Trader.alreadyCalled")) - 1);
+
+    // Intuitively one would store a flag whether or not `prepareSwap()` has
+    // been called before inside a mutable member veriable bool. However, this
+    // bool can not be reliably initialized as this would require running a
+    // constructor which would be annoying to do as that would require a multi
+    // trace call.
+    // Instead we store the flag inside a storage slot. The EVM spec says
+    // that loading from uninitialized storage results in a `0` byte which is
+    // equal to `false`.
+    /// @dev Returns the value that is currently in storage at `ALREADY_CALLED_SLOT`
+    /// and sets that storage to true to indicate that the function has been called.
+    function alreadyCalled() private returns (bool value) {
+        bytes32 slot = ALREADY_CALLED_SLOT;
+        assembly {
+            value := sload(slot)
+            sstore(slot, 1)
+        }
+    }
 
     // The `Trader` contract gets deployed on the `from` address of the quote.
     // Since the `from` address might be a safe or other smart contract we still
@@ -52,8 +74,7 @@ contract Trader {
         address nativeToken,
         address payable receiver
     ) external {
-        require(!_alreadyCalled, "prepareSwap can only be called once");
-        _alreadyCalled = true;
+        require(!alreadyCalled(), "prepareSwap can only be called once");
 
         if (sellToken == nativeToken) {
             uint256 availableBalance = IERC20(sellToken).balanceOf(address(this));
