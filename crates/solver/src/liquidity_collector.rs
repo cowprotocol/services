3dcb5aab44a43ce2f073ba78479eec2d29273018
Martin Beckmann <martin.beckmann@gnosis.pm>
2023-08-15 09:34:09 +0200

    Init liquidity in background

--- crates/solver/src/liquidity_collector.rs
+++ crates/solver/src/liquidity_collector.rs
@@ -3,7 +3,8 @@ use {
     anyhow::Result,
     model::TokenPair,
     shared::{baseline_solver::BaseTokens, recent_block_cache::Block},
-    std::{collections::HashSet, sync::Arc},
+    std::{collections::HashSet, future::Future, sync::Arc, time::Duration},
+    tokio::sync::Mutex,
 };
 
 #[mockall::automock]
@@ -41,5 +42,71 @@ impl LiquidityCollecting for LiquidityCollector {
             .collect();
         tracing::debug!("got {} AMMs", amms.len());
         Ok(amms)
+    }
+}
+
+/// A liquidity source which might not be initialised on creation. Instead
+/// initialisation gets retried in a background task over and over until it
+/// succeeds. Until the liquidity source has been initialised no liquidity will
+/// be provided.
+pub struct BackgroundInitLiquiditySource<L> {
+    liquidity_source: Arc<Mutex<Option<L>>>,
+}
+
+impl<L> BackgroundInitLiquiditySource<L> {
+    /// Creates a new liquidity source which might only be initialized at a
+    /// later point in time.
+    pub fn new<I, F>(init: I, retry_init_timeout: Duration) -> Self
+    where
+        I: Fn() -> F + Send + Sync + 'static,
+        F: Future<Output = Result<L>> + Send,
+        L: LiquidityCollecting + 'static,
+    {
+        let liquidity_source: Arc<Mutex<Option<L>>> = Default::default();
+        let inner = liquidity_source.clone();
+        tokio::task::spawn(async move {
+            loop {
+                match init().await {
+                    Err(err) => {
+                        tracing::warn!(
+                            "failed to initialise liquidity source; next init attempt in \
+                             {retry_init_timeout:?}: {err:?}"
+                        );
+                        tokio::time::sleep(retry_init_timeout).await;
+                    }
+                    Ok(source) => {
+                        let _ = inner.lock().await.insert(source);
+                        tracing::debug!("successfully initialised liquidity source");
+                        break;
+                    }
+                }
+            }
+        });
+
+        Self { liquidity_source }
+    }
+}
+
+#[async_trait::async_trait]
+impl<L> LiquidityCollecting for BackgroundInitLiquiditySource<L>
+where
+    L: LiquidityCollecting,
+{
+    async fn get_liquidity(
+        &self,
+        pairs: HashSet<TokenPair>,
+        at_block: Block,
+    ) -> Result<Vec<Liquidity>> {
+        // Use `try_lock` to not block caller when the lock is currently being held for
+        // a potentially very slow init logic.
+        let liquidity_source = match self.liquidity_source.try_lock() {
+            Ok(lock) => lock,
+            Err(_) => return Ok(vec![]),
+        };
+
+        match &*liquidity_source {
+            Some(initialised_source) => initialised_source.get_liquidity(pairs, at_block).await,
+            None => Ok(vec![]),
+        }
     }
 }
