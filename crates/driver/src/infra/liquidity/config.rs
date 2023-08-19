fd25e6b66e153501599665ac91f4243aed9b3920
sunce86 <dusan.stanivukovic@gmail.com>
2023-08-19 16:13:27 +0200

    keep nice things

--- crates/driver/src/infra/liquidity/config.rs
+++ crates/driver/src/infra/liquidity/config.rs
@@ -115,7 +115,7 @@ pub struct UniswapV3 {
     pub router: eth::ContractAddress,
 
     /// How many pools should be initialized during start up.
-    pub max_pools_to_initialize: u64,
+    pub max_pools_to_initialize: usize,
 }
 
 impl UniswapV3 {
