50f24c953c49e7477b1feef1f8dd5c779161d0f3
sunce86 <dusan.stanivukovic@gmail.com>
2023-08-28 17:15:51 +0200

    Fix inflight orders log

--- crates/solver/src/driver.rs
+++ crates/solver/src/driver.rs
@@ -265,16 +265,14 @@ impl Driver {
 
         let before_count = auction.orders.len();
         let inflight_order_uids = self.in_flight_orders.update_and_filter(&mut auction);
-        if before_count != auction.orders.len() {
-            tracing::debug!(
-                "reduced {} orders to {} because in flight at last seen block {}, orders in \
-                 flight: {:?}",
-                before_count,
-                auction.orders.len(),
-                auction.latest_settlement_block,
-                inflight_order_uids
-            );
-        }
+
+        tracing::debug!(
+            "before orders {} after orders {} last seen block {}, orders in flight: {:?}",
+            before_count,
+            auction.orders.len(),
+            auction.latest_settlement_block,
+            inflight_order_uids
+        );
 
         let auction_start_block = auction.block;
         let competition_auction = CompetitionAuction {
