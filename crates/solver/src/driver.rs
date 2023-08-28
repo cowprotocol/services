ef22debef343b02d5e48155e808bb74d03b010c1
sunce86 <dusan.stanivukovic@gmail.com>
2023-08-28 20:02:16 +0200

    clippy fix

--- crates/solver/src/driver.rs
+++ crates/solver/src/driver.rs
@@ -263,8 +263,7 @@ impl Driver {
 
         let current_block_during_liquidity_fetch = self.block_stream.borrow().number;
 
-        let before_count = auction.orders.len();
-        let inflight_order_uids = self.in_flight_orders.update_and_filter(&mut auction);
+        self.in_flight_orders.update_and_filter(&mut auction);
 
         let auction_start_block = auction.block;
         let competition_auction = CompetitionAuction {
