842b4c9e6cd67bddd94597ab5284f754585d5e12
mfw78 <mfw78@protonmail.com>
2023-08-31 17:53:14 +0000

    chore(e2e): anvil rpc calls

--- crates/e2e/src/setup/colocation.rs
+++ crates/e2e/src/setup/colocation.rs
@@ -38,7 +38,7 @@ pub fn start_driver(
 ) -> JoinHandle<()> {
     let config_file = config_tmp_file(format!(
         r#"
-# CI e2e tests run with hardhat, which doesn't support access lists.
+# CI e2e tests run with anvil, which does support access lists.
 disable-access-list-simulation = true
 
 [contracts]
