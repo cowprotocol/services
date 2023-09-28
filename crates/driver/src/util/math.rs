43aede4515bf55c990d0d7f2d7180da82537fb0e
Nicholas Rodrigues Lordello <nick@cow.fi>
2023-09-28 11:54:34 +0200

    suggestion from Martin

--- crates/driver/src/util/math.rs
+++ crates/driver/src/util/math.rs
@@ -14,5 +14,5 @@ pub fn mul_ratio_ceil(x: U256, q: U256, d: U256) -> Option<U256> {
     let p = x.full_mul(q);
     let result = U256::try_from(p.checked_div(d.into())?).ok()?;
     let round_up = !p.checked_rem(d.into())?.is_zero();
-    result.checked_add((round_up as u8).into())
+    result.checked_add(u8::from(round_up).into())
 }
