28d3773c6b990b8d11b11483a8734f60cc468320
sunce86 <dusan.stanivukovic@gmail.com>
2023-10-02 12:49:57 +0200

    fix driver tests

--- crates/driver/src/run.rs
+++ crates/driver/src/run.rs
@@ -122,7 +122,7 @@ async fn ethrpc(args: &cli::Args) -> blockchain::Rpc {
 
 async fn ethereum(config: &infra::Config, ethrpc: blockchain::Rpc) -> Ethereum {
     let gas = Arc::new(
-        blockchain::gas::GasPriceEstimator::new(ethrpc.web3(), &config.mempools)
+        blockchain::GasPriceEstimator::new(ethrpc.web3(), &config.mempools)
             .await
             .expect("initialize gas price estimator"),
     );
