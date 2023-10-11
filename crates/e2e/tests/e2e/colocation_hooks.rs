0c7e612dbd3eb4cf1fcb47c4b6851e200b907e74
sunce86 <dusan.stanivukovic@gmail.com>
2023-10-12 00:27:58 +0200

    fix local node tests

--- crates/e2e/tests/e2e/colocation_hooks.rs
+++ crates/e2e/tests/e2e/colocation_hooks.rs
@@ -71,7 +71,7 @@ async fn allowance(web3: Web3) {
     let services = Services::new(onchain.contracts()).await;
     services.start_autopilot(vec![
         "--enable-colocation=true".to_string(),
-        "--drivers=http://localhost:11088/test_solver".to_string(),
+        "--drivers=test_solver|http://localhost:11088/test_solver".to_string(),
     ]);
     services
         .start_api(vec!["--enable-custom-interactions=true".to_string()])
@@ -249,7 +249,7 @@ async fn signature(web3: Web3) {
     let services = Services::new(onchain.contracts()).await;
     services.start_autopilot(vec![
         "--enable-colocation=true".to_string(),
-        "--drivers=http://localhost:11088/test_solver".to_string(),
+        "--drivers=test_solver|http://localhost:11088/test_solver".to_string(),
     ]);
     services
         .start_api(vec!["--enable-custom-interactions=true".to_string()])
@@ -356,7 +356,7 @@ async fn partial_fills(web3: Web3) {
     let services = Services::new(onchain.contracts()).await;
     services.start_autopilot(vec![
         "--enable-colocation=true".to_string(),
-        "--drivers=http://localhost:11088/test_solver".to_string(),
+        "--drivers=test_solver|http://localhost:11088/test_solver".to_string(),
     ]);
     services
         .start_api(vec!["--enable-custom-interactions=true".to_string()])
