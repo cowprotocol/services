43cbffc92394458d836a984ed2c4ff937ea431ce
sunce86 <dusan.stanivukovic@gmail.com>
2023-10-17 17:43:20 +0200

    encoding failed

--- crates/driver/src/domain/competition/mod.rs
+++ crates/driver/src/domain/competition/mod.rs
@@ -94,8 +94,7 @@ impl Competition {
                 result
                     .tap_err(|err| {
                         observe::encoding_failed(self.solver.name(), id, err);
-                        //notify::encoding_failed(&self.solver, auction.id(),
-                        // id);
+                        notify::encoding_failed(&self.solver, auction.id(), id, err);
                     })
                     .ok()
             })
