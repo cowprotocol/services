3850103cf68d0f7ca53ca7f5f100f85a7d43252d
sunce86 <dusan.stanivukovic@gmail.com>
2023-10-19 21:50:47 +0200

    fix driver test 3

--- crates/driver/src/tests/cases/score_competition.rs
+++ crates/driver/src/tests/cases/score_competition.rs
@@ -11,14 +11,14 @@ async fn solver_score_winner() {
     let test = setup()
         .pool(ab_pool())
         .order(ab_order())
-        .solution(ab_solution().score(Score::Solver(2992421280589416499u128.into()))) // not higher than objective value
+        .solution(ab_solution().score(Score::Solver(2902421280589416499u128.into()))) // not higher than objective value
         .solution(ab_solution().score(Score::RiskAdjusted(0.4)))
         .done()
         .await;
 
     assert_eq!(
         test.solve().await.ok().score(),
-        2992421280589416499u128.into()
+        2902421280589416499u128.into()
     );
     test.reveal().await.ok().orders(&[ab_order().name]);
 }
