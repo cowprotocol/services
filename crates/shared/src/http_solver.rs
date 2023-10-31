c973fa765ace41dfffa3a79878a9ad8d30797916
Martin Beckmann <martin.beckmann@gnosis.pm>
2023-10-31 15:48:22 +0100

    build /notify URL respecting multiple paths

--- crates/shared/src/http_solver.rs
+++ crates/shared/src/http_solver.rs
@@ -230,7 +230,9 @@ impl HttpSolverApi for DefaultHttpSolverApi {
     }
 
     fn notify_auction_result(&self, auction_id: AuctionId, result: model::AuctionResult) {
-        let mut url = crate::url::join(&self.base, "notify");
+        let mut url = crate::url::join(&self.base, &self.solve_path);
+        // `/notify` should be a sibling of the `/solve` endpoint
+        url.path_segments_mut().unwrap().pop().push("notify");
 
         let client = self.client.clone();
         let config_api_key = self.config.api_key.clone();
