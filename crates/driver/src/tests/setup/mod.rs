eec7c06cb269857fad82f5bf8e3e64ec70f13f53
sunce86 <dusan.stanivukovic@gmail.com>
2023-11-16 17:53:44 +0100

    increase deadline a bit

--- crates/driver/src/tests/setup/mod.rs
+++ crates/driver/src/tests/setup/mod.rs
@@ -645,7 +645,7 @@ impl Setup {
     }
 
     fn deadline(&self) -> chrono::DateTime<chrono::Utc> {
-        time::now() + chrono::Duration::seconds(2)
+        time::now() + chrono::Duration::seconds(3)
     }
 }
 
