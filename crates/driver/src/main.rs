1c3e2874511c988384790277257081bc0d0072ec
Felix Leupold <felixleupold90@gmail.com>
2023-11-28 09:27:46 +0100

    gprof wip

--- crates/driver/src/main.rs
+++ crates/driver/src/main.rs
@@ -1,4 +1,12 @@
+use gperftools::heap_profiler::HEAP_PROFILER;
+
 #[tokio::main]
 async fn main() {
+    HEAP_PROFILER
+        .lock()
+        .unwrap()
+        .start("/Users/felixleupold/Gnosis/gp-v2-services/my-prof.hprof")
+        .unwrap();
     driver::start(std::env::args()).await;
+    HEAP_PROFILER.lock().unwrap().stop().unwrap();
 }
