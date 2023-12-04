d2ac47e1ad411be5d89a019e831e24f194e1af27
sunce86 <dusan.stanivukovic@gmail.com>
2023-12-04 22:01:44 +0100

    small fixes

--- crates/database/src/fee_policies.rs
+++ crates/database/src/fee_policies.rs
@@ -1,5 +1,5 @@
 use {
-    crate::{auction::AuctionId, Address, PgTransaction},
+    crate::{auction::AuctionId, OrderUid, PgTransaction},
     bigdecimal::BigDecimal,
     sqlx::PgConnection,
     std::ops::DerefMut,
@@ -9,20 +9,23 @@ use {
 pub struct FeePolicies {
     pub auction_id: AuctionId,
     pub order_uid: OrderUid,
-    pub quote_deviation_factor: Vec<f64>,
+    pub price_improvement_factor: Vec<f64>,
     pub volume_factor: Vec<f64>,
-    pub absolute_fee: Vec<BigDecimal>, 
+    pub absolute_fee: Vec<BigDecimal>,
 }
 
-pub async fn insert(ex: &mut PgTransaction<'_>, fee_policies: FeePolicies) -> Result<(), sqlx::Error> {
+pub async fn insert(
+    ex: &mut PgTransaction<'_>,
+    fee_policies: FeePolicies,
+) -> Result<(), sqlx::Error> {
     const QUERY: &str = r#"
-        INSERT INTO fee_policies (auction_id, order_uid, quote_deviation_factor, volume_factor, absolute_fee)
+        INSERT INTO fee_policies (auction_id, order_uid, price_improvement_factor, volume_factor, absolute_fee)
         VALUES ($1, $2, $3, $4, $5)
     "#;
     sqlx::query(QUERY)
         .bind(fee_policies.auction_id)
         .bind(fee_policies.order_uid)
-        .bind(fee_policies.quote_deviation_factor)
+        .bind(fee_policies.price_improvement_factor)
         .bind(fee_policies.volume_factor)
         .bind(fee_policies.absolute_fee)
         .execute(ex.deref_mut())
@@ -34,13 +37,13 @@ pub async fn fetch(
     ex: &mut PgConnection,
     auction_id: AuctionId,
     order_uid: OrderUid,
-) -> Result<Option<FeePolicy>, sqlx::Error> {
+) -> Result<Option<FeePolicies>, sqlx::Error> {
     const QUERY: &str = r#"
-        SELECT auction_id, order_uid, quote_deviation_factor, volume_factor, absolute_fee
+        SELECT auction_id, order_uid, price_improvement_factor, volume_factor, absolute_fee
         FROM fee_policies
         WHERE auction_id = $1 AND order_uid = $2
     "#;
-    let fee_policy = sqlx::query_as::<_, FeePolicy>(QUERY)
+    let fee_policy = sqlx::query_as::<_, FeePolicies>(QUERY)
         .bind(auction_id)
         .bind(order_uid)
         .fetch_optional(ex)
@@ -59,16 +62,17 @@ mod tests {
         let mut db = db.begin().await.unwrap();
         crate::clear_DANGER_(&mut db).await.unwrap();
 
-        let input = FeePolicy {
+        let order_uid = ByteArray([1; 56]);
+        let input = FeePolicies {
             auction_id: 1,
-            order_uid: OrderUid::new(ByteArray::from([1; 16])),
-            quote_deviation_factor: Some(0.1),
-            volume_factor: Some(0.2),
-            absolute_fee: Some(BigDecimal::from(5)),
+            order_uid,
+            price_improvement_factor: [0.1, 0.2].to_vec(),
+            volume_factor: [0.3, 0.4].to_vec(),
+            absolute_fee: [BigDecimal::from(5), BigDecimal::from(6)].to_vec(),
         };
         insert(&mut db, input.clone()).await.unwrap();
 
-        let output = fetch(&mut db, 1).await.unwrap().unwrap();
+        let output = fetch(&mut db, 1, order_uid).await.unwrap().unwrap();
         assert_eq!(input, output);
     }
 }
--- database/sql/V057__add_fee_policy.sql
+++ database/sql/V057__add_fee_policy.sql
@@ -1,16 +1,16 @@
 -- Fee polices represents input parameters for the fee calculation, where fee is taken to compensate the protocol for it's services.
 -- There are different fee policies:
--- 1. Based on quote deviation - if limit order is filled at the price higher than max(limit price, best_quote), then fee is taken as a cut from the price difference. For this type of fee policy, all parameters exist and the fee is minimum of them.
+-- 1. Based on price improvement - if limit order is filled at the price higher than max(limit price, best_quote), then fee is taken as a cut from the price difference. For this type of fee policy, all parameters exist and the fee is minimum of them.
 -- 2. Based on the volume - fee is taken as part of the order volume. For this type of fee policy, only `volume_percentage_factor` exist.
 -- 3. Based on the absolute fee - fee is taken as an absolute value. For this type of fee policy, only `absolute_fee` exist.
 CREATE TABLE fee_policies (
   auction_id bigint NOT NULL
   order_uid bytea NOT NULL
 
-  -- The fee should be taken as a percentage of the quote deviation. The value is between 0 and 1.
-  quote_deviation_percentage NUMERIC(3, 2)[]
+  -- The fee should be taken as a percentage of the price improvement. The value is between 0 and 1.
+  price_improvement_factor NUMERIC(3, 2)[]
   -- The fee should be taken as a percentage of the order volume. The value is between 0 and 1.
-  volume_percentage_factor NUMERIC(3, 2)[]
+  volume_factor NUMERIC(3, 2)[]
   -- The fee should be taken as an absolute value.
   absolute_fee NUMERIC(78, 0)[]
 
