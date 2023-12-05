fe6b6dbc64867ea1fc1c16ec3374d349cf234f29
sunce86 <dusan.stanivukovic@gmail.com>
2023-12-05 23:28:08 +0100

    adjust the fee_policies table

--- database/sql/V057__add_fee_policy.sql
+++ database/sql/V057__add_fee_policy.sql
@@ -6,13 +6,17 @@
 CREATE TABLE fee_policies (
   auction_id bigint NOT NULL
   order_uid bytea NOT NULL
+  -- The order in which the fee policies are inserted and applied.
+  insertion_order SERIAL NOT NULL
 
   -- The fee should be taken as a percentage of the price improvement. The value is between 0 and 1.
-  price_improvement_factor NUMERIC(3, 2)[]
+  price_improvement_factor NUMERIC(3, 2) CHECK (price_improvement_factor >= 0 AND price_improvement_factor <= 1)
   -- The fee should be taken as a percentage of the order volume. The value is between 0 and 1.
-  volume_factor NUMERIC(3, 2)[]
+  volume_factor NUMERIC(3, 2) CHECK (volume_factor >= 0 AND volume_factor <= 1)
   -- The fee should be taken as an absolute value.
-  absolute_fee NUMERIC(78, 0)[]
+  absolute_fee NUMERIC(78, 0)
 
   PRIMARY KEY (auction_id, order_uid)
+
+  CONSTRAINT unique_auction_order UNIQUE (auction_id, order_uid, insertion_order)
 );
