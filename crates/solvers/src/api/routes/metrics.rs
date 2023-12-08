d1ef287a8ce19c620188059fe4181c35316bdc20
Felix Leupold <felixleupold90@gmail.com>
2023-12-08 16:16:48 +0100

    comments

--- crates/solvers/src/api/routes/metrics.rs
+++ crates/solvers/src/api/routes/metrics.rs
@@ -1,5 +1,3 @@
-use prometheus::Encoder;
-
 pub async fn metrics() -> String {
     let registry = observe::metrics::get_registry();
     observe::metrics::encode(registry)
--- crates/solvers/src/infra/metrics.rs
+++ crates/solvers/src/infra/metrics.rs
@@ -1,25 +1,22 @@
-use {
-    crate::domain::{auction, solution},
-    chrono::Utc,
-};
+use crate::domain::{auction, solution};
 
 /// Metrics for the solver engine.
 #[derive(Debug, Clone, prometheus_metric_storage::MetricStorage)]
-pub struct Metrics {
+struct Metrics {
     /// The amount of time this solver engine has for solving.
     #[metric(buckets(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15))]
-    pub time_limit: prometheus::Histogram,
+    time_limit: prometheus::Histogram,
 
     /// The amount of time this solver engine has left when it finished solving.
     #[metric(buckets(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15))]
-    pub remaining_time: prometheus::Histogram,
+    remaining_time: prometheus::Histogram,
 
     /// Errors that occurred during solving.
     #[metric(labels("reason"))]
-    pub solve_errors: prometheus::IntCounterVec,
+    solve_errors: prometheus::IntCounterVec,
 
     /// The number of solutions that were found.
-    pub solutions: prometheus::IntCounter,
+    solutions: prometheus::IntCounter,
 }
 
 /// Setup the metrics registry.
@@ -28,11 +25,19 @@ pub fn init() {
 }
 
 pub fn solve(auction: &auction::Auction) {
-    get().time_limit.observe(remaining_time(&auction.deadline));
+    get().time_limit.observe(
+        auction
+            .deadline
+            .remaining()
+            .unwrap_or_default()
+            .as_secs_f64(),
+    );
 }
 
 pub fn solved(deadline: &auction::Deadline, solutions: &[solution::Solution]) {
-    get().remaining_time.observe(remaining_time(deadline));
+    get()
+        .remaining_time
+        .observe(deadline.remaining().unwrap_or_default().as_secs_f64());
     get().solutions.inc_by(solutions.len() as u64);
 }
 
@@ -44,12 +49,4 @@ pub fn solve_error(reason: &str) {
 fn get() -> &'static Metrics {
     Metrics::instance(observe::metrics::get_storage_registry())
         .expect("unexpected error getting metrics instance")
-}
-
-fn remaining_time(deadline: &auction::Deadline) -> f64 {
-    deadline
-        .0
-        .signed_duration_since(Utc::now())
-        .num_milliseconds() as f64
-        / 1000.0
 }
