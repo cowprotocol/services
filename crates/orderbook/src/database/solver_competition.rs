263e205f932b12c60acba60e313d7e7659861a66
narcis96 <7252787+narcis96@users.noreply.github.com>
2023-12-18 11:52:45 +0200

    Add /solver_competition/latest endpoint (#2162)
    # Description
    Add new orderbook endpoint for the latest competion based on the latest
    auction ID
    
    ## How to test
    E2R tests should pass

--- crates/orderbook/src/database/solver_competition.rs
+++ crates/orderbook/src/database/solver_competition.rs
@@ -8,10 +8,28 @@ use {
         byte_array::ByteArray,
         settlement_scores::Score,
     },
-    model::solver_competition::{SolverCompetitionAPI, SolverCompetitionDB},
+    model::{
+        auction::AuctionId,
+        solver_competition::{SolverCompetitionAPI, SolverCompetitionDB},
+    },
     number::conversions::u256_to_big_decimal,
     primitive_types::H256,
+    sqlx::types::JsonValue,
 };
+
+fn deserialize_solver_competition(
+    json: JsonValue,
+    auction_id: AuctionId,
+    transaction_hash: Option<H256>,
+) -> Result<SolverCompetitionAPI, LoadSolverCompetitionError> {
+    let common: SolverCompetitionDB =
+        serde_json::from_value(json).context("deserialize SolverCompetitionDB")?;
+    Ok(SolverCompetitionAPI {
+        auction_id,
+        transaction_hash,
+        common,
+    })
+}
 
 #[async_trait::async_trait]
 impl SolverCompetitionStoring for Postgres {
@@ -122,28 +140,45 @@ impl SolverCompetitionStoring for Postgres {
             Identifier::Id(id) => database::solver_competition::load_by_id(&mut ex, id)
                 .await
                 .context("solver_competition::load_by_id")?
-                .map(|row| (row.json, id, row.tx_hash.map(|hash| H256(hash.0)))),
+                .map(|row| {
+                    deserialize_solver_competition(
+                        row.json,
+                        row.id,
+                        row.tx_hash.map(|hash| H256(hash.0)),
+                    )
+                }),
             // TODO: change this query to use the auction_transaction and settlements tables to
             // find the tx hash.
             Identifier::Transaction(hash) => {
                 database::solver_competition::load_by_tx_hash(&mut ex, &ByteArray(hash.0))
                     .await
                     .context("solver_competition::load_by_tx_hash")?
-                    .map(|row| (row.json, row.id, Some(hash)))
+                    .map(|row| deserialize_solver_competition(row.json, row.id, Some(hash)))
             }
         }
-        .map(
-            |(json, auction_id, transaction_hash)| -> Result<_, LoadSolverCompetitionError> {
-                let common: SolverCompetitionDB =
-                    serde_json::from_value(json).context("deserialize SolverCompetitionDB")?;
-                Ok(SolverCompetitionAPI {
-                    auction_id,
-                    transaction_hash,
-                    common,
-                })
-            },
-        )
         .ok_or(LoadSolverCompetitionError::NotFound)?
+    }
+
+    async fn load_latest_competition(
+        &self,
+    ) -> Result<SolverCompetitionAPI, LoadSolverCompetitionError> {
+        let _timer = super::Metrics::get()
+            .database_queries
+            .with_label_values(&["load_latest_solver_competition"])
+            .start_timer();
+
+        let mut ex = self.pool.acquire().await.map_err(anyhow::Error::from)?;
+        database::solver_competition::load_latest_competition(&mut ex)
+            .await
+            .context("solver_competition::load_latest")?
+            .map(|row| {
+                deserialize_solver_competition(
+                    row.json,
+                    row.id,
+                    row.tx_hash.map(|hash| H256(hash.0)),
+                )
+            })
+            .ok_or(LoadSolverCompetitionError::NotFound)?
     }
 }
 
