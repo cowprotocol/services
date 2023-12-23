4a6ef018f5db9f2afb15385ba614d5b5f585d1b0
sunce86 <dusan.stanivukovic@gmail.com>
2023-12-23 02:23:10 +0100

    refactor fee

--- crates/autopilot/src/protocol/fee.rs
+++ crates/autopilot/src/protocol/fee.rs
@@ -7,44 +7,70 @@
 use {
     crate::{
         arguments,
+        database::Postgres,
         driver_model::solve::{fee_policy_to_dto, FeePolicy},
+        run_loop::is_order_outside_market_price,
     },
     model::{
         auction::Auction,
         order::{OrderClass, OrderUid},
     },
-    std::collections::HashMap,
+    number::conversions::big_decimal_to_u256,
+    std::{collections::HashMap, sync::Arc},
 };
+
+pub struct PolicyFactory {
+    config: arguments::FeePolicy,
+    db: Arc<Postgres>,
+}
+
+impl PolicyFactory {
+    pub async fn build(&self, auction: &Auction) -> Policies {
+        let quotes = self.db.read_quotes(auction).await.unwrap();
+        Policies {
+            policies: {
+                auction
+                    .orders
+                    .iter()
+                    .filter_map(|order| {
+                        match order.metadata.class {
+                            OrderClass::Market => None,
+                            OrderClass::Liquidity => None,
+                            // TODO: https://github.com/cowprotocol/services/issues/2115
+                            // skip protocol fee for TWAP limit orders
+                            OrderClass::Limit(_) => {
+                                let quote = quotes.get(&order.metadata.uid)?;
+                                let quote_buy_amount = big_decimal_to_u256(&quote.buy_amount)?;
+                                let quote_sell_amount = big_decimal_to_u256(&quote.sell_amount)?;
+                                let is_in_money_order = !is_order_outside_market_price(
+                                    &order.data.sell_amount,
+                                    &order.data.buy_amount,
+                                    quote_buy_amount,
+                                    quote_sell_amount,
+                                );
+                                if self.config.fee_policy_skip_market_orders && is_in_money_order {
+                                    return None;
+                                }
+                                Some((order.metadata.uid, vec![fee_policy_to_dto(&self.config)]))
+                            }
+                        }
+                    })
+                    .collect()
+            },
+        }
+    }
+}
 
 pub struct Policies {
     policies: HashMap<OrderUid, Vec<FeePolicy>>,
 }
 
 impl Policies {
-    pub fn new(auction: &Auction, config: arguments::FeePolicy) -> Self {
-        Self {
-            policies: auction
-                .orders
-                .iter()
-                .filter_map(|order| {
-                    match order.metadata.class {
-                        OrderClass::Market => None,
-                        OrderClass::Liquidity => None,
-                        // TODO: https://github.com/cowprotocol/services/issues/2092
-                        // skip protocol fee for limit orders with in-market price
-
-                        // TODO: https://github.com/cowprotocol/services/issues/2115
-                        // skip protocol fee for TWAP limit orders
-                        OrderClass::Limit(_) => {
-                            Some((order.metadata.uid, vec![fee_policy_to_dto(&config)]))
-                        }
-                    }
-                })
-                .collect(),
-        }
+    pub fn new(policies: HashMap<OrderUid, Vec<FeePolicy>>) -> Self {
+        Self { policies }
     }
 
-    pub fn get(&self, order: &OrderUid) -> Option<Vec<FeePolicy>> {
-        self.policies.get(order).cloned()
+    pub fn get(&self, order: &OrderUid) -> Option<&Vec<FeePolicy>> {
+        self.policies.get(order)
     }
 }
