07e9520bb52a0a820fa0ea2481817a4b61df455f
sunce86 <dusan.stanivukovic@gmail.com>
2023-12-26 10:44:58 +0100

    order_execution save

--- crates/database/src/order_execution.rs
+++ crates/database/src/order_execution.rs
@@ -10,19 +10,17 @@ pub async fn save(
     auction: AuctionId,
     executed_fee: &BigDecimal,
 ) -> Result<(), sqlx::Error> {
-    let scoring_fee: Option<&BigDecimal> = None; // we don't need a scoring fee anymore
     const QUERY: &str = r#"
-INSERT INTO order_execution (order_uid, auction_id, reward, surplus_fee, solver_fee)
-VALUES ($1, $2, $3, $4, $5)
+INSERT INTO order_execution (order_uid, auction_id, reward, surplus_fee)
+VALUES ($1, $2, $3, $4)
 ON CONFLICT (order_uid, auction_id)
-DO UPDATE SET reward = $3, surplus_fee = $4, solver_fee = $5
+DO UPDATE SET reward = $3, surplus_fee = $4
 ;"#;
     sqlx::query(QUERY)
         .bind(order)
         .bind(auction)
         .bind(0.) // reward is deprecated but saved for historical analysis
         .bind(Some(executed_fee))
-        .bind(scoring_fee)
         .execute(ex)
         .await?;
     Ok(())
