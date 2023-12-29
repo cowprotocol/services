36001d0decb962ea5b1c3fef94616ebca58a8f60
Felix Leupold <felixleupold90@gmail.com>
2023-12-29 16:33:41 +0100

    [Hotfix] Extend solver engine request body limit (#2223)
    # Description
    We are seeing 413 repsonses between the driver and solver engine in
    prod. Default limit seems to be 2MB
    ([docs](https://docs.rs/axum/latest/axum/extract/struct.DefaultBodyLimit.html))
    
    # Changes
    - [x] Explicitly set the limit larger (10MB)
    - [x] Disable default body limit in axum
    
    ## How to test
    No longer see 413 errors in shadow
    
    Locally override autopilot to send a sovle request with 3x the orders.
    No longer see 413s.

--- crates/solvers/src/api/mod.rs
+++ crates/solvers/src/api/mod.rs
@@ -8,6 +8,8 @@ use {
 
 mod routes;
 
+const REQUEST_BODY_LIMIT: usize = 10 * 1024 * 1024;
+
 pub struct Api {
     pub addr: SocketAddr,
     pub solver: Solver,
@@ -20,6 +22,9 @@ impl Api {
         shutdown: impl Future<Output = ()> + Send + 'static,
     ) -> Result<(), hyper::Error> {
         let app = axum::Router::new()
+            .layer(tower::ServiceBuilder::new().layer(
+                tower_http::limit::RequestBodyLimitLayer::new(REQUEST_BODY_LIMIT),
+            ))
             .route("/metrics", axum::routing::get(routes::metrics))
             .route("/healthz", axum::routing::get(routes::healthz))
             .route("/solve", axum::routing::post(routes::solve))
@@ -27,7 +32,8 @@ impl Api {
             .layer(
                 tower::ServiceBuilder::new().layer(tower_http::trace::TraceLayer::new_for_http()),
             )
-            .with_state(Arc::new(self.solver));
+            .with_state(Arc::new(self.solver))
+            .layer(axum::extract::DefaultBodyLimit::disable());
 
         let make_svc = observe::make_service_with_task_local_storage!(app);
 
