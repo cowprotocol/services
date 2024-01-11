2206d4e926e92a4cba026f59e44d3e2775c52e6a
sunce86 <dusan.stanivukovic@gmail.com>
2024-01-11 12:02:15 +0100

    fix comments

--- crates/e2e/tests/e2e/protocol_fee.rs
+++ crates/e2e/tests/e2e/protocol_fee.rs
@@ -123,7 +123,6 @@ async fn price_improvement_fee_buy_order_capped_test(web3: Web3) {
 fn assert_between(value: U256, expected: U256) {
     let lower = expected * U256::from(99999999999u128) / U256::from(100000000000u128); // in percents = 99.999999999%
     let upper = expected * U256::from(100000000001u128) / U256::from(100000000000u128); // in percents = 100.000000001%
-    println!("executed: {}, lower: {}, upper: {}", value, lower, upper);
     assert!(value >= lower);
     assert!(value <= upper);
 }
@@ -241,13 +240,7 @@ async fn execute_test(
     let metadata_updated = || async {
         onchain.mint_block().await;
         let order = services.get_order(&uid).await.unwrap();
-        let executed_surplus_fee = match order.metadata.class {
-            OrderClass::Limit(LimitOrderClass {
-                executed_surplus_fee,
-                ..
-            }) => executed_surplus_fee,
-            _ => unreachable!(),
-        };
+        let executed_surplus_fee = order.metadata.executed_surplus_fee;
         if !executed_surplus_fee.is_zero() {
             assert_between(executed_surplus_fee, expected_surplus_fee);
         }
