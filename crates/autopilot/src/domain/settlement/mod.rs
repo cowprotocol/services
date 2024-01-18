ea632cc0c1f9587c165ca6a01425072f7c599ffa
sunce86 <dusan.stanivukovic@gmail.com>
2024-01-18 23:28:32 +0100

    domain_separator

--- crates/autopilot/src/domain/settlement/mod.rs
+++ crates/autopilot/src/domain/settlement/mod.rs
@@ -10,7 +10,6 @@ pub use {encoded::Encoded, event::Event, transaction::Transaction};
 /// A transaction that settles a settlement. Interacts with the settlement
 /// contract.
 pub struct Settlement {
-    domain_separator: eth::DomainSeparator,
     /// Mined transaction parameters.
     pub transaction: Transaction,
     /// Encoded transaction data as expected by settlement contract
@@ -20,17 +19,9 @@ pub struct Settlement {
 impl Settlement {
     pub async fn new(tx: eth::TxId, eth: infra::Ethereum) -> Result<Self> {
         let transaction = eth.transaction(tx).await?.unwrap();
-        let domain_separator = eth::DomainSeparator(
-            eth.contracts()
-                .settlement()
-                .domain_separator()
-                .call()
-                .await?
-                .0,
-        );
+        let domain_separator = eth.contracts().settlement_domain_separator();
         let encoded = Encoded::new(&transaction.input, domain_separator)?;
         Ok(Self {
-            domain_separator,
             transaction,
             encoded,
         })
