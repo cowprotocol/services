c891c42111c6462e83c7496182a66066bd9e7175
sunce86 <dusan.stanivukovic@gmail.com>
2024-01-18 16:35:27 +0100

    use f64

--- crates/autopilot/src/infra/persistence/dto/quote.rs
+++ crates/autopilot/src/infra/persistence/dto/quote.rs
@@ -4,28 +4,17 @@ use {
     primitive_types::U256,
 };
 
-pub fn into_domain(quote: boundary::database::orders::Quote) -> Result<domain::Quote, Error> {
+pub fn into_domain(
+    quote: boundary::database::orders::Quote,
+) -> Result<domain::Quote, AmountOverflow> {
     Ok(domain::Quote {
         order_uid: domain::OrderUid(quote.order_uid.0),
-        sell_amount: big_decimal_to_u256(&quote.sell_amount).ok_or(Error::AmountOverflow)?,
-        buy_amount: big_decimal_to_u256(&quote.buy_amount).ok_or(Error::AmountOverflow)?,
-        fee: {
-            let gas_amount = U256::from_f64_lossy(quote.gas_amount);
-            let gas_price = U256::from_f64_lossy(quote.gas_price);
-            let sell_token_price = U256::from_f64_lossy(quote.sell_token_price);
-            gas_amount
-                .checked_mul(gas_price)
-                .ok_or(Error::AmountOverflow)?
-                .checked_div(sell_token_price)
-                .ok_or(Error::DivisionByZero)?
-        },
+        sell_amount: big_decimal_to_u256(&quote.sell_amount).ok_or(AmountOverflow)?,
+        buy_amount: big_decimal_to_u256(&quote.buy_amount).ok_or(AmountOverflow)?,
+        fee: U256::from_f64_lossy(quote.gas_amount * quote.gas_price / quote.sell_token_price),
     })
 }
 
 #[derive(Debug, thiserror::Error)]
-pub enum Error {
-    #[error("amount overflow")]
-    AmountOverflow,
-    #[error("division by zero with sell_token_price")]
-    DivisionByZero,
-}
+#[error("amount overflow")]
+pub struct AmountOverflow;
