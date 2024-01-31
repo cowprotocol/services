71cebe688e0b7cf27d273e41d936af53a6161cd7
sunce86 <dusan.stanivukovic@gmail.com>
2024-01-31 12:41:59 +0100

    update migration

--- database/sql/V059__add_block_number_order_execution.sql
+++ database/sql/V059__add_block_number_order_execution.sql
@@ -1,8 +1,11 @@
 ALTER TABLE order_execution
-    ADD COLUMN block_number bigint NOT NULL;
+    ADD COLUMN block_number bigint NOT NULL DEFAULT 0;
 
 -- Populate block_number for existing records
 UPDATE order_execution
-SET block_number = COALESCE(settlements.block_number, 0)
+SET block_number = settlements.block_number
 FROM settlements
 WHERE order_execution.auction_id = settlements.auction_id;
+
+ALTER TABLE order_execution
+    ALTER COLUMN block_number DROP DEFAULT;
