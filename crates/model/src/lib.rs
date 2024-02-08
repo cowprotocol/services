b65011283bfbc4b93cf17a371856f9305de313f1
Martin Beckmann <martin.beckmann@protonmail.com>
2024-02-08 22:06:37 +0100

    Log call data as hex string

--- crates/model/src/lib.rs
+++ crates/model/src/lib.rs
@@ -148,6 +148,16 @@ pub struct SolvableOrders {
     pub latest_settlement_block: u64,
 }
 
+fn debug_optional_bytes(
+    bytes: &Option<impl AsRef<[u8]>>,
+    formatter: &mut std::fmt::Formatter,
+) -> Result<(), std::fmt::Error> {
+    match bytes {
+        Some(bytes) => formatter.write_fmt(format_args!("0x{}", hex::encode(bytes.as_ref()))),
+        None => formatter.write_str("None"),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use {
