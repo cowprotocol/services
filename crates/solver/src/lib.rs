2f2f2d71059dc8b0390ffc83187e695d1796c6c3
Martin Beckmann <martin.beckmann@protonmail.com>
2024-02-13 14:09:45 +0100

    Delete settlement_post_processing

--- crates/solver/src/lib.rs
+++ crates/solver/src/lib.rs
@@ -8,7 +8,6 @@ pub mod order_balance_filter;
 pub mod s3_instance_upload;
 pub mod settlement;
 pub mod settlement_access_list;
-pub mod settlement_post_processing;
 pub mod settlement_rater;
 pub mod settlement_simulation;
 pub mod settlement_submission;
