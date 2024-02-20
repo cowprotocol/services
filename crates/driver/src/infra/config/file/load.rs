1eb773ac5aa48858ac6d4b6002d2a429216bcfa7
Frederico Marques <frederico.marques.22@gmail.com>
2024-02-20 11:05:52 -0300

    lint: remove ununsed export

--- crates/driver/src/infra/config/file/load.rs
+++ crates/driver/src/infra/config/file/load.rs
@@ -1,7 +1,7 @@
 use {
     crate::{
-        boundary::liquidity::uniswap, domain::eth, infra::{self, blockchain, config::file, liquidity, mempool, simulator, solver}
-    }, futures::future::join_all, lazy_static::lazy_static, reqwest::Url, shared::sources::uniswap_v3, std::path::Path, tokio::fs
+        domain::eth, infra::{self, blockchain, config::file, liquidity, mempool, simulator, solver}
+    }, futures::future::join_all, lazy_static::lazy_static, reqwest::Url, std::path::Path, tokio::fs
 };
 
 lazy_static! {
