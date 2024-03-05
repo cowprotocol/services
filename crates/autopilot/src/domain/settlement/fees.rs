f39f734924e8cbf73549ae1e9fa785adc82d8280
sunce86 <dusan.stanivukovic@gmail.com>
2024-03-05 14:44:28 +0100

    remove comments

--- crates/autopilot/src/domain/settlement/fees.rs
+++ crates/autopilot/src/domain/settlement/fees.rs
@@ -1,14 +1,3 @@
-//! There are two types of fees in the system: gas cost fees and protocol fees.
-//!
-//! Gas cost fees are fees that are paid to a network for the onchain
-//! execution of the settlement. These fees are paid in ETH and are calculated
-//! based on the amount of gas used by the transaction. The gas cost fees are
-//! determined by solvers.
-//!
-//! Protocol fees are fees that are paid to the protocol for it's services.
-//! These fees are paid in the native token of the protocol and are determined
-//! by the protocol.
-
 use {
     crate::domain::{auction, eth, settlement},
     std::collections::HashMap,
