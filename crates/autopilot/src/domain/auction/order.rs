5861c7f955a4d9080613e2ee3db187802e3f6a82
sunce86 <dusan.stanivukovic@gmail.com>
2024-03-19 02:26:57 +0100

    remove from autopilot

--- crates/autopilot/src/domain/auction/order.rs
+++ crates/autopilot/src/domain/auction/order.rs
@@ -11,7 +11,6 @@ pub struct Order {
     pub buy_token: H160,
     pub sell_amount: U256,
     pub buy_amount: U256,
-    pub user_fee: U256,
     pub protocol_fees: Vec<fee::Policy>,
     pub side: Side,
     pub class: Class,
