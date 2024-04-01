8c14b90f22cfc839c1165b7564520cba4b6015a0
sunce86 <dusan.stanivukovic@gmail.com>
2024-04-01 12:55:33 +0200

    remove score as response 2

--- crates/solvers/src/tests/naive/without_pool.rs
+++ crates/solvers/src/tests/naive/without_pool.rs
@@ -90,10 +90,6 @@ async fn test() {
                     },
                 ],
                 "interactions": [],
-                "score": {
-                    "kind": "riskAdjusted",
-                    "successProbability": 0.5,
-                },
                 "gas": 259417,
             }]
         }),
