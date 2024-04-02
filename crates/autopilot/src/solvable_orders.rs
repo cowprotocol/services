6f7271d0024783de52e41ef22d6458186f52ae53
Martin Beckmann <martin.beckmann@protonmail.com>
2024-04-02 08:13:56 +0200

    Make update() private

--- crates/autopilot/src/solvable_orders.rs
+++ crates/autopilot/src/solvable_orders.rs
@@ -140,7 +140,7 @@ impl SolvableOrdersCache {
     /// Usually this method is called from update_task. If it isn't, which is
     /// the case in unit tests, then concurrent calls might overwrite each
     /// other's results.
-    pub async fn update(&self, block: u64) -> Result<()> {
+    async fn update(&self, block: u64) -> Result<()> {
         let min_valid_to = now_in_epoch_seconds() + self.min_order_validity_period.as_secs() as u32;
         let db_solvable_orders = self.persistence.solvable_orders(min_valid_to).await?;
 
