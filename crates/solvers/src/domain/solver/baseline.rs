1a8e8a9de10c37403be1979d62bd4e5a9342c1dd
sunce86 <dusan.stanivukovic@gmail.com>
2024-04-04 12:07:51 +0200

    one comment

--- crates/solvers/src/domain/solver/baseline.rs
+++ crates/solvers/src/domain/solver/baseline.rs
@@ -139,6 +139,8 @@ impl Inner {
             let sell_token = match auction.tokens.reference_price(&order.sell.token) {
                 Some(price) => price,
                 None => {
+                    // internal routing request to estimate the price of the sell token in the native token
+
                     // Early return if the sell token is native token
                     if user_order.get().sell.token == self.weth.0.into() {
                         auction::Price(eth::Ether(eth::U256::exp10(18)))
