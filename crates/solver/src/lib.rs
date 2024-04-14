8fd3e852c436243f16c46e52cdc42909801f3aae
MartinquaXD <martin.beckmann@protonmail.com>
2024-04-14 21:00:40 +0200

    Delete old submisison logic

--- crates/solver/src/lib.rs
+++ crates/solver/src/lib.rs
@@ -1,10 +1,8 @@
 pub mod interactions;
 pub mod liquidity;
 pub mod liquidity_collector;
-mod metrics;
 pub mod order_balance_filter;
 pub mod settlement;
 pub mod settlement_access_list;
 pub mod settlement_simulation;
-pub mod settlement_submission;
 pub mod solver;
