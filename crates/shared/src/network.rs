c682d4e4462655b50d56ba7b1b045ddefd2c7a9e
MartinquaXD <martin.beckmann@protonmail.com>
2024-04-29 13:12:45 +0200

    Delete `block_interval()`

--- crates/shared/src/network.rs
+++ crates/shared/src/network.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 /// Maps ChainId to the network name.
 /// If the output is from a known network, it represents the canonical name of
 /// the network on CoW Protocol.
@@ -14,17 +12,4 @@ pub fn network_name(chain_id: u64) -> &'static str {
         42161 => "Arbitrum / Mainnet",
         _ => panic!("Unknown network (chain_id={chain_id})"),
     }
-}
-
-/// The expected time between blocks on the network.
-pub fn block_interval(chain_id: u64) -> Option<Duration> {
-    match chain_id {
-        1 => Duration::from_secs(12),
-        5 => Duration::from_secs(12),
-        100 => Duration::from_secs(5),
-        11155111 => Duration::from_secs(12),
-        42161 => Duration::from_millis(250),
-        _ => return None,
-    }
-    .into()
 }
