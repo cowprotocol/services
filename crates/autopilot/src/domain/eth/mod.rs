84c81ebe405147a35541fcbf4b736fe7d368bb42
sunce86 <dusan.stanivukovic@gmail.com>
2024-05-07 15:46:39 +0200

    Initial commit

--- crates/autopilot/src/domain/eth/mod.rs
+++ crates/autopilot/src/domain/eth/mod.rs
@@ -30,6 +30,108 @@ impl From<TokenAmount> for U256 {
     }
 }
 
+impl TokenAmount {
+    /// Applies a factor to the token amount.
+    ///
+    /// The factor is first multiplied by 10^18 to convert it to integer, to
+    /// avoid rounding to 0. Then, the token amount is divided by 10^18 to
+    /// convert it back to the original scale.
+    ///
+    /// The higher the conversion factor (10^18) the precision is higher. E.g.
+    /// 0.123456789123456789 will be converted to 123456789123456789.
+    pub fn apply_factor(&self, factor: f64) -> Option<Self> {
+        Some(
+            (self
+                .0
+                .checked_mul(U256::from_f64_lossy(factor * 1000000000000000000.))?
+                / 1000000000000000000u128)
+                .into(),
+        )
+    }
+}
+
+impl std::ops::Sub<Self> for TokenAmount {
+    type Output = TokenAmount;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        self.0.sub(rhs.0).into()
+    }
+}
+
+impl num::CheckedSub for TokenAmount {
+    fn checked_sub(&self, other: &Self) -> Option<Self> {
+        self.0.checked_sub(other.0).map(Into::into)
+    }
+}
+
+impl std::ops::Add for TokenAmount {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0)
+    }
+}
+
+impl num::CheckedAdd for TokenAmount {
+    fn checked_add(&self, other: &Self) -> Option<Self> {
+        self.0.checked_add(other.0).map(Into::into)
+    }
+}
+
+impl std::ops::Mul<Self> for TokenAmount {
+    type Output = TokenAmount;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        self.0.mul(rhs.0).into()
+    }
+}
+
+impl num::CheckedMul for TokenAmount {
+    fn checked_mul(&self, other: &Self) -> Option<Self> {
+        self.0.checked_mul(other.0).map(Into::into)
+    }
+}
+
+impl std::ops::Div<Self> for TokenAmount {
+    type Output = TokenAmount;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        self.0.div(rhs.0).into()
+    }
+}
+
+impl num::CheckedDiv for TokenAmount {
+    fn checked_div(&self, other: &Self) -> Option<Self> {
+        self.0.checked_div(other.0).map(Into::into)
+    }
+}
+
+impl std::ops::AddAssign for TokenAmount {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0 += rhs.0;
+    }
+}
+
+impl num::Zero for TokenAmount {
+    fn zero() -> Self {
+        Self(U256::zero())
+    }
+
+    fn is_zero(&self) -> bool {
+        self.0.is_zero()
+    }
+}
+
+/// A token amount in wei, always representing the sell token of an order.
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct SellTokenAmount(pub U256);
+
+impl From<U256> for SellTokenAmount {
+    fn from(value: U256) -> Self {
+        Self(value)
+    }
+}
+
 /// An asset on the Ethereum blockchain. Represents a particular amount of a
 /// particular token.
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
