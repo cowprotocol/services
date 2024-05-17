930df8fa5ffc9ce3266e9fc9380d44f02c45b9d0
sunce86 <dusan.stanivukovic@gmail.com>
2024-05-17 13:58:46 +0200

    fix syntax

--- database/sql/V065__auction_orders.sql
+++ database/sql/V065__auction_orders.sql
@@ -1,5 +1,5 @@
 -- All orders that were part of an Auction. An order can appear in multiple auctions.
 CREATE TABLE auction_orders (
   auction_id bigint PRIMARY KEY,
-  order_uid bytea NOT NULL,
+  order_uid bytea NOT NULL
 );
