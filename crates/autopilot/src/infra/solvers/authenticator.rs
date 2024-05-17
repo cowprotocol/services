eecd7ea0b08d0fde4de58ffa5b2fb61eb5533830
Mateo <mateo@cow.fi>
2024-05-17 09:51:21 +0200

    Rework comments

--- crates/autopilot/src/infra/solvers/authenticator.rs
+++ crates/autopilot/src/infra/solvers/authenticator.rs
@@ -1,9 +1,10 @@
 use {
     crate::{
-        domain,
+        domain::{self, eth},
         infra::blockchain::{contracts::deployment_address, ChainId},
     },
-    ethcontract::{dyns::DynWeb3, transaction::TransactionResult},
+    ethcontract::dyns::DynWeb3,
+    primitive_types::H160,
 };
 
 #[allow(dead_code)]
@@ -17,14 +18,28 @@ pub struct Authenticator {
     authenticator_eoa: ethcontract::Account,
 }
 
+pub struct Addresses {
+    pub settlement: Option<eth::Address>,
+    pub authenticator_eoa: eth::H256,
+}
+
 impl Authenticator {
     /// Creates an authenticator which can remove solvers from the allow-list
-    pub async fn new(
-        web3: DynWeb3,
-        chain: ChainId,
-        settlement: contracts::GPv2Settlement,
-        authenticator_eoa: ethcontract::Account,
-    ) -> Self {
+    pub async fn new(web3: DynWeb3, chain: ChainId, addresses: Addresses) -> Self {
+        let address_for = |contract: &ethcontract::Contract, address: Option<H160>| {
+            address
+                .or_else(|| deployment_address(contract, &chain))
+                .unwrap()
+        };
+
+        let settlement = contracts::GPv2Settlement::at(
+            &web3,
+            address_for(
+                contracts::GPv2Settlement::raw_contract(),
+                addresses.settlement.map(Into::into),
+            ),
+        );
+
         let authenticator = contracts::GPv2AllowListAuthentication::at(
             &web3,
             settlement
@@ -42,17 +57,17 @@ impl Authenticator {
         Self {
             authenticator,
             authenticator_role,
-            authenticator_eoa,
+            authenticator_eoa: ethcontract::Account::Offline(
+                ethcontract::PrivateKey::from_raw(addresses.authenticator_eoa.0).unwrap(),
+                None,
+            ),
         }
     }
 
-    /// Removes solver from the allow-list in the authenticator contract. This
-    /// solver will no longer be able to settle.
+    /// Fire and forget: Removes solver from the allow-list in the authenticator
+    /// contract. This solver will no longer be able to settle.
     #[allow(dead_code)]
-    async fn remove_solver(
-        &self,
-        solver: domain::eth::Address,
-    ) -> Result<TransactionResult, Error> {
+    async fn remove_solver(&self, solver: domain::eth::Address) -> Result<(), Error> {
         let calldata = self
             .authenticator
             .methods()
@@ -60,20 +75,26 @@ impl Authenticator {
             .tx
             .data
             .ok_or(Error::SolverRemovalBadCalldata)?;
-        self.authenticator_role
-            .methods()
-            .exec_transaction_with_role(
-                self.authenticator.address(),
-                0.into(),
-                ethcontract::Bytes(calldata.0),
-                0,
-                ethcontract::Bytes([0; 32]), // @TODO: populate role
-                true,
-            )
-            .from(self.authenticator_eoa.clone())
-            .send()
-            .await
-            .map_err(Error::SolverRemovalFailed)
+        let authenticator_eoa = self.authenticator_eoa.clone();
+        let authenticator_address = self.authenticator.address();
+        let authenticator_role = self.authenticator_role.clone();
+        tokio::task::spawn(async move {
+            authenticator_role
+                .methods()
+                .exec_transaction_with_role(
+                    authenticator_address,
+                    0.into(),
+                    ethcontract::Bytes(calldata.0),
+                    0,
+                    ethcontract::Bytes([0; 32]), // @TODO: populate role
+                    true,
+                )
+                .from(authenticator_eoa)
+                .send()
+                .await
+                .map_err(Error::SolverRemovalFailed)
+        });
+        Ok(())
     }
 }
 
