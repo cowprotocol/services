e344bccff7eee314808d9e55dfeb024ad4d230e1
ilya <ilya@cow.fi>
2024-06-06 20:06:47 +0100

    Script fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Runs heaptrack for the orderbook binary only
 if [ "$1" = "orderbook" ]; then
-  exec heaptrack -o /tmp/heaptrack/heaptrack.orderbook.$(shuf -i 1-99999 -n 1).gz /usr/local/bin/orderbook
+  exec /bin/sh -c "heaptrack -o /tmp/heaptrack/heaptrack.orderbook.$(shuf -i 1-99999 -n 1).gz /usr/local/bin/orderbook"
 else
   exec "$@"
 fi
