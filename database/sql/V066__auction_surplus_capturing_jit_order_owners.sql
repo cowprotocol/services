e64137cd0ea0e5d05f194080ea7e1aee4e5cc892
sunce86 <dusan.stanivukovic@gmail.com>
2024-07-15 12:19:39 +0200

    fix comment

--- database/sql/V066__auction_surplus_capturing_jit_order_owners.sql
+++ database/sql/V066__auction_surplus_capturing_jit_order_owners.sql
@@ -1,4 +1,4 @@
--- All surplus_capturing_jit_order_owners that were part of an auction.
+-- All surplus capturing JIT order owners that were part of an auction.
 CREATE TABLE surplus_capturing_jit_order_owners (
   auction_id bigint PRIMARY KEY,
   owners bytea[] NOT NULL
