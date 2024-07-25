ea010ecb788b2c285570d6bf945c5f7008f31cf5
sunce86 <dusan.stanivukovic@gmail.com>
2024-07-25 22:39:30 +0200

    simplification

--- database/sql/V067__create_jit_orders.sql
+++ database/sql/V067__create_jit_orders.sql
@@ -1,28 +1,12 @@
-CREATE TABLE jit_orders (
-    block_number bigint NOT NULL,
-    log_index bigint NOT NULL,
-    uid bytea PRIMARY KEY,
-    owner bytea NOT NULL,
-    creation_timestamp timestamptz NOT NULL,
-    sell_token bytea NOT NULL,
-    buy_token bytea NOT NULL,
-    sell_amount numeric(78,0) NOT NULL,
-    buy_amount numeric(78,0) NOT NULL,
-    valid_to bigint NOT NULL,
-    app_data bytea NOT NULL,
-    fee_amount numeric(78,0) NOT NULL,
-    kind OrderKind NOT NULL,
-    partially_fillable boolean NOT NULL,
-    signature bytea NOT NULL -- r + s + v
-    cancellation_timestamp timestamptz
-    receiver bytea
-    signing_scheme SigningScheme NOT NULL
-    settlement_contract bytea NOT NULL
-    sell_token_balance SellTokenSource NOT NULL
-    buy_token_balance BuyTokenDestination NOT NULL
-    full_fee_amount numeric(78,0) NOT NULL
-    class OrderClass NOT NULL
-);
+-- Create a new table based on the structure of the existing table
+CREATE TABLE jit_orders AS
+TABLE orders WITH NO DATA;
+
+-- Add the new columns to the newly created table, that represent the event index
+ALTER TABLE jit_orders
+ADD COLUMN block_number bigint NOT NULL,
+ADD COLUMN log_index bigint NOT NULL,
+ADD CONSTRAINT jit_orders_pkey PRIMARY KEY (uid);
 
 -- Get a specific user's orders.
 CREATE INDEX jit_order_owner ON jit_orders USING HASH (owner);
