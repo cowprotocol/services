539f29dd52ad95abb3e56180204268ce185bbbd9
sunce86 <dusan.stanivukovic@gmail.com>
2024-07-29 10:59:38 +0200

    remove duplicated index

--- database/sql/V067__create_index_on_ethflow_orders.sql
+++ database/sql/V067__create_index_on_ethflow_orders.sql
@@ -1,2 +1,5 @@
 -- To get all valid orders quickly, we create an index
 CREATE INDEX ethflow_user_valid_to ON ethflow_orders USING BTREE (valid_to);
+
+-- Remove wrongly added index in V031__create_ethflow_orders.sql
+DROP INDEX user_valid_to;
