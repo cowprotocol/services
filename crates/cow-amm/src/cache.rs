4af69c5ac11969fd7d8ff1796a44d609df2a6e81
ilya <ilya@cow.fi>
2024-08-01 12:25:12 +0300

    Revert "Check token balance before appending"
    This reverts commit 4890fea9

--- crates/cow-amm/src/cache.rs
+++ crates/cow-amm/src/cache.rs
@@ -16,13 +16,12 @@ use {
 pub(crate) struct Storage(Arc<Inner>);
 
 impl Storage {
-    pub(crate) fn new(deployment_block: u64, helper: CowAmmLegacyHelper, web3: Web3) -> Self {
+    pub(crate) fn new(deployment_block: u64, helper: CowAmmLegacyHelper) -> Self {
         Self(Arc::new(Inner {
             cache: Default::default(),
             // make sure to start 1 block **before** the deployment to get all the events
             start_of_index: deployment_block - 1,
             helper,
-            web3,
         }))
     }
 
@@ -52,7 +51,6 @@ struct Inner {
     start_of_index: u64,
     /// Helper contract to query required data from the cow amm.
     helper: CowAmmLegacyHelper,
-    web3: Web3,
 }
 
 #[async_trait::async_trait]
@@ -92,27 +90,10 @@ impl EventStoring<CowAmmEvent> for Storage {
             let CowAmmEvent::CowammpoolCreated(cow_amm) = event.data;
             let cow_amm = cow_amm.amm;
 
-            let amm = Amm::new(cow_amm, &self.0.helper).await?;
-            let futures = amm.traded_tokens().iter().map(|token| {
-                let web3 = self.0.web3.clone();
-                async move {
-                    match ERC20::at(&web3, *token).balance_of(cow_amm).call().await {
-                        Ok(balance) if balance == U256::zero() => {
-                            Err(anyhow::anyhow!(format!("AMM token {} balance is 0", token)))
-                        }
-                        _ => Ok(()),
-                    }
-                }
-            });
-            join_all(futures)
-                .await
-                .into_iter()
-                .collect::<Result<Vec<_>, _>>()?;
-
             cache
                 .entry(meta.block_number)
                 .or_default()
-                .push(Arc::new(amm));
+                .push(Arc::new(Amm::new(cow_amm, &self.0.helper).await?));
             tracing::info!(?cow_amm, "indexed new cow amm");
         }
         Ok(())
