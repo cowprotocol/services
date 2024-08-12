cbedf0be95f47e58740184dd7843fa9afd9d6ed9
ilya <ilya@cow.fi>
2024-08-12 12:33:28 +0300

    Redundant change

--- crates/e2e/tests/e2e/cow_amm.rs
+++ crates/e2e/tests/e2e/cow_amm.rs
@@ -531,8 +531,6 @@ async fn cow_amm_driver_support(web3: Web3) {
     .await
     .unwrap();
 
-    onchain.mint_block().await;
-
     // all cow amms on mainnet the helper contract is aware of
     tracing::info!("Waiting for all cow amms to be indexed.");
     let expected_cow_amms = [
