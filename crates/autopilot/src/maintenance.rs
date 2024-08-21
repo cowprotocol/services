f45ce73531758647aef4b9abd50c77b3e07e61d1
MartinquaXD <martin.beckmann@protonmail.com>
2024-08-21 13:49:03 +0000

    fix background task for tests

--- crates/autopilot/src/maintenance.rs
+++ crates/autopilot/src/maintenance.rs
@@ -118,12 +118,23 @@ impl Maintenance {
         Ok(())
     }
 
-    /// Spawns a background task that runs updates when new blocks are seen.
+    /// Spawns a background task that runs every `interval` or when a new block
+    /// is seen.
     pub fn spawn_background_task(self_: Arc<Self>, current_block: CurrentBlockWatcher) {
         tokio::task::spawn(async move {
+            let mut latest_block = *current_block.borrow();
             let mut stream = into_stream(current_block);
-            while let Some(block) = stream.next().await {
-                self_.update(&block).await;
+            loop {
+                let next_update = timeout(Duration::from_millis(1_000), stream.next());
+                let current_block = match next_update.await {
+                    Ok(Some(block)) => block,
+                    Ok(None) => break,
+                    Err(_timeout) => latest_block,
+                };
+                if let Err(err) = self_.update_inner(&current_block).await {
+                    tracing::warn!(?err, "failed to run background task successfully");
+                }
+                latest_block = current_block;
             }
             panic!("block stream terminated unexpectedly");
         });
