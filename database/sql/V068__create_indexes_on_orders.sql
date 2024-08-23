350191fe5a2b901b1075acc1637cf8c83a956e36
ilya <ilya@cow.fi>
2024-08-23 12:09:30 +0300

    Redundant index

--- database/sql/V068__create_indexes_on_orders.sql
+++ database/sql/V068__create_indexes_on_orders.sql
@@ -1,2 +1,1 @@
-CREATE INDEX order_cancellation_timestamp ON orders USING BTREE (cancellation_timestamp);
 CREATE INDEX order_creation_cancellation ON orders USING BTREE (creation_timestamp, cancellation_timestamp);
