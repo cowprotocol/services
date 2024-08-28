c20b156b376c0cd38bf02598a238391619267a62
ilya <ilya@cow.fi>
2024-08-28 13:14:16 +0300

    More indexes

---
+++ database/sql/V069__create_index_on_order_execution.sql
@@ -0,0 +1,1 @@
+CREATE INDEX order_execution_block_number ON order_execution USING BTREE (block_number);
---
+++ database/sql/V070__create_index_on_ethflow_refunds.sql
@@ -0,0 +1,1 @@
+CREATE INDEX ethflow_refunds_block_number ON ethflow_refunds USING BTREE (block_number);
