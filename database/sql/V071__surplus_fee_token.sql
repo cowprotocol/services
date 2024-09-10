ad453eab89289a4bf6632d6abc34ad05b69cbc37
sunce86 <dusan.stanivukovic@gmail.com>
2024-09-10 15:44:24 +0200

    fix bugs

--- database/sql/V071__surplus_fee_token.sql
+++ database/sql/V071__surplus_fee_token.sql
@@ -2,12 +2,20 @@
 ALTER TABLE order_execution 
 ADD COLUMN surplus_fee_token bytea NOT NULL DEFAULT '\x0000000000000000000000000000000000000000';
 
--- Now populate existing rows with the sell token taken from the 'trades' table, JOIN-ing on the order uid.
+-- Now populate existing rows with the sell token taken from the 'orders' table, or if it doesn't exist, try from 'jit_orders'.
 UPDATE order_execution oe
-SET surplus_fee_token = COALESCE(t.sell_token, '\x0000000000000000000000000000000000000000')
-FROM trades t
-WHERE oe.order_uid = t.order_uid
-OR NOT EXISTS (SELECT 1 FROM trades WHERE order_uid = oe.order_uid);
+SET surplus_fee_token = COALESCE(
+    sub.sell_token, '\x0000000000000000000000000000000000000000'
+)
+FROM (
+    SELECT o.uid, o.sell_token
+    FROM orders o
+    UNION
+    SELECT j.uid, j.sell_token
+    FROM jit_orders j
+    WHERE NOT EXISTS (SELECT 1 FROM orders WHERE orders.uid = j.uid)
+) AS sub
+WHERE oe.order_uid = sub.uid;
 
 -- Drop the default value for the surplus_fee_token column
 ALTER TABLE order_execution ALTER COLUMN surplus_fee_token DROP DEFAULT;
