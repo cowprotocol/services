8514278d5f9755317666288218a305667afc6931
m-lord-renkse <160488334+m-lord-renkse@users.noreply.github.com>
2024-09-11 13:31:36 +0200

    Update database/sql/V071__drop_settlement_call_data.sql
    Co-authored-by: Martin Magnus <martin.beckmann@protonmail.com>

--- database/sql/V071__drop_settlement_call_data.sql
+++ database/sql/V071__drop_settlement_call_data.sql
@@ -1,2 +1,2 @@
---- We don't need to store then uninternalised and internalised calldata, so this table can be dropped
+--- We don't need to store the uninternalised and internalised calldata, so this table can be dropped
 DROP TABLE settlement_call_data;
