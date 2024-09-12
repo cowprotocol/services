04c52cb5be0e5bdeb14045c755752f459d90ea48
Mateo <mateo@cow.fi>
2024-09-12 09:51:59 +0200

    Extend migration comments

--- database/sql/V071__drop_settlement_call_data.sql
+++ database/sql/V071__drop_settlement_call_data.sql
@@ -1,2 +1,4 @@
 --- We don't need to store the uninternalised and internalised calldata, so this table can be dropped
+--- This is because we don't need to show/return the calldata from the competition endpoints for safety reasons
+--- See more: https://github.com/cowprotocol/services/issues/2942
 DROP TABLE settlement_call_data;
