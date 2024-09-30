acad568f6c1e1770cc87a8b619ffc7656d0fdec6
sunce86 <dusan.stanivukovic@gmail.com>
2024-09-30 09:51:24 +0200

    Cleanup competition::Solution

--- crates/autopilot/src/infra/solvers/dto/solve.rs
+++ crates/autopilot/src/infra/solvers/dto/solve.rs
@@ -59,7 +59,7 @@ impl Request {
 impl Response {
     pub fn into_domain(
         self,
-    ) -> Vec<Result<domain::competition::SolutionWithId, domain::competition::SolutionError>> {
+    ) -> Vec<Result<domain::competition::Solution, domain::competition::SolutionError>> {
         self.solutions
             .into_iter()
             .map(Solution::into_domain)
@@ -92,8 +92,8 @@ pub struct Token {
 impl Solution {
     pub fn into_domain(
         self,
-    ) -> Result<domain::competition::SolutionWithId, domain::competition::SolutionError> {
-        Ok(domain::competition::SolutionWithId::new(
+    ) -> Result<domain::competition::Solution, domain::competition::SolutionError> {
+        Ok(domain::competition::Solution::new(
             self.solution_id,
             self.submission_address.into(),
             domain::competition::Score::new(self.score.into())?,
