fa177a8d7098458091f1288cc26d93ffd7bbc59d
Mateo <mateo@cow.fi>
2024-10-07 15:32:06 +0200

    Change the e2e tests to be sync-to-blockchain

--- crates/e2e/src/setup/services.rs
+++ crates/e2e/src/setup/services.rs
@@ -144,6 +144,7 @@ impl<'a> Services<'a> {
 
         let args = [
             "autopilot".to_string(),
+            "--run-loop-mode=sync-to-blockchain".to_string(),
             "--auction-update-interval=1s".to_string(),
             "--run-loop-native-price-timeout=500ms".to_string(),
             format!("--ethflow-contract={:?}", self.contracts.ethflow.address()),
