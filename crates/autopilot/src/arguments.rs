54ee105ded4ab7733d960476697a7360ba7a9fa0
sunce86 <dusan.stanivukovic@gmail.com>
2024-10-07 12:03:47 +0200

    revert merge conflit

--- crates/autopilot/src/arguments.rs
+++ crates/autopilot/src/arguments.rs
@@ -291,6 +291,7 @@ impl std::fmt::Display for Arguments {
             max_winners_per_auction,
             execute_solver_competition_migration,
         } = self;
+
         write!(f, "{}", shared)?;
         write!(f, "{}", order_quoting)?;
         write!(f, "{}", http_client)?;
@@ -377,7 +378,9 @@ impl std::fmt::Display for Arguments {
         Ok(())
     }
 }
+
 /// A fee policy to be used for orders base on it's class.
+/// Examples:
 /// - Surplus with a high enough cap for limit orders: surplus:0.5:0.9:limit
 ///
 /// - Surplus with cap for market orders: surplus:0.5:0.06:market
