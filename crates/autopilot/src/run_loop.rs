4446f0fe062980929dc047a9571a21dab78ea947
sunce86 <dusan.stanivukovic@gmail.com>
2024-10-15 13:39:15 +0200

    initial commit

--- crates/autopilot/src/run_loop.rs
+++ crates/autopilot/src/run_loop.rs
@@ -85,10 +85,10 @@ impl RunLoop {
         // Added to make sure no more than one winner is activated by accident
         // Should be removed once we decide to activate "multiple winners per auction"
         // feature.
-        assert_eq!(
-            config.max_winners_per_auction, 1,
-            "only one winner is supported"
-        );
+        // assert_eq!(
+        //     config.max_winners_per_auction, 1,
+        //     "only one winner is supported"
+        // );
         Self {
             config,
             eth,
