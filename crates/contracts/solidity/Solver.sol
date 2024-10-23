6baeb4584883f70b96933617ff324525d60aed35
MartinquaXD <martin.beckmann@protonmail.com>
2024-10-23 19:19:07 +0000

    Revert "trade verification workaround for pre-interactions"
    This reverts commit 0e55848b00ad1bf8de192200bc1fe779f00be264.

--- crates/contracts/solidity/Solver.sol
+++ crates/contracts/solidity/Solver.sol
@@ -35,10 +35,6 @@ contract Solver {
     /// @param nativeToken - ERC20 version of the chain's token
     /// @param receiver - address receiving the bought tokens
     /// @param settlementCall - the calldata of the `settle()` call
-    /// @param mockPreconditions - controls whether things like ETH wrapping
-    ///            or setting allowance should be done on behalf of the
-    ///            user to support quote verification even if the user didn't
-    ///            wrap their ETH or set the necessary allowances yet.
     ///
     /// @return gasUsed - gas used for the `settle()` call
     /// @return queriedBalances - list of balances stored during the simulation
@@ -50,35 +46,19 @@ contract Solver {
         address buyToken,
         address nativeToken,
         address payable receiver,
-        bytes calldata settlementCall,
-        bool mockPreconditions
+        bytes calldata settlementCall
     ) external returns (
         uint256 gasUsed,
         uint256[] memory queriedBalances
     ) {
         require(msg.sender == address(this), "only simulation logic is allowed to call 'swap' function");
-
         // Prepare the trade in the context of the trader so we are allowed
         // to set approvals and things like that.
-        if (mockPreconditions) {
-            Trader(trader)
-                .prepareSwap(
-                    settlementContract,
-                    sellToken,
-                    sellAmount,
-                    nativeToken
-                );
-        }
-
-        // Warm the storage for sending ETH to smart contract addresses.
-        // We allow this call to revert becaues it was either unnecessary in the first place
-        // or failing to send `ETH` to the `receiver` will cause a revert in the settlement
-        // contract.
-        receiver.call{value: 0}("");
-
+        Trader(trader).prepareSwap(settlementContract, sellToken, sellAmount, nativeToken, receiver);
         this.storeBalance(sellToken, address(settlementContract), false);
         this.storeBalance(buyToken, address(settlementContract), false);
         uint256 gasStart = gasleft();
+        // TODO can we assume the overhead of this function call to be negligible due to inlining?
         address(settlementContract).doCall(settlementCall);
         gasUsed = gasStart - gasleft() - _simulationOverhead;
         this.storeBalance(sellToken, address(settlementContract), false);
@@ -89,7 +69,7 @@ contract Solver {
     /// @dev Helper function that reads the `owner`s balance for a given `token` and
     /// stores it. These stored balances will be returned as part of the simulation
     /// `Summary`.
-    /// @param token - which token we read the balance from
+    /// @param token - which token's we read the balance from
     /// @param owner - whos balance we are reading
     /// @param countGas - controls whether this gas cost should be discounted from the settlement gas.
     function storeBalance(address token, address owner, bool countGas) external {
