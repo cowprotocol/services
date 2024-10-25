74358536f924ec0789631cc1bb33c9d508c9023a
MartinquaXD <martin.beckmann@protonmail.com>
2024-10-25 21:37:59 +0000

    Use mimalloc

--- crates/driver/src/main.rs
+++ crates/driver/src/main.rs
@@ -1,3 +1,7 @@
+use mimalloc::MiMalloc;
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 #[tokio::main]
 async fn main() {
     driver::start(std::env::args()).await;
--- crates/solvers/src/main.rs
+++ crates/solvers/src/main.rs
@@ -1,3 +1,7 @@
+use mimalloc::MiMalloc;
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 #[tokio::main]
 async fn main() {
     solvers::start(std::env::args()).await;
