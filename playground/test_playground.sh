d5e062f94d6ebaf4f07c4de6f9f9742552d95176
Michal Strug <michal.strug@cow.fi>
2024-10-25 11:33:32 +0200

    Added allowances, cleanup

--- playground/test_playground.sh
+++ playground/test_playground.sh
@@ -7,23 +7,29 @@ set -u
 
 # Setup parameters
 HOST=localhost:8080
-WETHADDRESS="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2" # WETH token
-SELLTOKEN=$WETHADDRESS
-BUYTOKEN="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48" # USDC token
-RECEIVER="0xa0Ee7A142d267C1f36714E4a8F75612F20a79720"
-AMOUNT="1000000000000000000"
-PRIVATEKEY="0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6"
+WETH_ADDRESS="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"  # WETH token
+SELL_TOKEN=$WETH_ADDRESS
+BUY_TOKEN="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"  # USDC token
+AMOUNT="1000000000000000000"  # 1 ETH
+PRIVATE_KEY="0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6"
+COW_SETTLMENT_CONTRACT="0x9008D19f58AAbD9eD0D60971565AA8510560ab41"
+COW_VAULT_RELAYER_CONTRACT="0xC92E8bdf79f0507f65a392b0ab4667716BFE0110"
 APPDATA='{"version":"1.3.0","metadata":{}}'
+MAXUINT256="115792089237316195423570985008687907853269984665640564039457584007913129639935"
 
 # Run test flow
-echo "Using private key:" $PRIVATEKEY
+echo "Using private key:" $PRIVATE_KEY
+receiver=$(cast wallet address $PRIVATE_KEY)
 
 # Calculate AppData hash
 app_data_hash=$(cast keccak $APPDATA)
 
 # Deposit WETH
 echo "Wrapping some ETH"
-cast send --private-key $PRIVATEKEY --value 3ether $WETHADDRESS > /dev/null
+cast send --private-key $PRIVATE_KEY --value 3ether $WETH_ADDRESS > /dev/null
+
+echo "Setting WETH allowance"
+cast send --private-key $PRIVATE_KEY $WETH_ADDRESS "approve(address, uint)" $COW_VAULT_RELAYER_CONTRACT $MAXUINT256 > /dev/null
 
 echo "Request price qoute for buying USDC for WETH"
 quote_response=$( curl --fail-with-body -s -X 'POST' \
@@ -31,10 +37,10 @@ quote_response=$( curl --fail-with-body -s -X 'POST' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "sellToken": "'$SELLTOKEN'",
-  "buyToken": "'$BUYTOKEN'",
-  "from": "'$RECEIVER'",
-  "receiver": "'$RECEIVER'",
+  "sellToken": "'$SELL_TOKEN'",
+  "buyToken": "'$BUY_TOKEN'",
+  "from": "'$receiver'",
+  "receiver": "'$receiver'",
   "sellTokenBalance": "erc20",
   "buyTokenBalance": "erc20",
   "priceQuality": "fast",
@@ -48,7 +54,7 @@ quote_response=$( curl --fail-with-body -s -X 'POST' \
 buyAmount=$(jq -r --args '.quote.buyAmount' <<< "${quote_response}")
 feeAmount=$(jq -r --args '.quote.feeAmount' <<< "${quote_response}")
 validTo=$(($(date +%s) + 120)) # validity time: now + 2 minutes
-sellAmount=$((AMOUNT-feeAmount))
+sellAmount=$((AMOUNT - feeAmount))
 
 # Prepare EIP712 message
 eip712_message=$(jq -r --args '
@@ -88,7 +94,7 @@ eip712_typed_struct='{
     "name": "Gnosis Protocol",
     "version": "v2",
     "chainId": 1,
-    "verifyingContract": "0x9008D19f58AAbD9eD0D60971565AA8510560ab41"
+    "verifyingContract": "'$COW_SETTLMENT_CONTRACT'"
     },
   "message": '$eip712_message'
 }'
@@ -100,20 +106,20 @@ eip712_typed_struct=$(jq -r -c <<< "${eip712_typed_struct}")
 echo $eip712_typed_struct > tmp.json
 
 # Sign quote_response with private key
-signature=$(cast wallet sign --private-key $PRIVATEKEY --data --from-file tmp.json)
+signature=$(cast wallet sign --private-key $PRIVATE_KEY --data --from-file tmp.json)
 echo "Intent signature:" $signature
 
 app_data=${APPDATA//\"/\\\"}   # escape quotes for json field
 
 # Update EIP712 message with additional fields required for order submit
 order_proposal=$(jq -r -c --args '
-  .from="'$RECEIVER'" |
+  .from="'$receiver'" |
   .appData|="'$app_data'" |
   .appDataHash="'$app_data_hash'" |
   .signature="'$signature'"' <<< "${eip712_message}")
 
 echo "Submit an order"
-orderUid=$( curl -v -X 'POST' \
+orderUid=$( curl --fail-with-body -s -X 'POST' \
   "http://$HOST/api/v1/orders" \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
@@ -126,6 +132,13 @@ do
   orderStatus=$( curl --fail-with-body -s -X 'GET' \
     "http://$HOST/api/v1/orders/$orderUid/status" \
     -H 'accept: application/json' | jq -r '.type')
-  echo "Order status: $orderStatus"
+  echo -e -n "Order status: $orderStatus     \r"
+  if [ "$orderStatus" = "traded" ]; then
+    echo -e "\nSuccess"
+    exit 0
+  fi
   sleep 2
 done
+
+echo -e "\nTimeout"
+exit 1
