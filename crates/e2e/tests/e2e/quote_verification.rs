eb98aaf571f26d66897b5cd51d0a93efc5d07cc0
ilya <ilya@cow.fi>
2024-12-02 13:18:39 +0000

    Support JIT orders in the trade verifier (#3085)
    # Description
    Closes task n2 from https://github.com/cowprotocol/services/issues/3082
    by implementing support of the quotes with JIT orders in the trade
    verifier. For a gradual migration, it has to support both quote
    versions.
    
    # Changes
    
    - [ ] Added a new version of the trade with JIT orders.
    - [ ] Utilized the clearing prices to calculate the out amount.
    - [ ] Altered the `Solver.sol` helper contract to fetch all token
    balances as was proposed in [one of the
    comments](https://github.com/cowprotocol/services/pull/3085#discussion_r1830692576),
    which reduces the overall code complexity.
    - [ ] Bumped into an issue while converting floats into `BigRational`.
    ~~Implemented a workaround with converting float's string representation
    into `BigRational`.~~ Used `BigDecimal` in the config instead.
    
    ## How to test
    Unit tests. e2e would be possible only once the driver support is
    implemented(see https://github.com/cowprotocol/services/pull/3103).

--- crates/e2e/tests/e2e/quote_verification.rs
+++ crates/e2e/tests/e2e/quote_verification.rs
@@ -1,4 +1,5 @@
 use {
+    bigdecimal::{BigDecimal, Zero},
     e2e::setup::*,
     ethcontract::H160,
     ethrpc::Web3,
@@ -13,7 +14,7 @@ use {
             Estimate,
             Verification,
         },
-        trade_finding::{Interaction, Trade},
+        trade_finding::{Interaction, LegacyTrade, TradeKind},
     },
     std::{str::FromStr, sync::Arc},
 };
@@ -61,8 +62,10 @@ async fn test_bypass_verification_for_rfq_quotes(web3: Web3) {
         block_stream,
         onchain.contracts().gp_settlement.address(),
         onchain.contracts().weth.address(),
-        0.0,
-    );
+        BigDecimal::zero(),
+    )
+    .await
+    .unwrap();
 
     let verify_trade = |tx_origin| {
         let verifier = verifier.clone();
@@ -86,7 +89,7 @@ async fn test_bypass_verification_for_rfq_quotes(web3: Web3) {
                         sell_token_source: SellTokenSource::Erc20,
                         buy_token_destination: BuyTokenDestination::Erc20,
                     },
-                    Trade {
+                    TradeKind::Legacy(LegacyTrade {
                         out_amount: 16380122291179526144u128.into(),
                         gas_estimate: Some(225000),
                         interactions: vec![Interaction {
@@ -98,7 +101,7 @@ async fn test_bypass_verification_for_rfq_quotes(web3: Web3) {
                         solver: H160::from_str("0xe3067c7c27c1038de4e8ad95a83b927d23dfbd99")
                             .unwrap(),
                         tx_origin,
-                    },
+                    }),
                 )
                 .await
         }
--- crates/shared/src/trade_finding/mod.rs
+++ crates/shared/src/trade_finding/mod.rs
@@ -4,12 +4,19 @@
 pub mod external;
 
 use {
-    crate::price_estimation::{PriceEstimationError, Query},
-    anyhow::Result,
+    crate::{
+        conversions::U256Ext,
+        price_estimation::{PriceEstimationError, Query},
+        trade_finding::external::dto,
+    },
+    anyhow::{Context, Result},
     derive_more::Debug,
     ethcontract::{contract::MethodBuilder, tokens::Tokenize, web3::Transport, Bytes, H160, U256},
-    model::interaction::InteractionData,
+    model::{interaction::InteractionData, order::OrderKind},
+    num::CheckedDiv,
+    number::conversions::big_rational_to_u256,
     serde::Serialize,
+    std::{collections::HashMap, ops::Mul},
     thiserror::Error,
 };
 
@@ -21,7 +28,7 @@ use {
 #[async_trait::async_trait]
 pub trait TradeFinding: Send + Sync + 'static {
     async fn get_quote(&self, query: &Query) -> Result<Quote, TradeError>;
-    async fn get_trade(&self, query: &Query) -> Result<Trade, TradeError>;
+    async fn get_trade(&self, query: &Query) -> Result<TradeKind, TradeError>;
 }
 
 /// A quote.
@@ -32,9 +39,73 @@ pub struct Quote {
     pub solver: H160,
 }
 
-/// A trade.
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum TradeKind {
+    Legacy(LegacyTrade),
+    Regular(Trade),
+}
+
+impl TradeKind {
+    pub fn gas_estimate(&self) -> Option<u64> {
+        match self {
+            TradeKind::Legacy(trade) => trade.gas_estimate,
+            TradeKind::Regular(trade) => trade.gas_estimate,
+        }
+    }
+
+    pub fn solver(&self) -> H160 {
+        match self {
+            TradeKind::Legacy(trade) => trade.solver,
+            TradeKind::Regular(trade) => trade.solver,
+        }
+    }
+
+    pub fn tx_origin(&self) -> Option<H160> {
+        match self {
+            TradeKind::Legacy(trade) => trade.tx_origin,
+            TradeKind::Regular(trade) => trade.tx_origin,
+        }
+    }
+
+    pub fn out_amount(
+        &self,
+        buy_token: &H160,
+        sell_token: &H160,
+        in_amount: &U256,
+        order_kind: &OrderKind,
+    ) -> Result<U256> {
+        match self {
+            TradeKind::Legacy(trade) => Ok(trade.out_amount),
+            TradeKind::Regular(trade) => {
+                trade.out_amount(buy_token, sell_token, in_amount, order_kind)
+            }
+        }
+    }
+
+    pub fn interactions(&self) -> Vec<Interaction> {
+        match self {
+            TradeKind::Legacy(trade) => trade.interactions.clone(),
+            TradeKind::Regular(trade) => trade.interactions.clone(),
+        }
+    }
+
+    pub fn pre_interactions(&self) -> Vec<Interaction> {
+        match self {
+            TradeKind::Legacy(_) => Vec::new(),
+            TradeKind::Regular(trade) => trade.pre_interactions.clone(),
+        }
+    }
+}
+
+impl Default for TradeKind {
+    fn default() -> Self {
+        TradeKind::Legacy(LegacyTrade::default())
+    }
+}
+
+/// A legacy trade.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
-pub struct Trade {
+pub struct LegacyTrade {
     /// For sell orders: how many buy_tokens this trade will produce.
     /// For buy orders: how many sell_tokens this trade will cost.
     pub out_amount: U256,
@@ -47,6 +118,61 @@ pub struct Trade {
     /// If this is set the quote verification need to use this as the
     /// `tx.origin` to make the quote pass the simulation.
     pub tx_origin: Option<H160>,
+}
+
+/// A trade with JIT orders.
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct Trade {
+    pub clearing_prices: HashMap<H160, U256>,
+    /// How many units of gas this trade will roughly cost.
+    pub gas_estimate: Option<u64>,
+    /// The onchain calls to run before sending user funds to the settlement
+    /// contract.
+    pub pre_interactions: Vec<Interaction>,
+    /// Interactions needed to produce the expected trade amount.
+    pub interactions: Vec<Interaction>,
+    /// Which solver provided this trade.
+    pub solver: H160,
+    /// If this is set the quote verification need to use this as the
+    /// `tx.origin` to make the quote pass the simulation.
+    pub tx_origin: Option<H160>,
+    pub jit_orders: Vec<dto::JitOrder>,
+}
+
+impl Trade {
+    pub fn out_amount(
+        &self,
+        buy_token: &H160,
+        sell_token: &H160,
+        in_amount: &U256,
+        order_kind: &OrderKind,
+    ) -> Result<U256> {
+        let sell_price = self
+            .clearing_prices
+            .get(sell_token)
+            .context("clearing sell price missing")?
+            .to_big_rational();
+        let buy_price = self
+            .clearing_prices
+            .get(buy_token)
+            .context("clearing buy price missing")?
+            .to_big_rational();
+        let order_amount = in_amount.to_big_rational();
+
+        let out_amount = match order_kind {
+            OrderKind::Sell => order_amount
+                .mul(&sell_price)
+                .checked_div(&buy_price)
+                .context("div by zero: buy price")?
+                .ceil(), /* `ceil` is used to compute buy amount only: https://github.com/cowprotocol/contracts/blob/main/src/contracts/GPv2Settlement.sol#L389-L411 */
+            OrderKind::Buy => order_amount
+                .mul(&buy_price)
+                .checked_div(&sell_price)
+                .context("div by zero: sell price")?,
+        };
+
+        big_rational_to_u256(&out_amount).context("out amount is not a valid U256")
+    }
 }
 
 /// Data for a raw GPv2 interaction.
