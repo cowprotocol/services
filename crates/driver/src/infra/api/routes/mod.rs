441681fa8e31190018a816b776deb174d40736d1
sunce86 <dusan.stanivukovic@gmail.com>
2024-12-09 15:44:54 +0100

    Reveal and settle to receive integer solution id

--- crates/driver/src/infra/api/routes/mod.rs
+++ crates/driver/src/infra/api/routes/mod.rs
@@ -15,3 +15,34 @@ pub(super) use {
     settle::settle,
     solve::{solve, AuctionError},
 };
+
+pub fn deserialize_solution_id<'de, D>(deserializer: D) -> Result<u64, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    struct SolutionIdVisitor;
+
+    impl serde::de::Visitor<'_> for SolutionIdVisitor {
+        type Value = u64;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("a string or integer representing a solution ID")
+        }
+
+        fn visit_u64<E>(self, value: u64) -> Result<u64, E>
+        where
+            E: serde::de::Error,
+        {
+            Ok(value)
+        }
+
+        fn visit_str<E>(self, value: &str) -> Result<u64, E>
+        where
+            E: serde::de::Error,
+        {
+            value.parse::<u64>().map_err(serde::de::Error::custom)
+        }
+    }
+
+    deserializer.deserialize_any(SolutionIdVisitor)
+}
