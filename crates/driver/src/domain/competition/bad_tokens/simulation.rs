3fc408b8fbf6cc390dbef40d8a1c9f93b772104b
MartinquaXD <martin.beckmann@protonmail.com>
2024-12-17 10:46:25 +0000

    Add request sharing to bad token detection

--- crates/driver/src/domain/competition/bad_tokens/simulation.rs
+++ crates/driver/src/domain/competition/bad_tokens/simulation.rs
@@ -3,14 +3,19 @@ use {
         domain::{
             competition::{
                 bad_tokens::{cache::Cache, Quality},
+                order,
                 Order,
             },
             eth,
         },
         infra,
     },
+    futures::FutureExt,
     model::interaction::InteractionData,
-    shared::bad_token::{trace_call::TraceCallDetectorRaw, TokenQuality},
+    shared::{
+        bad_token::{trace_call::TraceCallDetectorRaw, TokenQuality},
+        request_sharing::BoxRequestSharing,
+    },
     std::{
         sync::Arc,
         time::{Duration, Instant},
@@ -23,6 +28,7 @@ pub struct Detector(Arc<Inner>);
 struct Inner {
     cache: Cache,
     detector: TraceCallDetectorRaw,
+    sharing: BoxRequestSharing<order::Uid, Option<Quality>>,
 }
 
 impl Detector {
@@ -32,6 +38,7 @@ impl Detector {
         Self(Arc::new(Inner {
             cache: Cache::new(max_age),
             detector,
+            sharing: BoxRequestSharing::labelled("bad_tokens".into()),
         }))
     }
 
@@ -45,42 +52,57 @@ impl Detector {
             return Some(quality);
         }
 
-        let token = order.sell.token;
-        let pre_interactions: Vec<_> = order
-            .pre_interactions
-            .iter()
-            .map(|i| InteractionData {
-                target: i.target.0,
-                value: i.value.0,
-                call_data: i.call_data.0.clone(),
-            })
-            .collect();
+        // The simulation detector gets used by multiple solvers at the same time
+        // and therefore will have to handle a lot of duplicate requests. To avoid
+        // doing duplicate work we use the `RequestSharing` component which checks
+        // if an equivalent request is already in-flight and awaits that instead of
+        // creating a new one.
+        let uid = order.uid;
+        self.0
+            .sharing
+            .shared_or_else(uid, move |_uid| {
+                let inner = self.0.clone();
+                let sell_token = order.sell.token;
+                let pre_interactions: Vec<_> = order
+                    .pre_interactions
+                    .iter()
+                    .map(|i| InteractionData {
+                        target: i.target.0,
+                        value: i.value.0,
+                        call_data: i.call_data.0.clone(),
+                    })
+                    .collect();
+                let trader = eth::Address::from(order.trader()).0;
+                let sell_amount = order.sell.amount.0;
 
-        match self
-            .0
-            .detector
-            .test_transfer(
-                eth::Address::from(order.trader()).0,
-                token.0 .0,
-                order.sell.amount.0,
-                &pre_interactions,
-            )
+                async move {
+                    let result = inner
+                        .detector
+                        .test_transfer(trader, sell_token.0 .0, sell_amount, &pre_interactions)
+                        .await;
+                    match result {
+                        Err(err) => {
+                            tracing::debug!(?err, "failed to determine token quality");
+                            None
+                        }
+                        Ok(TokenQuality::Good) => {
+                            inner
+                                .cache
+                                .update_quality(sell_token, Quality::Supported, now);
+                            Some(Quality::Supported)
+                        }
+                        Ok(TokenQuality::Bad { reason }) => {
+                            tracing::debug!(reason, "cache token as unsupported");
+                            inner
+                                .cache
+                                .update_quality(sell_token, Quality::Unsupported, now);
+                            Some(Quality::Unsupported)
+                        }
+                    }
+                }
+                .boxed()
+            })
             .await
-        {
-            Err(err) => {
-                tracing::debug!(?err, "failed to determine token quality");
-                None
-            }
-            Ok(TokenQuality::Good) => {
-                cache.update_quality(token, Quality::Supported, now);
-                Some(Quality::Supported)
-            }
-            Ok(TokenQuality::Bad { reason }) => {
-                tracing::debug!(reason, "cache token as unsupported");
-                cache.update_quality(token, Quality::Unsupported, now);
-                Some(Quality::Unsupported)
-            }
-        }
     }
 }
 
