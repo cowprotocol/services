e8f6962fe889dcecf3b48e93b0b083b0d914f304
ilya <ilya@cow.fi>
2024-12-18 17:27:34 +0000

    Docs

--- crates/driver/src/domain/competition/bad_tokens/metrics.rs
+++ crates/driver/src/domain/competition/bad_tokens/metrics.rs
@@ -5,6 +5,11 @@ use {
     std::{collections::HashSet, sync::Arc},
 };
 
+/// Monitors tokens to determine whether they are considered "unsupported" based
+/// on the number of consecutive participation in failing settlement encoding.
+/// Tokens that consistently participate in failures beyond a predefined
+/// threshold are marked as unsupported. Once token participates in a successful
+/// settlement encoding, it is removed from the cache.
 #[derive(Default)]
 pub struct Detector(Arc<Inner>);
 
@@ -14,7 +19,9 @@ struct Inner {
 }
 
 impl Detector {
-    const UNSUPPORTED_THRESHOLD: u64 = 10;
+    /// Defines the threshold for the number of consecutive unsupported
+    /// quality detections before a token is considered unsupported.
+    const UNSUPPORTED_THRESHOLD: u64 = 5;
 
     pub fn get_quality(&self, token: eth::TokenAddress) -> Option<Quality> {
         self.0
@@ -24,6 +31,7 @@ impl Detector {
             .map(|_| Quality::Unsupported)
     }
 
+    /// Increments the counter of failures for each token.
     pub fn update_failing_tokens(&self, tokens: HashSet<eth::TokenAddress>) {
         for token in tokens {
             self.0
@@ -34,6 +42,8 @@ impl Detector {
         }
     }
 
+    /// Removes tokens from the cache since they all participated in a
+    /// successful settlement encoding.
     pub fn update_successful_tokens(&self, tokens: HashSet<eth::TokenAddress>) {
         for token in tokens {
             self.0.counter.remove(&token);
