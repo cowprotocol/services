b3ac58707397de548d3d14989b0707cea6c4f494
Dusan Stanivukovic <dusan.stanivukovic@gmail.com>
2024-12-25 14:37:47 +0100

    Limit subquery results to 1 (#3181)
    # Description
    Fixes failing of queries when orders with multiple order executions are
    queried from database.
    
    Introduced in https://github.com/cowprotocol/services/pull/2966
    
    # Changes
    <!-- List of detailed changes (how the change is accomplished) -->
    
    - [ ] In case mutliple rows exist, return only one. Right now it's
    guaranteed that all of them are the same.
    
    ## How to test
    trust me bro
    will be tested on staging since all staging pods are down
    
    <!--
    ## Related Issues
    
    Fixes #
    -->

--- crates/database/src/jit_orders.rs
+++ crates/database/src/jit_orders.rs
@@ -32,7 +32,7 @@ NULL AS ethflow_data,
 NULL AS onchain_user,
 NULL AS onchain_placement_error,
 COALESCE((SELECT SUM(executed_fee) FROM order_execution oe WHERE oe.order_uid = o.uid), 0) as executed_fee,
-COALESCE((SELECT executed_fee_token FROM order_execution oe WHERE oe.order_uid = o.uid), o.sell_token) as executed_fee_token, -- TODO surplus token
+COALESCE((SELECT executed_fee_token FROM order_execution oe WHERE oe.order_uid = o.uid LIMIT 1), o.sell_token) as executed_fee_token, -- TODO surplus token
 NULL AS full_app_data
 "#;
 
