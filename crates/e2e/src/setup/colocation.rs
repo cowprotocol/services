131b90595c74d4182dd5e83caa32a2152d47544e
ilya <ilya@cow.fi>
2025-01-17 18:35:34 +0000

    e2e fix

--- crates/e2e/src/setup/colocation.rs
+++ crates/e2e/src/setup/colocation.rs
@@ -186,6 +186,7 @@ mempool = "public"
     let args = vec![
         "driver".to_string(),
         format!("--config={}", config_file.display()),
+        "--orderbook-url=http://localhost:8080".to_string(),
         format!("--ethrpc={NODE_HOST}"),
     ];
 
