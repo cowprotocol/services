c67fe231a267c322438bbd6263e78b6c9ffa9acf
Mateo <mateo@cow.fi>
2025-03-14 12:43:48 +0100

    rework comment

--- crates/orderbook/src/api/get_order_status.rs
+++ crates/orderbook/src/api/get_order_status.rs
@@ -29,7 +29,7 @@ pub fn get_status(
                     StatusCode::NOT_FOUND,
                 ),
                 Err(orderbook::Error::NotFound) => reply::with_status(
-                    super::error("NotFound", "Competition was not found"),
+                    super::error("NotFound", "Order status was not found"),
                     StatusCode::NOT_FOUND,
                 ),
                 Err(err) => {
