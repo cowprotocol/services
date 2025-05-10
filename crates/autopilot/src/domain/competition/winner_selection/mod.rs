98002ff7bdf23a940d4548ae88e4cd87738e134e
MartinquaXD <martin.beckmann@protonmail.com>
2025-05-10 12:05:14 +0000

    Make fairness check test stable again and document ordering assumptions

--- crates/autopilot/src/domain/competition/winner_selection/mod.rs
+++ crates/autopilot/src/domain/competition/winner_selection/mod.rs
@@ -9,19 +9,29 @@ use {
 
 pub mod max_score;
 
+/// Implements auction arbitration in 3 phases:
+/// 1. filter unfair solutions
+/// 2. mark winners
+/// 3. compute reference scores
+///
+/// The functions assume the `Arbitrator` is the only one
+/// changing the ordering or the `participants.
 pub trait Arbitrator: Send + Sync + 'static {
+    /// Removes unfair solutions from the set of all solutions.
+    fn filter_unfair_solutions(
+        &self,
+        participants: Vec<Participant<Unranked>>,
+        auction: &Auction,
+    ) -> Vec<Participant<Unranked>>;
+
     /// Picks winners and sorts all solutions where winners come before losers
     /// and higher scores come before lower scores.
-    fn mark_winners(&self, solutions: Vec<Participant<Unranked>>) -> Vec<Participant>;
+    fn mark_winners(&self, participants: Vec<Participant<Unranked>>) -> Vec<Participant>;
 
     /// Computes the reference scores which are used to compute
     /// rewards for the winning solvers.
-    fn compute_reference_scores(&self, solutions: &[Participant]) -> HashMap<eth::Address, Score>;
-
-    /// Removes unfair solutions from the set of all solutions.
-    fn filter_solutions(
+    fn compute_reference_scores(
         &self,
-        solutions: Vec<Participant<Unranked>>,
-        auction: &Auction,
-    ) -> Vec<Participant<Unranked>>;
+        participants: &[Participant],
+    ) -> HashMap<eth::Address, Score>;
 }
