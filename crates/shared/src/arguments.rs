c7105d8ede9ec62267c5836245f100925f9a1e77
Miguel Naveira <47919901+mrnaveira@users.noreply.github.com>
2025-05-14 08:44:22 +0100

    Add support for JSON log format (#3336)
    # Description
    Structured logs offer several advantages for our services:
    - Improved troubleshooting and root cause analysis, as the flow of a
    request can be more easily traced.
    - Simpler log correlation.
    - Easier integration and automation.
    
    This PR introduces a new parameter, `use_json_format`, for log output
    (currently set to `false` by default). When enabled, the `tracing` crate
    will emit logs in JSON format instead of the standard plain text.
    
    Below is an example of a log entry from the `autopilot` service using
    the new structured format:
    ```json
    {"timestamp":"2025-03-25T12:12:42.492Z","level":"DEBUG","fields":{"message":"stored order events","label":"Executing","count":1,"elapsed":"7.604417ms"},"target":"autopilot::database::order_events","span":{"auction_id":1057,"name":"auction"},"spans":[{"auction_id":1057,"name":"auction"}]}
    ```
    
    Related to https://github.com/cowprotocol/services/issues/3282
    
    # Changes
    - Added the `json` feature to the `tracing-subscriber` crate, and
    updated the version to the latest.
    - Introduced a new `log_use_json_format` parameter to specify whether to
    use structured logging. It is disabled by default.
    - Used across all crates that perform logging: `orderbook`, `autopilot`,
    `driver`, `solver`, `alerter`, and `refunder`.
    - Since logging is encapsulated within the `observer` crate, the
    creation of the new JSON logging layer only needed to be implemented
    there.
    - The new parameter is currently set to `false` in all unit and E2E
    tests.
    
    ## How to test
    Testing was done by running e2e tests in both scenarios (new parameter
    enabled and disabled) and inspecting the resulting logs for the
    services.

--- crates/shared/src/arguments.rs
+++ crates/shared/src/arguments.rs
@@ -34,6 +34,9 @@ macro_rules! logging_args_with_default_filter {
 
             #[clap(long, env, default_value = "error")]
             pub log_stderr_threshold: LevelFilter,
+
+            #[clap(long, env, default_value = "false")]
+            pub use_json_logs: bool,
         }
 
         impl ::std::fmt::Display for $struct_name {
@@ -41,10 +44,12 @@ macro_rules! logging_args_with_default_filter {
                 let Self {
                     log_filter,
                     log_stderr_threshold,
+                    use_json_logs,
                 } = self;
 
                 writeln!(f, "log_filter: {}", log_filter)?;
                 writeln!(f, "log_stderr_threshold: {}", log_stderr_threshold)?;
+                writeln!(f, "use_json_logs: {}", use_json_logs)?;
                 Ok(())
             }
         }
