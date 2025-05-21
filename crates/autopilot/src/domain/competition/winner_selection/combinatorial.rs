c6e2a8228afb566e56023c2473a8d5136a34d149
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2025-05-21 16:28:57 +0100

    use order side when creating a trade

--- crates/autopilot/src/domain/competition/winner_selection/combinatorial.rs
+++ crates/autopilot/src/domain/competition/winner_selection/combinatorial.rs
@@ -1599,7 +1599,7 @@ mod tests {
         executed_buy: eth::U256,
     ) -> TradedOrder {
         TradedOrder {
-            side: order::Side::Sell,
+            side: order.side,
             sell: order.sell,
             buy: order.buy,
             executed_sell: executed_sell.into(),
