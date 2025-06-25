92bfbaaf4bbf72a51e3ce670efc233dc5b86e5cc
markin.io <markin.io210@gmail.com>
2025-06-25 18:48:59 +0300

    Add Notifier at infra layer

--- crates/driver/src/infra/notify/liquidity_source/mod.rs
+++ crates/driver/src/infra/notify/liquidity_source/mod.rs
@@ -1,19 +1,33 @@
 pub mod config;
 
+use std::sync::Arc;
 pub use config::Config;
-use crate::infra;
+use crate::{infra, boundary};
+use crate::boundary::notifier::LiquiditySourcesNotifying;
+use crate::domain::competition::solution::settlement::Settlement;
 
 /// Auctions and settlement notifier for liquidity sources
+#[derive(Debug, Clone)]
 pub struct Notifier {
+    inner: Arc<boundary::notifier::Notifier>,
 }
 
 impl Notifier {
-    pub fn new(_config: &infra::notify::liquidity_source::Config) -> Self {
-        Self {}
+    pub fn try_new(config: &infra::notify::liquidity_source::Config, chain_id: u64) -> Result<Self, Error> {
+        Ok(Self {
+            inner: Arc::new(boundary::notifier::Notifier::try_new(config, chain_id)?)
+        })
     }
 
     /// Sends notification to liquidity sources before settlement
-    pub async fn notify_before_settlement(&self) {
-        todo!()
+    pub async fn notify_before_settlement(&self, settlement: &Settlement) -> Result<(), Error> {
+        let _ = self.inner.notify_before_settlement(settlement).await?;
+        Ok(())
     }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("boundary error: {0:?}")]
+    Boundary(#[from] boundary::Error),
 }
