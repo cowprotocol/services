eeb249766c5fc0b6237bf047cba522545b401e65
ilya <ilya@cow.fi>
2025-07-07 17:48:58 +0300

    Fix

--- crates/observe/src/tracing.rs
+++ crates/observe/src/tracing.rs
@@ -205,10 +205,14 @@ impl Injector for HeaderInjector<'_> {
     /// Set a key and value in the HeaderMap. Does nothing if the key or value
     /// are not valid inputs.
     fn set(&mut self, key: &str, value: String) {
-        if let Ok(name) = http::header::HeaderName::from_bytes(key.as_bytes())
-            && let Ok(val) = http::header::HeaderValue::from_str(&value)
-        {
-            self.0.insert(name, val);
+        match (
+            http::header::HeaderName::from_bytes(key.as_bytes()),
+            http::header::HeaderValue::from_str(&value),
+        ) {
+            (Ok(name), Ok(val)) => {
+                self.0.insert(name, val);
+            }
+            _ => {}
         }
     }
 }
