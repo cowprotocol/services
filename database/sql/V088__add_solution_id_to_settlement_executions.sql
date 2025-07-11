c5049cb4df5041d4a76e3dc35ce83de18e834165
ilya <ilya@cow.fi>
2025-07-11 18:05:16 +0300

    Missing file

---
+++ database/sql/V088__add_solution_id_to_settlement_executions.sql
@@ -0,0 +1,12 @@
+-- Add nullable column `solution_uid`
+ALTER TABLE settlement_executions
+    ADD COLUMN solution_uid BIGINT;
+
+-- Drop existing primary key
+ALTER TABLE settlement_executions
+    DROP CONSTRAINT settlement_executions_pkey;
+
+-- Recreate primary key
+ALTER TABLE settlement_executions
+    ADD CONSTRAINT settlement_executions_pkey
+        PRIMARY KEY (auction_id, solution_uid);
