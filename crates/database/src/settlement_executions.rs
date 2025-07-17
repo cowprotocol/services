1f57acd10e371770c096e654a65f667ffa2cd7cd
ilya <ilya@cow.fi>
2025-07-17 13:11:20 +0300

    Updated tests

--- crates/database/src/settlement_executions.rs
+++ crates/database/src/settlement_executions.rs
@@ -93,6 +93,18 @@ mod tests {
         )
         .await
         .unwrap();
+
+        insert(
+            &mut db,
+            auction_id,
+            solver_a,
+            2,
+            start_timestamp,
+            start_block,
+            deadline_block,
+        )
+        .await
+        .unwrap();
         insert(
             &mut db,
             auction_id,
@@ -106,7 +118,7 @@ mod tests {
         .unwrap();
 
         let output = fetch(&mut db, auction_id).await.unwrap();
-        assert_eq!(output.len(), 2);
+        assert_eq!(output.len(), 3);
         let expected_a = ExecutionRow {
             auction_id,
             solver: solver_a,
@@ -120,6 +132,17 @@ mod tests {
         };
         let expected_b = ExecutionRow {
             auction_id,
+            solver: solver_a,
+            solution_uid: 2,
+            start_timestamp,
+            end_timestamp: None,
+            start_block,
+            end_block: None,
+            deadline_block,
+            outcome: None,
+        };
+        let expected_c = ExecutionRow {
+            auction_id,
             solver: solver_b,
             solution_uid: 1,
             start_timestamp,
@@ -131,10 +154,12 @@ mod tests {
         };
         assert!(output.contains(&expected_a));
         assert!(output.contains(&expected_b));
+        assert!(output.contains(&expected_c));
 
         let end_timestamp_a = now_truncated_to_microseconds();
         let end_block_a = 8;
-        let outcome_a = "success".to_string();
+        let success_outcome = "success".to_string();
+        let failure_outcome = "failure".to_string();
         update(
             &mut db,
             auction_id,
@@ -142,14 +167,24 @@ mod tests {
             1,
             end_timestamp_a,
             end_block_a,
-            outcome_a.clone(),
+            success_outcome.clone(),
+        )
+        .await
+        .unwrap();
+        update(
+            &mut db,
+            auction_id,
+            solver_a,
+            2,
+            end_timestamp_a,
+            end_block_a,
+            failure_outcome.clone(),
         )
         .await
         .unwrap();
 
         let end_timestamp_b = now_truncated_to_microseconds();
         let end_block_b = 10;
-        let outcome_b = "failure".to_string();
         update(
             &mut db,
             auction_id,
@@ -157,13 +192,13 @@ mod tests {
             1,
             end_timestamp_b,
             end_block_b,
-            outcome_b.clone(),
+            success_outcome.clone(),
         )
         .await
         .unwrap();
 
         let output = fetch(&mut db, auction_id).await.unwrap();
-        assert_eq!(output.len(), 2);
+        assert_eq!(output.len(), 3);
         let expected_a = ExecutionRow {
             auction_id,
             solver: solver_a,
@@ -173,9 +208,20 @@ mod tests {
             start_block,
             end_block: Some(end_block_a),
             deadline_block,
-            outcome: Some(outcome_a),
+            outcome: Some(success_outcome.clone()),
         };
         let expected_b = ExecutionRow {
+            auction_id,
+            solver: solver_a,
+            solution_uid: 2,
+            start_timestamp,
+            end_timestamp: Some(end_timestamp_a),
+            start_block,
+            end_block: Some(end_block_a),
+            deadline_block,
+            outcome: Some(failure_outcome),
+        };
+        let expected_c = ExecutionRow {
             auction_id,
             solver: solver_b,
             solution_uid: 1,
@@ -184,10 +230,11 @@ mod tests {
             start_block,
             end_block: Some(end_block_b),
             deadline_block,
-            outcome: Some(outcome_b),
+            outcome: Some(success_outcome),
         };
         assert!(output.contains(&expected_a));
         assert!(output.contains(&expected_b));
+        assert!(output.contains(&expected_c));
     }
 
     #[derive(Debug, Clone, Eq, PartialEq, sqlx::FromRow)]
