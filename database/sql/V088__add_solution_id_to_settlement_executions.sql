eaa48be92340daf0c7b83018795d0535f42427ad
ilya <ilya@cow.fi>
2025-07-17 13:06:47 +0300

    Fixed comment

--- database/sql/V088__add_solution_id_to_settlement_executions.sql
+++ database/sql/V088__add_solution_id_to_settlement_executions.sql
@@ -1,15 +1,15 @@
--- Clean up the table
+-- Empty the table
 DELETE FROM settlement_executions;
 
 -- Add the new column `solution_uid`
 ALTER TABLE settlement_executions
     ADD COLUMN solution_uid BIGINT NOT NULL;
 
--- Drop existing primary key
+-- Drop the existing primary key
 ALTER TABLE settlement_executions
     DROP CONSTRAINT settlement_executions_pkey;
 
--- Recreate primary key
+-- Recreate the primary key
 ALTER TABLE settlement_executions
     ADD CONSTRAINT settlement_executions_pkey
         PRIMARY KEY (auction_id, solver, solution_uid);
