cowprotocol/services

commit 95ecc4e01b7fd06ec0b71c6486cb2cdd962e5040

Tokio console and log filter reloading (#2868)
# Description
https://github.com/cowprotocol/services/pull/2815 reverted 2 changes
(https://github.com/cowprotocol/services/pull/2791,
https://github.com/cowprotocol/services/pull/2792) because we again saw
issues with logs that were suddenly no longer emitted.
This problem also happened the last time we merged support for
`tokio-console` but I hoped that:
1. the issue was resolved in one of the related dependencies somehow
2. making the feature opt-in with a CLI flag would allow normal
operations when disabled

However, even with the feature disabled we saw that logs were again
missing. I tried to debug the underlying issue and it appears that for
some reason a log like `::tracing::event!(target: "sqlx::query",
tracing::Level::DEBUG, summary);` would not get logged with the original
implementation (not a huge deal since we weren't using them) but these
logs would also cause the NEXT log that would normally be emitted to be
skipped.
This can be seen in this
[code](https://github.com/cowprotocol/services/commit/5ba466668781d60bdf13531187f6baa1eac9fd89)
which reproduced the error.

Unfortunately `sqlx` is using `tracing::event!()` when executing an SQL
query which means with the described problem it could happen randomly
that some logs are missing. Luckily there are also scenarios where some
SQL query always gets executed before a log we issue ourselves (e.g.
`saving fee policies` log in the autopilot run loop would always be
skipped) which made it easier to find the underlying cause.

# Changes
1. Add `tokio-console` and `filter reloading` feature again
2. Fiddled around with how the tracing subscriber gets initialized until
the test that reproduced the issue no longer skipped any logs. This was
not straight forward since initializing the tokio console and our
regular log filters requires you to jump through a few type system
hoops.

crates/observe/src/tracing.rs
@@ -1,8 +1,15 @@
 use {
+    crate::tracing_reload_handler::spawn_reload_handler,
     std::{panic::PanicInfo, sync::Once},
     time::macros::format_description,
     tracing::level_filters::LevelFilter,
-    tracing_subscriber::fmt::{time::UtcTime, writer::MakeWriterExt as _},
+    tracing_subscriber::{
+        fmt::{time::UtcTime, writer::MakeWriterExt as _},
+        prelude::*,
+        util::SubscriberInitExt,
+        EnvFilter,
+        Layer,
+    },
 };
 
 /// Initializes tracing setup that is shared between the binaries.
@@ -28,22 +35,67 @@ pub fn initialize_reentrant(env_filter: &str) {
 }
 
 fn set_tracing_subscriber(env_filter: &str, stderr_threshold: LevelFilter) {
-    // This is what kibana uses to separate multi line log messages.
-    let subscriber_builder = tracing_subscriber::fmt::fmt()
-        .with_timer(UtcTime::new(format_description!(
-            "[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:3]Z"
-        )))
-        .with_env_filter(env_filter)
-        .with_ansi(atty::is(atty::Stream::Stdout));
-    match stderr_threshold.into_level() {
-        Some(threshold) => subscriber_builder
-            .with_writer(
-                std::io::stderr
-                    .with_max_level(threshold)
-                    .or_else(std::io::stdout),
-            )
-            .init(),
-        None => subscriber_builder.init(),
+    let initial_filter = env_filter.to_string();
+
+    // The `tracing` APIs are heavily generic to enable zero overhead. Unfortunately
+    // this leads to very annoying type constraints which can only be satisfied
+    // by literally copy and pasting the code so the compiler doesn't try to
+    // infer types that satisfy both the tokio-console and the regular case.
+    // It's tempting to resolve this mess by first configuring the `fmt_layer` and
+    // only then the `console_subscriber`. However, this setup was the only way
+    // I found that:
+    // 1. actually makes `tokio-console` work
+    // 2. prints logs if `tokio-console` is disabled
+    // 3. does NOT skip the next log following a `tracing::event!()`. These calls
+    //    happen for example under the hood in `sqlx`. I don't understand what's
+    //    actually causing that but at this point I'm just happy if all the features
+    //    work correctly.
+    macro_rules! fmt_layer {
+        ($env_filter:expr, $stderr_threshold:expr) => {{
+            tracing_subscriber::fmt::layer()
+                .with_writer(
+                    std::io::stdout
+                        .with_min_level(
+                            $stderr_threshold
+                                .into_level()
+                                .unwrap_or(tracing::Level::ERROR),
+                        )
+                        .or_else(std::io::stderr),
+                )
+                .with_timer(UtcTime::new(format_description!(
+                    "[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:3]Z"
+                )))
+                .with_ansi(atty::is(atty::Stream::Stdout))
+                .with_filter($env_filter)
+        }};
+    }
+
+    if cfg!(tokio_unstable) {
+        let (env_filter, reload_handle) =
+            tracing_subscriber::reload::Layer::new(EnvFilter::new(&initial_filter));
+
+        tracing_subscriber::registry()
+            .with(console_subscriber::spawn())
+            .with(fmt_layer!(env_filter, stderr_threshold))
+            .init();
+
+        if cfg!(unix) {
+            spawn_reload_handler(initial_filter, reload_handle);
+        }
+    } else {
+        let (env_filter, reload_handle) =
+            tracing_subscriber::reload::Layer::new(EnvFilter::new(&initial_filter));
+
+        tracing_subscriber::registry()
+            // Without this the subscriber ignores the next log after an `tracing::event!()` which
+            // `sqlx` uses under the hood.
+            .with(tracing::level_filters::LevelFilter::TRACE)
+            .with(fmt_layer!(env_filter, stderr_threshold))
+            .init();
+
+        if cfg!(unix) {
+            spawn_reload_handler(initial_filter, reload_handle);
+        }
     }
 }
 

.cargo/config.toml
@@ -1,0 +1,2 @@
+[build]
+rustflags = ["--cfg", "tokio_unstable"]

Cargo.toml
@@ -47,7 +47,7 @@ tempfile = "3.10.1"
 time = { version = "0.3.36", features = ["macros"] }
 thiserror = "1.0.61"
 toml = "0.8.14"
-tokio = "1.38.0"
+tokio = { version = "1.38.0", features = ["tracing"] }
 tokio-stream = { version = "0.1.15", features = ["sync"] }
 tracing = "0.1.40"
 tracing-subscriber = "0.3.18"

crates/observe/Cargo.toml
@@ -6,7 +6,8 @@ edition = "2021"
 license = "MIT OR Apache-2.0"
 
 [dependencies]
-atty = "0.2.14"
+atty = "0.2"
+console-subscriber = "0.3.0"
 futures = { workspace = true }
 once_cell = { workspace = true }
 pin-project-lite = "0.2.14"

crates/observe/build.rs
@@ -1,0 +1,4 @@
+fn main() {
+    // Make build system aware of custom config flags to avoid clippy warnings
+    println!("cargo::rustc-check-cfg=cfg(tokio_unstable)");
+}

crates/observe/src/lib.rs
@@ -6,3 +6,6 @@ pub mod metrics;
 pub mod panic_hook;
 pub mod request_id;
 pub mod tracing;
+
+#[cfg(unix)]
+mod tracing_reload_handler;

crates/observe/src/tracing_reload_handler.rs
@@ -1,0 +1,113 @@
+use {
+    tokio::{
+        io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+        net::{UnixListener, UnixStream},
+    },
+    tracing_subscriber::{reload, EnvFilter},
+};
+
+/// Spawns a new thread that listens for connections to a UNIX socket
+/// at "/tmp/log_filter_override_<process_name>_<pid>".
+/// Whenever a line gets writtedn to that socket the reload handler
+/// uses it as the new log filter.
+/// To reset to the original log filter send the message "reset".
+pub(crate) fn spawn_reload_handler<T: 'static>(
+    initial_filter: String,
+    reload_handle: reload::Handle<EnvFilter, T>,
+) {
+    tokio::spawn(async move {
+        let id = std::process::id();
+        let name = binary_name().unwrap_or_default();
+
+        let socket_path = format!("/tmp/log_filter_override_{name}_{id}.sock");
+        tracing::warn!(file = socket_path, "open log filter reload socket");
+        let handle = SocketHandle {
+            listener: UnixListener::bind(&socket_path).expect("socket handle is unique"),
+            socket_path,
+        };
+
+        loop {
+            handle_connection(&handle.listener, &initial_filter, &reload_handle).await;
+        }
+    });
+}
+
+struct SocketHandle {
+    socket_path: String,
+    listener: UnixListener,
+}
+
+impl Drop for SocketHandle {
+    fn drop(&mut self) {
+        let _ = std::fs::remove_file(&self.socket_path);
+    }
+}
+
+fn binary_name() -> Option<String> {
+    Some(
+        std::env::current_exe()
+            .ok()?
+            .file_name()?
+            .to_str()?
+            .to_string(),
+    )
+}
+
+async fn handle_connection<T>(
+    listener: &UnixListener,
+    initial_filter: &str,
+    reload_handle: &reload::Handle<EnvFilter, T>,
+) {
+    let Ok((mut socket, _addr)) = listener.accept().await else {
+        tracing::warn!("failed to accept UNIX socket connection");
+        return;
+    };
+
+    let _ = socket
+        .write_all(format!("log filter on process startup was: {initial_filter:?}\n",).as_bytes())
+        .await;
+
+    loop {
+        let message = read_line(&mut socket).await;
+
+        let filter = match message.as_deref() {
+            Some("") => {
+                log(&mut socket, "client terminated connection".into()).await;
+                break;
+            }
+            None => {
+                log(&mut socket, "failed to read message from socket".into()).await;
+                continue;
+            }
+            Some("reset") => initial_filter,
+            Some(message) => message,
+        };
+
+        let Ok(env_filter) = EnvFilter::try_new(filter) else {
+            log(&mut socket, format!("failed to parse filter: {filter:?}")).await;
+            continue;
+        };
+
+        match reload_handle.reload(env_filter) {
+            Ok(_) => log(&mut socket, format!("applied new filter: {filter:?}")).await,
+            Err(err) => log(&mut socket, format!("failed to apply filter: {err:?}")).await,
+        }
+    }
+}
+
+async fn read_line(socket: &mut UnixStream) -> Option<String> {
+    let mut reader = BufReader::new(socket);
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer).await.ok()?;
+    Some(buffer.trim().to_owned())
+}
+
+/// Logs the message in this process' logs and reports it back to the
+/// connected socket.
+async fn log(socket: &mut UnixStream, message: String) {
+    // Use a fairly high log level to improve chances that this actually gets logged
+    // when somebody messed with the log filter.
+    tracing::warn!(message);
+    let _ = socket.write_all(message.as_bytes()).await;
+    let _ = socket.write_all(b"\n").await;
+}

crates/orderbook/src/run.rs
@@ -412,9 +412,8 @@ pub async fn run(args: Arguments) {
             shutdown_sender.send(()).expect("failed to send shutdown signal");
             match tokio::time::timeout(Duration::from_secs(10), serve_api).await {
                 Ok(inner) => inner.expect("API failed during shutdown"),
-                Err(_) => tracing::error!("API shutdown exceeded timeout"),
+                Err(_) => panic!("API shutdown exceeded timeout"),
             }
-            std::process::exit(0);
         }
     };
 }

commit e0dc59d5de5f841416b88499a41aa6e7c8930384

Fix `stderr_threshold` setting (#3498)
# Description
Follow up item of a post mortem.
During the post mortem it was discussed that we did not get any slack
alerts despite emitting logs with the `error` log level. It was stated
that it was a deliberate decision to stop emitting these errors as slack
alerts due to noisy. However, my investigation suggests that this was
not deliberate and the functionality simply broke in #2868.

The infra repo still configures `stderr_threshold` as `error` and the
`stderr_threshold` config was never optional so some number of logs was
always expected to go to stderr.

# Changes
- fixed the bug that caused us to stop writing to stderr for logs that
had the correct level
- made `stderr_threshold` optional and disabled by default
- converted `LevelFilter` config member arguments to `Level` as that can
better express optionality of arguments. You can't use `LevelFilter` to
express that you want to always want logging to happen because you can
populate it with `LevelFilter::Off` which disables logging, that's not
possible with `Level` because that does not have the `OFF` variant. To
express optionality with `Level` you have to use `Option<Level>` which
communicates intent very clearly.

## How to test
Ran e2e tests with test logs with different levels and
`stderr_threshold` settings, piped `stderr` to a file and verified that
it only contains logs that were supposed to show up.

crates/observe/src/config.rs
@@ -1,4 +1,4 @@
-use {core::time::Duration, tracing::level_filters::LevelFilter};
+use {core::time::Duration, tracing::Level};
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -6,7 +6,7 @@ pub struct Config {
     /// https://docs.rs/tracing-subscriber/latest/tracing_subscriber/filter/struct.EnvFilter.html
     pub(crate) env_filter: String,
     /// Minimum level threshold for stderr output
-    pub(crate) stderr_threshold: LevelFilter,
+    pub(crate) stderr_threshold: Option<Level>,
     /// Output log events as JSON
     pub(crate) use_json_format: bool,
     /// Tracing config
@@ -16,7 +16,7 @@ pub struct Config {
 impl Config {
     pub fn new(
         env_filter: &str,
-        stderr_threshold: LevelFilter,
+        stderr_threshold: Option<Level>,
         use_json_format: bool,
         tracing_config: Option<TracingConfig>,
     ) -> Self {
@@ -39,8 +39,8 @@ impl Config {
         self
     }
 
-    pub fn with_stderr_threshold(mut self, stderr_threshold: LevelFilter) -> Self {
-        self.stderr_threshold = stderr_threshold;
+    pub fn with_stderr_threshold(mut self, stderr_threshold: Level) -> Self {
+        self.stderr_threshold = Some(stderr_threshold);
         self
     }
 
@@ -54,7 +54,7 @@ impl Default for Config {
     fn default() -> Self {
         Self {
             env_filter: "info".to_string(),
-            stderr_threshold: LevelFilter::ERROR,
+            stderr_threshold: None,
             use_json_format: false,
             tracing: None,
         }
@@ -70,7 +70,7 @@ pub struct TracingConfig {
     /// Timeout for exporting spans to collector
     pub(crate) export_timeout: Duration,
     /// Level of traces that should be collected
-    pub(crate) level: LevelFilter,
+    pub(crate) level: Level,
 }
 
 impl TracingConfig {
@@ -78,7 +78,7 @@ impl TracingConfig {
         collector_endpoint: String,
         service_name: String,
         export_timeout: Duration,
-        level: LevelFilter,
+        level: Level,
     ) -> Self {
         Self {
             collector_endpoint,

crates/observe/src/panic_hook.rs
@@ -37,12 +37,7 @@ mod tests {
     #[test]
     #[ignore]
     fn manual_thread() {
-        let obs_config = Config::new(
-            "info",
-            tracing::level_filters::LevelFilter::OFF,
-            false,
-            None,
-        );
+        let obs_config = Config::new("info", None, false, None);
         crate::tracing::initialize(&obs_config);
 
         // Should print panic trace log but not kill the process.
@@ -61,12 +56,7 @@ mod tests {
     #[tokio::test(flavor = "multi_thread")]
     #[ignore]
     async fn manual_tokio() {
-        let obs_config = Config::new(
-            "info",
-            tracing::level_filters::LevelFilter::OFF,
-            false,
-            None,
-        );
+        let obs_config = Config::new("info", None, false, None);
         crate::tracing::initialize(&obs_config);
 
         let handle = tokio::task::spawn(async { panic!("you should see this message") });

crates/observe/src/tracing.rs
@@ -72,15 +72,18 @@ fn set_tracing_subscriber(config: &Config) {
     //    happen for example under the hood in `sqlx`. I don't understand what's
     //    actually causing that but at this point I'm just happy if all the features
     //    work correctly.
+
     macro_rules! fmt_layer {
         ($env_filter:expr_2021, $stderr_threshold:expr_2021, $use_json_format:expr_2021) => {{
-            let writer = std::io::stdout
-                .with_min_level(
-                    $stderr_threshold
-                        .into_level()
-                        .unwrap_or(tracing::Level::ERROR),
-                )
-                .or_else(std::io::stderr);
+            let stderr_threshold = $stderr_threshold.clone();
+            let writer = std::io::stderr
+                .with_filter(move |meta| {
+                    // if the log is at most as verbose as our stderr threshold we log it to
+                    // stderr. For example if the threshold is WARN all WARN and ERROR logs
+                    // will get written to stderr.
+                    stderr_threshold.is_some_and(|min_verbosity| meta.level() <= &min_verbosity)
+                })
+                .or_else(std::io::stdout);
             let timer = UtcTime::new(format_description!(
                 "[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:3]Z"
             ));
@@ -140,7 +143,7 @@ fn set_tracing_subscriber(config: &Config) {
         Some(
             tracing_opentelemetry::layer()
                 .with_tracer(tracer)
-                .with_filter(tracing_config.level),
+                .with_filter(LevelFilter::from_level(tracing_config.level)),
         )
     } else {
         tracing::info!("no tracing layer set up");

crates/refunder/src/arguments.rs
@@ -3,7 +3,6 @@ use {
     ethcontract::H160,
     shared::{arguments::display_option, ethrpc, http_client, logging_args_with_default_filter},
     std::time::Duration,
-    tracing::level_filters::LevelFilter,
     url::Url,
 };
 

crates/shared/src/arguments.rs
@@ -21,7 +21,6 @@ use {
         str::FromStr,
         time::Duration,
     },
-    tracing::level_filters::LevelFilter,
     url::Url,
 };
 
@@ -33,8 +32,8 @@ macro_rules! logging_args_with_default_filter {
             #[clap(long, env, default_value = $default_filter)]
             pub log_filter: String,
 
-            #[clap(long, env, default_value = "error")]
-            pub log_stderr_threshold: LevelFilter,
+            #[clap(long, env)]
+            pub log_stderr_threshold: Option<tracing::Level>,
 
             #[clap(long, env, default_value = "false")]
             pub use_json_logs: bool,
@@ -49,7 +48,7 @@ macro_rules! logging_args_with_default_filter {
                 } = self;
 
                 writeln!(f, "log_filter: {}", log_filter)?;
-                writeln!(f, "log_stderr_threshold: {}", log_stderr_threshold)?;
+                writeln!(f, "log_stderr_threshold: {:?}", log_stderr_threshold)?;
                 writeln!(f, "use_json_logs: {}", use_json_logs)?;
                 Ok(())
             }
@@ -112,8 +111,8 @@ logging_args_with_default_filter!(
 pub struct TracingArguments {
     #[clap(long, env)]
     pub tracing_collector_endpoint: Option<String>,
-    #[clap(long, env, default_value_t = LevelFilter::INFO)]
-    pub tracing_level: LevelFilter,
+    #[clap(long, env, default_value_t = tracing::Level::INFO)]
+    pub tracing_level: tracing::Level,
     #[clap(long, env, value_parser = humantime::parse_duration, default_value = "10s")]
     pub tracing_exporter_timeout: Duration,
 }

crates/e2e/src/setup/mod.rs
@@ -185,7 +185,7 @@ async fn run<F, Fut, T>(
 {
     let obs_config = observe::Config::new(
         &with_default_filters(filters).join(","),
-        tracing::Level::ERROR.into(),
+        Some(tracing::Level::ERROR),
         false,
         None,
     );