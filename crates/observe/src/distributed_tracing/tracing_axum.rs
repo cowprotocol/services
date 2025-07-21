79c0ab0104ad3a39845b2bfbfb1fa698996eee11
Jan P <patomil@gmail.com>
2025-07-21 17:46:39 +0200

    Attach log to solver

--- crates/observe/src/distributed_tracing/tracing_axum.rs
+++ crates/observe/src/distributed_tracing/tracing_axum.rs
@@ -25,7 +25,10 @@ pub fn make_span<B>(request: &Request<B>) -> Span {
     let request_id = request_id(request.headers());
 
     let span = info_span!("http_request", ?request_id, trace_id = field::Empty);
-    info!(uri = %request.uri(), method = %request.method(), "HTTP request");
+    {
+        let _span = span.enter();
+        info!(uri = %request.uri(), method = %request.method(), "HTTP request");
+    }
 
     span.set_parent(parent_context);
 
