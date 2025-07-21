7a0d745e0dc3becf8b12f376130158ff543fbfe1
Jan P <patomil@gmail.com>
2025-07-21 17:36:26 +0200

    Add a log line with method and path

--- crates/observe/src/distributed_tracing/tracing_axum.rs
+++ crates/observe/src/distributed_tracing/tracing_axum.rs
@@ -25,6 +25,8 @@ pub fn make_span<B>(request: &Request<B>) -> Span {
     let request_id = request_id(request.headers());
 
     let span = info_span!("http_request", ?request_id, trace_id = field::Empty);
+    info!(uri = %request.uri(), method = %request.method(), "HTTP request");
+
     span.set_parent(parent_context);
     {
         let _span = span.enter();
