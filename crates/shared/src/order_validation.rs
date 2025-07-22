f966d9cae158283c4c3bfb3e2de405ec625fe013
ilya <ilya@cow.fi>
2025-07-22 11:58:08 +0300

    Revert "comment out broken balance check for testing purposes"
    This reverts commit e1c3df5aadcd19996edeab6d9e6a27b3f1246fcf.

--- crates/shared/src/order_validation.rs
+++ crates/shared/src/order_validation.rs
@@ -626,11 +626,8 @@ impl OrderValidating for OrderValidator {
             verification,
         };
 
-        // TODO: fix this check on lens, for now disable the logic to
-        // find more blockers
-        let _ = self
-            .ensure_token_is_transferable(&order, owner, &app_data)
-            .await;
+        self.ensure_token_is_transferable(&order, owner, &app_data)
+            .await?;
 
         // Check if we need to re-classify the market order if it is outside the market
         // price. We consider out-of-price orders as liquidity orders. See
