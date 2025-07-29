04816c357db8a55d59069daf8c7a4384e9a495aa
ilya <ilya@cow.fi>
2025-07-29 13:58:53 +0300

    Test fix

--- crates/e2e/tests/e2e/hooks.rs
+++ crates/e2e/tests/e2e/hooks.rs
@@ -100,7 +100,11 @@ async fn gas_limit(web3: Web3) {
     );
     let error = services.create_order(&order).await.unwrap_err();
     assert_eq!(error.0, StatusCode::BAD_REQUEST);
-    assert!(error.1.contains("TooMuchGas"));
+    assert!(
+        error
+            .1
+            .contains("Executing order requires too many gas units")
+    );
 }
 
 async fn allowance(web3: Web3) {
