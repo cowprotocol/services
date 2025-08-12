4e36771f5d96ec979631b4d673949e6ff3f82f8c
ilya <ilya@cow.fi>
2025-08-12 12:00:28 +0300

    Fix tests

--- crates/e2e/tests/e2e/limit_orders.rs
+++ crates/e2e/tests/e2e/limit_orders.rs
@@ -74,10 +74,10 @@ async fn forked_node_mainnet_single_limit_order() {
     .await;
 }
 
-const FORK_BLOCK_GNOSIS: u64 = 32070725;
+const FORK_BLOCK_GNOSIS: u64 = 41502478;
 /// USDC whale address as per [FORK_BLOCK_GNOSIS].
 const USDC_WHALE_GNOSIS: H160 = H160(hex_literal::hex!(
-    "ba12222222228d8ba445958a75a0704d566bf2c8"
+    "d4A39d219ADB43aB00739DC5D876D98Fdf0121Bf"
 ));
 
 #[tokio::test]
