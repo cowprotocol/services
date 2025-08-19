2049a9d8919e63bb67c368d26e9837a33f731ce2
markin.io <markin.io210@gmail.com>
2025-08-19 20:08:29 +0100

    Implement Liquorice API for e2e tests

--- crates/e2e/src/api/liquorice.rs
+++ crates/e2e/src/api/liquorice.rs
@@ -1,3 +1,4 @@
+pub use api::LiquoriceApi;
 use {
     crate::setup::TestAccount,
     ethcontract::common::abi::{Token, encode},
@@ -9,6 +10,67 @@ use {
         types::{H160, U256},
     },
 };
+
+mod api {
+    use {
+        driver::infra::notify::liquidity_sources::liquorice::client::request::v1::intent_origin::notification,
+        serde_json::json,
+        std::{convert::Infallible, net::SocketAddr, sync::Arc},
+        tokio::sync::{Mutex, MutexGuard},
+        warp::{Filter, Rejection},
+    };
+
+    pub struct State {
+        pub notification_requests: Vec<notification::post::Request>,
+    }
+
+    pub struct LiquoriceApi {
+        state: Arc<Mutex<State>>,
+        pub port: u16,
+    }
+
+    impl LiquoriceApi {
+        /// Creates new mocked `LiquoriceApi` with internal state
+        pub async fn start() -> Self {
+            let state = Arc::new(Mutex::new(State {
+                notification_requests: Default::default(),
+            }));
+
+            let addr: SocketAddr = ([0, 0, 0, 0], 0).into();
+            let server = warp::serve(Self::notification_route(state.clone()));
+            let (addr, server) = server.bind_ephemeral(addr);
+            let port = addr.port();
+            assert!(port > 0, "assigned port must be greater than 0");
+
+            tokio::spawn(async move {
+                server.await;
+            });
+
+            tracing::info!("Started Liquorice API server at {}", addr);
+
+            Self { state, port }
+        }
+
+        pub fn notification_route(
+            state: Arc<Mutex<State>>,
+        ) -> impl Filter<Extract = (warp::reply::Json,), Error = Rejection> + Clone {
+            warp::path!("intent-origin" / "notification")
+                .and(warp::post())
+                .and(warp::body::json::<notification::post::Request>())
+                .and_then(move |request| {
+                    let state = state.clone();
+                    async move {
+                        state.lock().await.notification_requests.push(request);
+                        Ok::<warp::reply::Json, Infallible>(warp::reply::json(&json!({})))
+                    }
+                })
+        }
+
+        pub async fn get_state(&self) -> MutexGuard<'_, State> {
+            self.state.lock().await
+        }
+    }
+}
 
 #[derive(Copy, Clone, Default, Eq, PartialEq)]
 pub struct DomainSeparator(pub [u8; 32]);
@@ -106,6 +168,7 @@ impl Eip712TypedLiquoriceSingleOrder {
         }
     }
 
+    // TODO: update
     // See <https://github.com/0xProject/protocol/blob/%400x/contracts-zero-ex%400.49.0/contracts/zero-ex/contracts/src/features/libs/LibNativeOrder.sol#L166-L195>
     pub fn hash_struct(&self) -> [u8; 32] {
         let order_data_part_1 = {
