bb6f90032b70a069534ebb7e3b6f72643c044704
extrawurst <mail@rusticorn.com>
2025-08-19 15:53:08 +0100

    two step migration

--- database/sql/V089__add_creation_timestamp_to_appdata_and_orderquotes.sql
+++ database/sql/V089__add_creation_timestamp_to_appdata_and_orderquotes.sql
@@ -1,10 +1,10 @@
 
 -- Add the columns as nullable first
 ALTER TABLE app_data
-    ADD COLUMN creation_timestamp timestamptz;
+    ADD COLUMN creation_timestamp timestamptz NOT NULL SET DEFAULT '1970-01-01 00:00:00+00'::timestamptz;
 
 ALTER TABLE order_quotes
-    ADD COLUMN creation_timestamp timestamptz;
+    ADD COLUMN creation_timestamp timestamptz NOT NULL SET DEFAULT '1970-01-01 00:00:00+00'::timestamptz;
 
 -- Set default for future inserts
 ALTER TABLE app_data
@@ -12,19 +12,3 @@ ALTER TABLE app_data
 
 ALTER TABLE order_quotes
     ALTER COLUMN creation_timestamp SET DEFAULT NOW();
-
--- Update existing rows with sentinel value (Unix epoch)
-UPDATE app_data
-SET creation_timestamp = '1970-01-01 00:00:00+00'::timestamptz
-WHERE creation_timestamp IS NULL;
-
-UPDATE order_quotes
-SET creation_timestamp = '1970-01-01 00:00:00+00'::timestamptz
-WHERE creation_timestamp IS NULL;
-
--- Make the columns NOT NULL
-ALTER TABLE app_data
-    ALTER COLUMN creation_timestamp SET NOT NULL;
-
-ALTER TABLE order_quotes
-    ALTER COLUMN creation_timestamp SET NOT NULL;
