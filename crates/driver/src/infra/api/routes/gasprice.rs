c559e96e305190deb12ff3e5ef43500ed8942a58
Felix Leupold <felixleupold90@gmail.com>
2025-08-20 11:17:34 +0000

    fmt

--- crates/driver/src/infra/api/routes/gasprice.rs
+++ crates/driver/src/infra/api/routes/gasprice.rs
@@ -10,13 +10,8 @@ use {
     tracing::instrument,
 };
 
-pub(in crate::infra::api) fn gasprice(
-    app: axum::Router<Ethereum>,
-) -> axum::Router<Ethereum> {
-    app.route(
-        "/gasprice",
-        axum::routing::get(route),
-    )
+pub(in crate::infra::api) fn gasprice(app: axum::Router<Ethereum>) -> axum::Router<Ethereum> {
+    app.route("/gasprice", axum::routing::get(route))
 }
 
 #[serde_as]
@@ -37,9 +32,7 @@ async fn route(
     eth: axum::extract::State<Ethereum>,
 ) -> Result<Json<GasPriceResponse>, (hyper::StatusCode, axum::Json<Error>)> {
     // For simplicity we use the default time limit (None)
-    let gas_price = eth
-        .gas_price(None)
-        .await?;
+    let gas_price = eth.gas_price(None).await?;
 
     Ok(Json(GasPriceResponse {
         max_fee_per_gas: gas_price.max().0.0,
