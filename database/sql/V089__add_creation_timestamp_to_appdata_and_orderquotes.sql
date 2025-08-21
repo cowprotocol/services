78702d478303b4a4c9a93b617c65928851fca3ec
extrawurst <776816+extrawurst@users.noreply.github.com>
2025-08-21 17:45:46 +0200

    fix V089 migration (#3575)
    fix SQL syntax expecting SET in `ALTER COLUMN` but not in `ADD COLUMN`
    
    # Description
    Invalid SQL migration syntax got merged because 'just' a failing
    migration does not trigger the CI to fail.
    
    followup to https://github.com/cowprotocol/services/pull/3534
    
    # Changes
    do not use `SET DEFAULT` in `ADD COLUMN` but just `DEFAULT`
    
    ## How to test
    observe migration log output actually successfully running migration

--- database/sql/V089__add_creation_timestamp_to_appdata_and_orderquotes.sql
+++ database/sql/V089__add_creation_timestamp_to_appdata_and_orderquotes.sql
@@ -2,10 +2,10 @@
 -- in order to help data ingestion for our analytics team
 
 ALTER TABLE app_data
-    ADD COLUMN creation_timestamp timestamptz NOT NULL SET DEFAULT '1970-01-01 00:00:00+00'::timestamptz;
+    ADD COLUMN creation_timestamp timestamptz NOT NULL DEFAULT '1970-01-01 00:00:00+00'::timestamptz;
 
 ALTER TABLE order_quotes
-    ADD COLUMN creation_timestamp timestamptz NOT NULL SET DEFAULT '1970-01-01 00:00:00+00'::timestamptz;
+    ADD COLUMN creation_timestamp timestamptz NOT NULL DEFAULT '1970-01-01 00:00:00+00'::timestamptz;
 
 -- Set default for future inserts
 ALTER TABLE app_data
