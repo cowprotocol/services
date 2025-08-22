b1f262e60ea8cc4c0fade310796f1a28c1e0267d
ilya <ilya@cow.fi>
2025-08-22 19:18:06 +0300

    Features

--- crates/autopilot/src/main.rs
+++ crates/autopilot/src/main.rs
@@ -1,7 +1,22 @@
+// Conditional global allocator selection based on environment variable
+#[cfg(all(feature = "jemalloc", not(feature = "mimalloc-allocator")))]
 #[global_allocator]
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
+#[cfg(all(feature = "mimalloc-allocator", not(feature = "jemalloc")))]
+#[global_allocator]
+static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 #[tokio::main]
 async fn main() {
+    #[cfg(feature = "jemalloc")]
+    tracing::info!("Using jemalloc allocator");
+
+    #[cfg(feature = "mimalloc-allocator")]
+    tracing::info!("Using mimalloc allocator");
+
+    #[cfg(not(any(feature = "jemalloc", feature = "mimalloc-allocator")))]
+    tracing::info!("Using system default allocator");
+
     autopilot::start(std::env::args()).await;
 }
