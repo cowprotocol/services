062a577b1cd56cf9306cdd53e34b0ad60d7a43be
ilya <ilya@cow.fi>
2025-08-22 10:22:31 +0300

    Deduplicate balance simulation code (#3558)
    # Description
    This is a follow-up to #3551 based on the following suggestion:
    https://github.com/cowprotocol/services/pull/3551#discussion_r2276154335
    Basically, balance simulation is implemented in the driver and shared
    crates and this code is duplicated. This PR addresses this.
    
    # Changes
    
    - [ ] Deduplicate the balances simulation code by keeping the logic only
    in the shared crate and extracting it into a new BalancSimulator struct.
    
    ## How to test
    Existing tests.

--- crates/shared/src/account_balances/mod.rs
+++ crates/shared/src/account_balances/mod.rs
@@ -1,5 +1,6 @@
 use {
-    anyhow::Result,
+    alloy::sol_types::{SolType, sol_data},
+    ethcontract::{Bytes, contract::MethodBuilder, dyns::DynTransport},
     ethrpc::{Web3, block_stream::CurrentBlockWatcher},
     model::{
         interaction::InteractionData,
@@ -7,6 +8,7 @@ use {
     },
     primitive_types::{H160, U256},
     std::sync::Arc,
+    thiserror::Error,
 };
 
 mod cached;
@@ -50,7 +52,7 @@ impl From<anyhow::Error> for TransferSimulationError {
 pub trait BalanceFetching: Send + Sync {
     // Returns the balance available to the allowance manager for the given owner
     // and token taking both balance as well as "allowance" into account.
-    async fn get_balances(&self, queries: &[Query]) -> Vec<Result<U256>>;
+    async fn get_balances(&self, queries: &[Query]) -> Vec<anyhow::Result<U256>>;
 
     // Check that the settlement contract can make use of this user's token balance.
     // This check could fail if the user does not have enough balance, has not
@@ -65,32 +67,139 @@ pub trait BalanceFetching: Send + Sync {
     ) -> Result<(), TransferSimulationError>;
 }
 
-/// Contracts required for balance simulation.
-pub struct Contracts {
-    pub settlement: contracts::GPv2Settlement,
-    pub balances: contracts::support::Balances,
-    pub vault_relayer: H160,
-    pub vault: Option<H160>,
-}
-
 /// Create the default [`BalanceFetching`] instance.
-pub fn fetcher(web3: &Web3, contracts: Contracts) -> Arc<dyn BalanceFetching> {
-    Arc::new(simulation::Balances::new(
-        web3,
-        contracts.settlement,
-        contracts.balances,
-        contracts.vault_relayer,
-        contracts.vault,
-    ))
+pub fn fetcher(web3: &Web3, balance_simulator: BalanceSimulator) -> Arc<dyn BalanceFetching> {
+    Arc::new(simulation::Balances::new(web3, balance_simulator))
 }
 
 /// Create a cached [`BalanceFetching`] instance.
 pub fn cached(
     web3: &Web3,
-    contracts: Contracts,
+    balance_simulator: BalanceSimulator,
     blocks: CurrentBlockWatcher,
 ) -> Arc<dyn BalanceFetching> {
-    let cached = Arc::new(cached::Balances::new(fetcher(web3, contracts)));
+    let cached = Arc::new(cached::Balances::new(fetcher(web3, balance_simulator)));
     cached.spawn_background_task(blocks);
     cached
+}
+
+pub struct BalanceSimulator {
+    settlement: contracts::GPv2Settlement,
+    balances: contracts::support::Balances,
+    vault_relayer: H160,
+    vault: H160,
+}
+
+impl BalanceSimulator {
+    pub fn new(
+        settlement: contracts::GPv2Settlement,
+        balances: contracts::support::Balances,
+        vault_relayer: H160,
+        vault: Option<H160>,
+    ) -> Self {
+        Self {
+            settlement,
+            vault_relayer,
+            vault: vault.unwrap_or_default(),
+            balances,
+        }
+    }
+
+    pub fn vault_relayer(&self) -> H160 {
+        self.vault_relayer
+    }
+
+    pub fn vault(&self) -> H160 {
+        self.vault
+    }
+
+    pub async fn simulate<F, Fut>(
+        &self,
+        owner: H160,
+        token: H160,
+        source: SellTokenSource,
+        interactions: &[InteractionData],
+        amount: Option<U256>,
+        add_access_lists: F,
+    ) -> Result<Simulation, SimulationError>
+    where
+        F: FnOnce(MethodBuilder<DynTransport, Bytes<Vec<u8>>>) -> Fut,
+        Fut: Future<Output = MethodBuilder<DynTransport, Bytes<Vec<u8>>>>,
+    {
+        // We simulate the balances from the Settlement contract's context. This
+        // allows us to check:
+        // 1. How the pre-interactions would behave as part of the settlement
+        // 2. Simulate the actual VaultRelayer transfers that would happen as part of a
+        //    settlement
+        //
+        // This allows us to end up with very accurate balance simulations.
+        let balance_call = self.balances.balance(
+            (self.settlement.address(), self.vault_relayer, self.vault),
+            owner,
+            token,
+            amount.unwrap_or_default(),
+            Bytes(source.as_bytes()),
+            interactions
+                .iter()
+                .map(|i| (i.target, i.value, Bytes(i.call_data.clone())))
+                .collect(),
+        );
+
+        let delegate_call = self
+            .settlement
+            .simulate_delegatecall(
+                self.balances.address(),
+                Bytes(balance_call.tx.data.unwrap_or_default().0),
+            )
+            .from(crate::SIMULATION_ACCOUNT.clone());
+
+        let delegate_call = add_access_lists(delegate_call).await;
+
+        let response = delegate_call.call().await?;
+        let (token_balance, allowance, effective_balance, can_transfer) =
+            <(
+                sol_data::Uint<256>,
+                sol_data::Uint<256>,
+                sol_data::Uint<256>,
+                sol_data::Bool,
+            )>::abi_decode(&response.0)
+            .map_err(|err| {
+                tracing::error!(?err, "failed to decode balance response");
+                web3::error::Error::Decoder("failed to decode balance response".to_string())
+            })?;
+
+        let simulation = Simulation {
+            token_balance: U256::from_little_endian(&token_balance.as_le_bytes()),
+            allowance: U256::from_little_endian(&allowance.as_le_bytes()),
+            effective_balance: U256::from_little_endian(&effective_balance.as_le_bytes()),
+            can_transfer,
+        };
+
+        tracing::trace!(
+            ?owner,
+            ?token,
+            ?source,
+            ?amount,
+            ?interactions,
+            ?simulation,
+            "simulated balances"
+        );
+        Ok(simulation)
+    }
+}
+
+#[derive(Debug)]
+pub struct Simulation {
+    pub token_balance: U256,
+    pub allowance: U256,
+    pub effective_balance: U256,
+    pub can_transfer: bool,
+}
+
+#[derive(Debug, Error)]
+pub enum SimulationError {
+    #[error("method error: {0:?}")]
+    Method(#[from] ethcontract::errors::MethodError),
+    #[error("web3 error: {0:?}")]
+    Web3(#[from] web3::error::Error),
 }
