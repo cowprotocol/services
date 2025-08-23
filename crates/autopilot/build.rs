727e5f1b4eb17bc6326a270b2275daebe8436657
ilya <ilya@cow.fi>
2025-08-23 10:07:14 +0300

    Naming

--- crates/autopilot/build.rs
+++ crates/autopilot/build.rs
@@ -13,7 +13,7 @@ fn main() -> Result<()> {
     // Handle allocator selection based on _RJEM_MALLOC_CONF environment variable
     // If _RJEM_MALLOC_CONF is set, use jemalloc; otherwise use mimalloc
     if std::env::var("_RJEM_MALLOC_CONF").is_ok() {
-        println!("cargo:rustc-cfg=feature=\"jemalloc\"");
+        println!("cargo:rustc-cfg=feature=\"jemalloc-allocator\"");
     } else {
         println!("cargo:rustc-cfg=feature=\"mimalloc-allocator\"");
     }
--- crates/autopilot/src/main.rs
+++ crates/autopilot/src/main.rs
@@ -1,21 +1,21 @@
 // Conditional global allocator selection based on environment variable
-#[cfg(all(feature = "jemalloc", not(feature = "mimalloc-allocator")))]
+#[cfg(all(feature = "jemalloc-allocator", not(feature = "mimalloc-allocator")))]
 #[global_allocator]
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
-#[cfg(all(feature = "mimalloc-allocator", not(feature = "jemalloc")))]
+#[cfg(all(feature = "mimalloc-allocator", not(feature = "jemalloc-allocator")))]
 #[global_allocator]
 static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[tokio::main]
 async fn main() {
-    #[cfg(feature = "jemalloc")]
+    #[cfg(feature = "jemalloc-allocator")]
     tracing::info!("Using jemalloc allocator");
 
     #[cfg(feature = "mimalloc-allocator")]
     tracing::info!("Using mimalloc allocator");
 
-    #[cfg(not(any(feature = "jemalloc", feature = "mimalloc-allocator")))]
+    #[cfg(not(any(feature = "jemalloc-allocator", feature = "mimalloc-allocator")))]
     tracing::info!("Using system default allocator");
 
     autopilot::start(std::env::args()).await;
