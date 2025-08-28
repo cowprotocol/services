7a90f2f966960816fa17553fda463773650f9177
MartinquaXD <martin.beckmann@protonmail.com>
2025-08-28 09:14:59 +0000

    Optimize helper contract

--- crates/contracts/solidity/Balances.sol
+++ crates/contracts/solidity/Balances.sol
@@ -2,103 +2,76 @@
 pragma solidity ^0.8.17;
 
 import { IERC20 } from "./interfaces/IERC20.sol";
-import { ISettlement, Interaction } from "./interfaces/ISettlement.sol";
+import { ISettlement } from "./interfaces/ISettlement.sol";
 import { IVault } from "./interfaces/IVault.sol";
 import { Transfer, IVaultRelayer } from "./interfaces/IVaultRelayer.sol";
 
 /// @title A contract for simulating available balances for settlements.
 contract Balances {
-    /// @dev The on-chain contracts required for the simulation.
-    struct Contracts {
-        ISettlement settlement;
-        IVaultRelayer vaultRelayer;
-        IVault vault;
+    ISettlement public immutable settlement;
+    IVaultRelayer public immutable vaultRelayer;
+    IVault public immutable vault;
+
+    constructor(
+        ISettlement _settlement,
+        IVaultRelayer _vaultRelayer,
+        IVault _vault
+    ) {
+        settlement = _settlement;
+        vaultRelayer = _vaultRelayer;
+        vault = _vault;
     }
 
-    /// @dev Retrieves the current effective balance for use with CoW Protocol.
-    ///
-    /// @param contracts - On-chain contract addresses required for the
-    /// simulation.
-    /// @param trader - The address of the account intending to trade on CoW
-    /// Protocol.
-    /// @param token - The address of the sell token of the trader.
-    /// @param amount - The amount of tokens to attempt to transfer. If this
-    /// value is specfied as 0, then the entire effective balance will be
-    /// transferred.
-    /// @param source - The balance source, this can either be the Keccak-256
-    /// digest of "erc20", "external", or "internal". This corresponds to the
-    /// `sellTokenBalance` field of the CoW Protocol order.
-    /// @param interactions - A list of pre-interactions required for setting
-    /// up balances and/or allowances.
-    ///
-    /// @return tokenBalance - The token balance of the user.
-    /// @return allowance - The allowance set by the user to the Protocol.
-    /// @return effectiveBalance - The effective balance of the user. This
-    /// represents a balance that can be transferred into the settlement
-    /// contract when executing a trade.
-    /// @return canTransfer - Returns whether or not the transfer into the
-    /// settlement contract for the specified amount would succeed.
-    function balance(
-        Contracts memory contracts,
+    struct Interaction {
+        address target;
+        bytes callData;
+    }
+
+    function balanceErc20(
         address trader,
         IERC20 token,
-        uint256 amount,
-        bytes32 source,
         Interaction[] calldata interactions
     ) external returns (
         uint256 tokenBalance,
-        uint256 allowance,
-        uint256 effectiveBalance,
-        bool canTransfer
+        uint256 allowance
     ) {
-        // Execute the interactions within the current context. This ensures
-        // that any pre-interactions that setup balances and/or allowances
-        // are executed before reading them.
-        executeInteractions(contracts, interactions);
+        executeInteractions(interactions);
+        tokenBalance = token.balanceOf(trader);
+        allowance = token.allowance(trader, this.vaultRelayer.address);
+    }
 
-        // Read the traders token balance and allowance.
-        if (source == keccak256("erc20")) {
-            tokenBalance = token.balanceOf(trader);
-            allowance = token.allowance(trader, address(contracts.vaultRelayer));
-        } else if (source == keccak256("external")) {
-            tokenBalance = token.balanceOf(trader);
-            allowance = contracts.vault
-                .hasApprovedRelayer(trader, address(contracts.vaultRelayer))
-                    ? token.allowance(trader, address(contracts.vault))
-                    : 0;
-        } else if (source == keccak256("internal")) {
-            address[] memory tokens = new address[](1);
-            tokens[0] = address(token);
-            tokenBalance = contracts.vault.getInternalBalance(trader, tokens)[0];
-            allowance = contracts.vault
-                .hasApprovedRelayer(trader, address(contracts.vaultRelayer))
-                    ? type(uint256).max
-                    : 0;
-        } else {
-            revert("invalid token source");
-        }
+    function balanceExternal(
+        address trader,
+        IERC20 token,
+        Interaction[] calldata interactions
+    ) external returns (
+        uint256 tokenBalance,
+        uint256 allowance
+    ) {
+        executeInteractions(interactions);
+        tokenBalance = token.balanceOf(trader);
+        allowance = vault
+            .hasApprovedRelayer(trader, address(vaultRelayer))
+                ? token.allowance(trader, address(vault))
+                : 0;
+    }
 
-        effectiveBalance = tokenBalance <= allowance
-            ? tokenBalance
-            : allowance;
-
-        // Verify that the transfer for the complete effective balance actually
-        // works.
-        Transfer[] memory transfers = new Transfer[](1);
-        transfers[0] = Transfer({
-            account: trader,
-            token: address(token),
-            amount: amount != 0
-                ? amount
-                : effectiveBalance,
-            balance: source
-        });
-        try contracts.vaultRelayer.transferFromAccounts(transfers) {
-            canTransfer = true;
-        }
-        catch {
-            canTransfer = false;
-        }
+    function balanceInternal(
+        address trader,
+        IERC20 token,
+        Interaction[] calldata interactions
+    ) external returns (
+        uint256 tokenBalance,
+        uint256 allowance
+    ) {
+        executeInteractions(interactions);
+        address[] memory tokens = new address[](1);
+        tokens[0] = address(token);
+        tokenBalance = vault.getInternalBalance(trader, tokens)[0];
+        allowance = vault
+            .hasApprovedRelayer(trader, address(vaultRelayer))
+                ? type(uint256).max
+                : 0;
     }
 
     /// @dev Execute a set of interactions. This code is ported from the CoW
@@ -106,21 +79,20 @@ contract Balances {
     /// <https://github.com/cowprotocol/contracts/blob/v1.0.0/src/contracts/GPv2Settlement.sol#L448-L470>
     /// <https://github.com/cowprotocol/contracts/blob/v1.0.0/src/contracts/libraries/GPv2Interaction.sol#L15-L49>
     function executeInteractions(
-        Contracts memory contracts,
         Interaction[] calldata interactions
     ) private {
         require(
-            address(this) == address(contracts.settlement),
+            address(this) == address(settlement),
             "incorrect calling context"
         );
 
         for (uint256 i; i < interactions.length; i++) {
             address target = interactions[i].target;
-            uint256 value = interactions[i].value;
+            uint256 value = 0;
             bytes calldata callData = interactions[i].callData;
 
             require(
-                target != address(contracts.vaultRelayer),
+                target != address(vaultRelayer),
                 "GPv2: forbidden interaction"
             );
 
