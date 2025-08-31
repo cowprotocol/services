36ae090e46ced0b5b5ab004de0f9f8e69c45a9b4
MartinquaXD <martin.beckmann@protonmail.com>
2025-08-31 20:06:41 +0000

    faster mul_ration_ceil

--- crates/driver/src/util/math.rs
+++ crates/driver/src/util/math.rs
@@ -11,8 +11,15 @@ pub fn mul_ratio(x: U256, q: U256, d: U256) -> Option<U256> {
 ///
 /// Returns `None` if `d` is `0` or if the result overflows a 256-bit integer.
 pub fn mul_ratio_ceil(x: U256, q: U256, d: U256) -> Option<U256> {
+    if d.is_zero() {
+        return None;
+    }
+
     let p = x.full_mul(q);
-    let result = U256::try_from(p.checked_div(d.into())?).ok()?;
-    let round_up = !p.checked_rem(d.into())?.is_zero();
-    result.checked_add(u8::from(round_up).into())
+    let (div, rem) = p.div_mod(d.into());
+    let mut result = U256::try_from(div).ok()?;
+    if !rem.is_zero() {
+        result = result.checked_add(U256::one())?;
+    }
+    Some(result)
 }
