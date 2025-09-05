e8a114213bd46222205348c7065351b432a8afbc
Jan P <patomil@gmail.com>
2025-09-05 09:58:29 +0200

    Use correct uniswapv3 router address
    The previous one was for pancake.

--- crates/contracts/build.rs
+++ crates/contracts/build.rs
@@ -1397,7 +1397,7 @@ fn main() {
             .add_network_str(OPTIMISM, "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45")
             .add_network_str(BASE, "0x2626664c2603336E57B271c5C0b26F421741e481")
             .add_network_str(AVALANCHE, "0xbb00FF08d01D300023C629E8fFfFcb65A5a578cE")
-            .add_network_str(BNB, "0xB971eF87ede563556b2ED4b1C0b0019111Dd85d2")
+            .add_network_str(BNB, "0xb971ef87eDe563556b2Ed4b1c0B0019111dd85d2")
             .add_network_str(LENS, "0x6ddD32cd941041D8b61df213B9f515A7D288Dc13")
         // Not available on Gnosis Chain
     });
