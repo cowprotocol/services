03bb8743d4433cded94352a13b60ddb502da556f
MartinquaXD <martin.beckmann@protonmail.com>
2025-09-17 08:53:50 +0000

    Implement helper to execute call with state overrides

--- crates/ethrpc/src/extensions.rs
+++ crates/ethrpc/src/extensions.rs
@@ -1,13 +1,20 @@
 //! Module containing Ethereum RPC extension methods.
 
 use {
+    ethcontract::{
+        BlockNumber,
+        contract::{MethodBuilder, ViewMethodBuilder},
+        errors::MethodError,
+        tokens::Tokenize,
+        transaction::TransactionBuilder,
+    },
     serde::{Deserialize, Serialize},
     std::collections::HashMap,
-    tracing::{Instrument, instrument::Instrumented},
     web3::{
         self,
         Transport,
         api::Namespace,
+        ethabi::{Function, Token},
         helpers::{self, CallFuture},
         types::{BlockId, Bytes, CallRequest, H160, H256, U64, U256},
     },
@@ -23,7 +30,7 @@ where
         call: CallRequest,
         block: BlockId,
         overrides: HashMap<H160, StateOverride>,
-    ) -> Instrumented<CallFuture<Bytes, T::Out>>;
+    ) -> CallFuture<Bytes, T::Out>;
 }
 
 impl<T> EthExt<T> for web3::api::Eth<T>
@@ -35,7 +42,7 @@ where
         call: CallRequest,
         block: BlockId,
         overrides: StateOverrides,
-    ) -> Instrumented<CallFuture<Bytes, T::Out>> {
+    ) -> CallFuture<Bytes, T::Out> {
         let call = helpers::serialize(&call);
         let block = helpers::serialize(&block);
         let overrides = helpers::serialize(&overrides);
@@ -44,7 +51,99 @@ where
             self.transport()
                 .execute("eth_call", vec![call, block, overrides]),
         )
-        .instrument(tracing::info_span!("eth_call"))
+    }
+}
+
+pub trait CallBuilderExt<T, R>
+where
+    T: Transport,
+    R: Tokenize,
+{
+    fn call_with_state_overrides(
+        self,
+        web3: &T,
+        overrides: HashMap<H160, StateOverride>,
+    ) -> impl Future<Output = Result<R, MethodError>>;
+}
+
+impl<T, R> CallBuilderExt<T, R> for MethodBuilder<T, R>
+where
+    T: Transport,
+    R: Tokenize,
+{
+    async fn call_with_state_overrides(
+        self,
+        web3: &T,
+        overrides: HashMap<H160, StateOverride>,
+    ) -> Result<R, MethodError> {
+        let function = self.function();
+        let call = tx_builder_into_call_request(&self.tx);
+        let block = BlockId::Number(BlockNumber::Latest);
+        let future = web3::api::Eth::new(web3).call_with_state_overrides(call, block, overrides);
+        convert_response::<_, R>(future, function).await
+    }
+}
+
+impl<T, R> CallBuilderExt<T, R> for ViewMethodBuilder<T, R>
+where
+    T: Transport,
+    R: Tokenize,
+{
+    async fn call_with_state_overrides(
+        self,
+        web3: &T,
+        overrides: HashMap<H160, StateOverride>,
+    ) -> Result<R, MethodError> {
+        let function = self.function();
+        let call = tx_builder_into_call_request(&self.m.tx);
+        let block = self.block.unwrap_or(BlockId::Number(BlockNumber::Latest));
+        let future = web3::api::Eth::new(web3).call_with_state_overrides(call, block, overrides);
+        convert_response::<_, R>(future, function).await
+    }
+}
+
+async fn convert_response<
+    F: std::future::Future<Output = Result<Bytes, web3::Error>>,
+    R: Tokenize,
+>(
+    future: F,
+    function: &Function,
+) -> Result<R, MethodError> {
+    let bytes = future
+        .await
+        .map_err(|err| MethodError::new(function, err))?;
+    let tokens = function
+        .decode_output(&bytes.0)
+        .map_err(|err| MethodError::new(function, err))?;
+    let token = match tokens.len() {
+        0 => Token::Tuple(Vec::new()),
+        1 => tokens.into_iter().next().unwrap(),
+        // Older versions of solc emit a list of tokens as the return type of functions returning
+        // tuples instead of a single type that is a tuple. In order to be backwards compatible we
+        // accept this too.
+        _ => Token::Tuple(tokens),
+    };
+    let result = R::from_token(token).map_err(|err| MethodError::new(function, err))?;
+    Ok(result)
+}
+
+pub fn tx_builder_into_call_request(tx: &TransactionBuilder<impl Transport>) -> CallRequest {
+    let resolved_gas_price = tx
+        .gas_price
+        .map(|gas_price| gas_price.resolve_for_transaction())
+        .unwrap_or_default();
+
+    CallRequest {
+        from: tx.from.as_ref().map(|account| account.address()),
+        to: tx.to,
+        gas: tx.gas,
+        gas_price: resolved_gas_price.gas_price,
+        value: tx.value,
+        data: tx.data.clone(),
+        transaction_type: resolved_gas_price.transaction_type,
+        access_list: tx.access_list.clone(),
+        max_fee_per_gas: resolved_gas_price.max_fee_per_gas,
+        max_priority_fee_per_gas: resolved_gas_price.max_priority_fee_per_gas,
     }
 }
 
@@ -176,5 +275,43 @@ mod tests {
             .unwrap();
 
         assert_eq!(output.0, (0..32).collect::<Vec<_>>());
+    }
+
+    #[ignore]
+    #[tokio::test]
+    async fn method_builder_with_state_overrides() {
+        let web3 = Web3::new_from_env();
+
+        let weth = H160(hex!("C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"));
+        let weth = contracts::WETH9::at(&web3, weth);
+
+        let trader = H160(hex!("1111111111111111111111111111111111111111"));
+        let balance_storage_slot = H256(hex!(
+            "fc40ea33816453f766ebc0872d4b5152b468882abe7b6b35528069db4d6e41c4"
+        ));
+        let faked_balance = U256::exp10(18);
+        let faked_balance_as_fixed_bytes = {
+            let mut buf = [0u8; 32];
+            faked_balance.to_big_endian(&mut buf);
+            H256(buf)
+        };
+
+        let balance = weth
+            .balance_of(trader)
+            .call_with_state_overrides(
+                web3.transport(),
+                hashmap! {
+                    weth.address() => StateOverride {
+                        state: Some(hashmap! {
+                            balance_storage_slot => faked_balance_as_fixed_bytes,
+                        }),
+                        ..Default::default()
+                    },
+                },
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(balance, faked_balance);
     }
 }
