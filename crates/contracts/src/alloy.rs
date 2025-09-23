d05911cfd377148bec9f21155ba2311722ac2ccf
José Duarte <duarte.gmj@gmail.com>
2025-09-23 14:43:44 +0100

    Revert "Revert #3679 and #3685 (#3689)"
    This reverts commit c911a51e258ba9ab1a622f8b33614c42dbab233a.

--- crates/contracts/src/alloy.rs
+++ crates/contracts/src/alloy.rs
@@ -438,6 +438,8 @@ crate::bindings!(
         // Not available on Lens
     }
 );
+crate::bindings!(IUniswapLikeRouter);
+crate::bindings!(IUniswapLikePair);
 
 pub use alloy::providers::DynProvider as Provider;
 
