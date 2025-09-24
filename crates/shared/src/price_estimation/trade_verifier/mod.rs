3a9f7496241879b50d7eb558baa661ce259edfd7
José Duarte <duarte.gmj@gmail.com>
2025-09-24 18:19:12 +0100

    Simplify trade verification code simulation (#3704)

--- crates/shared/src/price_estimation/trade_verifier/mod.rs
+++ crates/shared/src/price_estimation/trade_verifier/mod.rs
@@ -5,9 +5,9 @@ use {
     super::{Estimate, Verification},
     crate::{
         code_fetching::CodeFetching,
-        code_simulation::CodeSimulating,
         encoded_settlement::{EncodedSettlement, EncodedTrade, encode_trade},
         interaction::EncodedInteraction,
+        tenderly_api::TenderlyCodeSimulator,
         trade_finding::{
             Interaction,
             QuoteExecution,
@@ -18,7 +18,7 @@ use {
     },
     ::alloy::sol_types::SolCall,
     alloy::primitives::Address,
-    anyhow::{Context, Result},
+    anyhow::{Context, Result, anyhow},
     bigdecimal::BigDecimal,
     contracts::{
         GPv2Settlement,
@@ -49,7 +49,6 @@ use {
         sync::Arc,
     },
     tracing::instrument,
-    web3::types::CallRequest,
 };
 
 #[async_trait::async_trait]
@@ -69,7 +68,7 @@ pub trait TradeVerifying: Send + Sync + 'static {
 #[derive(Clone)]
 pub struct TradeVerifier {
     web3: Web3,
-    simulator: Arc<dyn CodeSimulating>,
+    simulator: Option<Arc<TenderlyCodeSimulator>>,
     code_fetcher: Arc<dyn CodeFetching>,
     balance_overrides: Arc<dyn BalanceOverriding>,
     block_stream: CurrentBlockWatcher,
@@ -88,7 +87,7 @@ impl TradeVerifier {
     #[allow(clippy::too_many_arguments)]
     pub async fn new(
         web3: Web3,
-        simulator: Arc<dyn CodeSimulating>,
+        simulator: Option<Arc<TenderlyCodeSimulator>>,
         code_fetcher: Arc<dyn CodeFetching>,
         balance_overrides: Arc<dyn BalanceOverriding>,
         block_stream: CurrentBlockWatcher,
@@ -177,28 +176,34 @@ impl TradeVerifier {
             )
             .tx;
 
-        let swap_simulation = Solver::Solver::swapCall {
-            settlementContract: self.settlement.address().into_alloy(),
-            tokens: tokens.iter().cloned().map(IntoAlloy::into_alloy).collect(),
-            receiver: verification.receiver.into_alloy(),
-            settlementCall: alloy::primitives::Bytes::from(settlement.data.unwrap().0),
-        };
-
         let block = *self.block_stream.borrow();
 
-        let call = CallRequest {
+        let solver = Solver::Instance::new(solver_address.into_alloy(), self.web3.alloy.clone());
+        let swap_simulation = solver.swap(
+            self.settlement.address().into_alloy(),
+            tokens.iter().cloned().map(IntoAlloy::into_alloy).collect(),
+            verification.receiver.into_alloy(),
+            alloy::primitives::Bytes::from(settlement.data.unwrap().0),
+        )
             // Initiate tx as solver so gas doesn't get deducted from user's ETH.
-            from: Some(solver_address),
-            to: Some(solver_address),
-            data: Some(swap_simulation.abi_encode().into()),
-            gas: Some(Self::DEFAULT_GAS.into()),
-            gas_price: Some(block.gas_price),
-            ..Default::default()
-        };
+        .from(solver_address.into_alloy())
+        .to(solver_address.into_alloy())
+        .gas(Self::DEFAULT_GAS)
+        .gas_price(u128::try_from(block.gas_price).map_err(|err| anyhow!(err)).context("converting gas price to u128")?);
 
-        let output = self
-            .simulator
-            .simulate(call, overrides, Some(block.number))
+        if let Some(tenderly) = &self.simulator
+            && let Err(err) = tenderly.log_simulation_command(
+                swap_simulation.clone().into_transaction_request(),
+                overrides.clone(),
+                Some(block.number),
+            )
+        {
+            tracing::debug!(?err, "could not log tenderly simulation command");
+        }
+
+        let output = swap_simulation
+            .call()
+            .overrides(overrides)
             .await
             .context("failed to simulate quote")
             .map_err(Error::SimulationFailed);
@@ -234,9 +239,7 @@ impl TradeVerifier {
             }
         };
 
-        let mut summary = SettleOutput::decode(&output?, query.kind, &tokens)
-            .context("could not decode simulation output")
-            .map_err(Error::SimulationFailed)?;
+        let mut summary = SettleOutput::from_swap(output?, query.kind, &tokens)?;
 
         {
             // Quote accuracy gets determined by how many tokens had to be paid out of the
@@ -290,7 +293,7 @@ impl TradeVerifier {
         verification: &mut Verification,
         query: &PriceQuery,
         trade: &TradeKind,
-    ) -> Result<HashMap<H160, StateOverride>> {
+    ) -> Result<alloy::rpc::types::eth::state::StateOverride> {
         let mut overrides = HashMap::with_capacity(6);
 
         // Provide mocked balances if possible to the spardose to allow it to
@@ -407,7 +410,7 @@ impl TradeVerifier {
         }
         overrides.insert(trade.tx_origin().unwrap_or(trade.solver()), solver_override);
 
-        Ok(overrides)
+        Ok(overrides.into_alloy())
     }
 }
 
@@ -746,16 +749,14 @@ struct SettleOutput {
 }
 
 impl SettleOutput {
-    fn decode(output: &[u8], kind: OrderKind, tokens_vec: &[H160]) -> Result<Self> {
-        use Solver::Solver::{swapCall, swapReturn};
-        // NOTE(jmg-duarte): The validation is (afaik) a typecheck over the result, I've
-        // defaulted to validate the data, but we may be able to skip it given we have
-        // some control over the nodes we're using
-        let swapReturn {
+    fn from_swap(
+        Solver::Solver::swapReturn {
             gasUsed,
             queriedBalances,
-        } = swapCall::abi_decode_returns_validate(output).context("decoding swap return")?;
-
+        }: Solver::Solver::swapReturn,
+        kind: OrderKind,
+        tokens_vec: &[H160],
+    ) -> Result<Self> {
         // The balances are stored in the following order:
         // [...tokens_before, user_balance_before, user_balance_after, ...tokens_after]
         let mut i = 0;
