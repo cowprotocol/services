f9c0fe895d70c6718a6d04e779590b6adf3e3532
José Duarte <duarte.gmj@gmail.com>
2025-09-24 15:42:17 +0100

    Migrate Spardose to alloy

--- crates/contracts/src/alloy.rs
+++ crates/contracts/src/alloy.rs
@@ -444,6 +444,7 @@ crate::bindings!(IUniswapLikePair);
 pub mod support {
     crate::bindings!(AnyoneAuthenticator);
     crate::bindings!(Solver);
+    crate::bindings!(Spardose);
 }
 
 pub use alloy::providers::DynProvider as Provider;
