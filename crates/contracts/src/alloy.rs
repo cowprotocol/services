1451b3d36f1918640c549b095d635670b5f26bd6
José Duarte <duarte.gmj@gmail.com>
2025-09-24 17:06:49 +0100

    Actually unused

--- crates/contracts/src/alloy.rs
+++ crates/contracts/src/alloy.rs
@@ -445,8 +445,6 @@ pub mod support {
     crate::bindings!(AnyoneAuthenticator);
     crate::bindings!(Solver);
     crate::bindings!(Spardose);
-    // Support contract used for solver fee simulations.
-    crate::bindings!(Swapper);
 }
 
 pub use alloy::providers::DynProvider as Provider;
