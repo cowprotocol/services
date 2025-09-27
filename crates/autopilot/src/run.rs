0fe8ea3004a27a29a9bd8bd876d698d979b9976e
MartinquaXD <martin.beckmann@protonmail.com>
2025-09-27 07:01:50 +0300

    Disallow span guard across .await points

--- crates/autopilot/src/run.rs
+++ crates/autopilot/src/run.rs
@@ -152,10 +152,6 @@ pub async fn start(args: impl Iterator<Item = String>) {
 /// Assumes tracing and metrics registry have already been set up.
 pub async fn run(args: Arguments) {
     assert!(args.shadow.is_none(), "cannot run in shadow mode");
-    // Start a new span that measures the initialization phase of the autopilot
-    let startup_span = info_span!("autopilot_startup");
-    let startup_span_guard = startup_span.enter();
-
     let db = Postgres::new(args.db_url.as_str(), args.insert_batch_size)
         .await
         .unwrap();
@@ -665,7 +661,6 @@ pub async fn run(args: Arguments) {
         Arc::new(maintenance),
         competition_updates_sender,
     );
-    drop(startup_span_guard);
     run.run_forever().await;
 }
 
