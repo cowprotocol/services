858da7531f2e0a1cdb902d031e537a9ab77c618d
ilya <ilya@cow.fi>
2025-10-22 19:15:01 +0300

    Fix CoW AMM factory address string representation (#3806)
    # Description
    By default, `H160::to_string()` returns something like `0x334ab..dfr`,
    we have to use a custom formatter.

--- crates/cow-amm/src/cache.rs
+++ crates/cow-amm/src/cache.rs
@@ -224,7 +224,7 @@ impl EventStoring<ethcontract::Event<CowAmmEvent>> for Storage {
 
     async fn last_event_block(&self) -> anyhow::Result<u64> {
         let mut ex = self.0.db.acquire().await?;
-        database::last_indexed_blocks::fetch(&mut ex, &self.0.factory_address.to_string())
+        database::last_indexed_blocks::fetch(&mut ex, &format!("{:#x}", self.0.factory_address))
             .await?
             .map(|block| block.try_into().context("last block is not u64"))
             .unwrap_or(Ok(self.0.start_of_index))
@@ -234,7 +234,7 @@ impl EventStoring<ethcontract::Event<CowAmmEvent>> for Storage {
         let mut ex = self.0.db.acquire().await?;
         database::last_indexed_blocks::update(
             &mut ex,
-            &self.0.factory_address.to_string(),
+            &format!("{:#x}", self.0.factory_address),
             i64::try_from(latest_block).context("latest block is not u64")?,
         )
         .await?;
