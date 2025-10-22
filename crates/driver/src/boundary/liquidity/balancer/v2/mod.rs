472261a1b4739ea71e1a486a223a391d1c78f131
Martin Magnus <martin.beckmann@protonmail.com>
2025-10-22 09:55:48 +0200

    Remove contracts dependency from ethrpc (#3796)
    # Description
    `ethrpc` only depends on 1 function from `contracts` which arguably
    shouldn't have been re-exported to begin with since it's only needed for
    `ethcontract` bindings.
    Removing that function from `ethrpc` means it no longer depends on
    `contracts` which allows `cargo` to compile it in parallel with
    `contracts`.
    
    Also where this `dummy` functionality lives become irrelevant when we
    finally switch to `alloy` completely since `alloy` does not need these
    dummy instances just to encode calldata. 🥳
    
    # Changes
    - remove `contracts` dependency from `ethrpc`
    
    ## How to test
    compiler

--- crates/driver/src/boundary/liquidity/balancer/v2/mod.rs
+++ crates/driver/src/boundary/liquidity/balancer/v2/mod.rs
@@ -53,7 +53,7 @@ fn to_interaction(
     output: &liquidity::ExactOutput,
     receiver: &eth::Address,
 ) -> eth::Interaction {
-    let web3 = ethrpc::dummy::web3();
+    let web3 = contracts::web3::dummy();
     let handler = balancer_v2::SettlementHandler::new(
         pool.id.into(),
         // Note that this code assumes `receiver == sender`. This assumption is
