98cfa66ce924d14922ecc15907c07f342380d53a
José Duarte <duarte.gmj@gmail.com>
2025-11-04 15:43:14 +0000

    Use correct method to get amount_in from uniswapv3 pools (#3865)
    # Description
    Use correct method to get amount_in from uniswapv3 pools
    
    # Changes
    <!-- List of detailed changes (how the change is accomplished) -->
    
    - [ ] Fixes a typo 🤠 `in` -> `out` meaning that the quotes were wrong
    (traces back to #3801)
    
    ## How to test
    <!--- Include details of how to test your changes, including any
    pre-requisites. If no unit tests are included, please explain why and
    how to test manually
    1.
    2.
    3.
    -->
    
    <!--
    ## Related Issues
    
    Fixes #
    -->

--- crates/solvers/src/boundary/liquidity/concentrated.rs
+++ crates/solvers/src/boundary/liquidity/concentrated.rs
@@ -1,7 +1,10 @@
 use {
     alloy::primitives::aliases::U24,
     contracts::{
-        alloy::UniswapV3QuoterV2::IQuoterV2::QuoteExactInputSingleParams,
+        alloy::UniswapV3QuoterV2::IQuoterV2::{
+            QuoteExactInputSingleParams,
+            QuoteExactOutputSingleParams,
+        },
         ethcontract::{H160, U256},
     },
     ethrpc::alloy::conversions::{IntoAlloy, IntoLegacy},
@@ -61,16 +64,16 @@ impl BaselineSolvable for Pool {
         }
 
         self.uni_v3_quoter_contract
-            .quoteExactInputSingle(QuoteExactInputSingleParams {
+            .quoteExactOutputSingle(QuoteExactOutputSingleParams {
                 tokenIn: in_token.into_alloy(),
                 tokenOut: out_token.into_alloy(),
-                amountIn: out_amount.into_alloy(),
+                amount: out_amount.into_alloy(),
                 fee: self.fee,
                 sqrtPriceLimitX96: alloy::primitives::U160::ZERO,
             })
             .call()
             .await
-            .map(|result| result.amountOut.into_legacy())
+            .map(|result| result.amountIn.into_legacy())
             .ok()
     }
 
