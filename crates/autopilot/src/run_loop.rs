b5d1ea4dc99cd3d1397fb102e746e2ebb17e7bda
Marcin Szymczak <mail@marszy.com>
2025-11-05 11:19:51 +0100

    Move Leader lock to separate file

--- crates/autopilot/src/run_loop.rs
+++ crates/autopilot/src/run_loop.rs
@@ -20,6 +20,7 @@ use {
             self,
             solvers::dto::{settle, solve},
         },
+        leader_lock_tracker::LeaderLockTracker,
         maintenance::Maintenance,
         run::Liveness,
         shutdown_controller::ShutdownController,
@@ -27,7 +28,7 @@ use {
     },
     ::observe::metrics,
     anyhow::{Context, Result},
-    database::{leader_pg_lock::LeaderLock, order_events::OrderEventLabel},
+    database::order_events::OrderEventLabel,
     ethrpc::block_stream::BlockInfo,
     futures::{FutureExt, TryFutureExt},
     itertools::Itertools,
@@ -88,95 +89,6 @@ pub struct RunLoop {
     maintenance: Arc<Maintenance>,
     competition_updates_sender: tokio::sync::mpsc::UnboundedSender<()>,
     winner_selection: winner_selection::Arbitrator,
-}
-
-/// Tracks the autopilot leader lock status.
-#[expect(clippy::large_enum_variant)]
-enum LeaderLockTracker {
-    Disabled,
-    Enabled {
-        is_leader: bool,
-        was_leader: bool,
-        leader_lock: LeaderLock,
-    },
-}
-
-impl LeaderLockTracker {
-    pub fn new(leader_lock: Option<LeaderLock>) -> Self {
-        match leader_lock {
-            Some(leader_lock) => Self::Enabled {
-                is_leader: false,
-                was_leader: false,
-                leader_lock,
-            },
-            None => Self::Disabled,
-        }
-    }
-
-    /// Tries to acquire the leader lock if it's enabled
-    /// If not, does nothing
-    /// Should be called at the beginning of every run loop iteration
-    pub async fn try_acquire(&mut self) {
-        let Self::Enabled {
-            is_leader,
-            was_leader,
-            leader_lock,
-        } = self
-        else {
-            return;
-        };
-
-        *was_leader = *is_leader;
-
-        *is_leader = leader_lock.try_acquire().await.unwrap_or_else(|err| {
-            tracing::error!(?err, "failed to become leader");
-            Metrics::leader_lock_error();
-            false
-        });
-
-        if self.just_stepped_up() {
-            tracing::info!("Stepped up as a leader");
-            Metrics::leader_step_up();
-        }
-    }
-
-    /// Releases the leader lock if it was held
-    /// Should be called after breaking out of run loop (for example: due to
-    /// shutdown)
-    pub async fn release(self) {
-        if let Self::Enabled {
-            mut leader_lock,
-            is_leader: true,
-            ..
-        } = self
-        {
-            tracing::info!("Shutdown received, stepping down as the leader");
-            leader_lock.release().await;
-            Metrics::leader_step_down();
-        }
-    }
-
-    /// Returns true if the last try_acquire call resulted in acquiring the
-    /// leader lock If the feature is disabled, always returns false
-    pub fn just_stepped_up(&self) -> bool {
-        match self {
-            Self::Enabled {
-                is_leader: true,
-                was_leader: false,
-                ..
-            } => true,
-            _ => false,
-        }
-    }
-
-    /// Returns true if the leader lock is being held
-    /// If the feature is disabled, always returns true
-    pub fn is_leader(&self) -> bool {
-        match self {
-            Self::Enabled { is_leader, .. } => *is_leader,
-            _ => true,
-        }
-    }
 }
 
 impl RunLoop {
@@ -1078,14 +990,6 @@ struct Metrics {
     /// function is started.
     #[metric(buckets(0, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 4, 5, 6))]
     current_block_delay: prometheus::Histogram,
-
-    /// Tracks the current leader status
-    /// 1 - is currently autopilot leader
-    /// 0 - is currently autopilot follower
-    is_leader: prometheus::IntGauge,
-
-    /// Trackes the count of errors acquiring leader lock (should never happen)
-    leader_lock_error: prometheus::IntCounter,
 }
 
 impl Metrics {
@@ -1185,18 +1089,6 @@ impl Metrics {
         Self::get()
             .current_block_delay
             .observe(init_block_timestamp.elapsed().as_secs_f64())
-    }
-
-    fn leader_step_up() {
-        Self::get().is_leader.set(1)
-    }
-
-    fn leader_step_down() {
-        Self::get().is_leader.set(0)
-    }
-
-    fn leader_lock_error() {
-        Self::get().leader_lock_error.inc()
     }
 }
 
