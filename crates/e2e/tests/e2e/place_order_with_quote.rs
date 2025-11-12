685ea1892b501622d0e0f19e6b2c3c4c9d3be304
Marcin Szymczak <mail@marszy.com>
2025-11-12 16:52:07 +0100

    cleanup comments

--- crates/e2e/tests/e2e/place_order_with_quote.rs
+++ crates/e2e/tests/e2e/place_order_with_quote.rs
@@ -136,23 +136,6 @@ async fn place_order_with_quote_same_token_pair(web3: Web3) {
 
     token.mint(trader.address(), to_wei(10)).await;
 
-    /*onchain
-        .contracts()
-        .weth
-        .approve(onchain.contracts().allowance.into_alloy(), eth(3))
-        .from(trader.address().into_alloy())
-        .send_and_watch()
-        .await
-        .unwrap();
-    onchain
-        .contracts()
-        .weth
-        .deposit()
-        .from(trader.address().into_alloy())
-        .value(eth(3))
-        .send_and_watch()
-        .await
-        .unwrap();*/
     token
         .approve(onchain.contracts().allowance.into_alloy(), eth(10))
         .from(trader.address().into_alloy())
