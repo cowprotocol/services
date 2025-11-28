b05b0f50b0051953d33f58bf1daa58ba86366a73
Martin Magnus <martin.beckmann@protonmail.com>
2025-11-28 13:26:22 +0100

    [TRIVIAL] Migrate zeroex to use alloy bytes types (#3931)
    # Description
    Migrates zereox related logic to use alloy's bytes types (Address,
    B256). I didn't touch the `U256` types yet since they tend to be more
    complicate.
    
    ## How to test
    existing e2e tests

--- crates/solver/src/interactions/zeroex.rs
+++ crates/solver/src/interactions/zeroex.rs
@@ -20,24 +20,24 @@ impl Interaction for ZeroExInteraction {
     fn encode(&self) -> EncodedInteraction {
         let method = self.zeroex.fillOrKillLimitOrder(
             IZeroex::LibNativeOrder::LimitOrder {
-                makerToken: self.order.maker_token.into_alloy(),
-                takerToken: self.order.taker_token.into_alloy(),
+                makerToken: self.order.maker_token,
+                takerToken: self.order.taker_token,
                 makerAmount: self.order.maker_amount,
                 takerAmount: self.order.taker_amount,
                 takerTokenFeeAmount: self.order.taker_token_fee_amount,
-                maker: self.order.maker.into_alloy(),
-                taker: self.order.taker.into_alloy(),
-                sender: self.order.sender.into_alloy(),
-                feeRecipient: self.order.fee_recipient.into_alloy(),
-                pool: self.order.pool.into_alloy(),
+                maker: self.order.maker,
+                taker: self.order.taker,
+                sender: self.order.sender,
+                feeRecipient: self.order.fee_recipient,
+                pool: self.order.pool,
                 expiry: self.order.expiry,
                 salt: self.order.salt.into_alloy(),
             },
             IZeroex::LibSignature::Signature {
                 signatureType: self.order.signature.signature_type,
                 v: self.order.signature.v,
-                r: self.order.signature.r.into_alloy(),
-                s: self.order.signature.s.into_alloy(),
+                r: self.order.signature.r,
+                s: self.order.signature.s,
             },
             self.taker_token_fill_amount,
         );
