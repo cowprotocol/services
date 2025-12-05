47ffd26324dd7a4be07f994bcceafbf73bdd9841
Marcin Szymczak <mail@marszy.com>
2025-12-05 16:30:21 +0100

    Remove sell=buy test case already covered by onchain verification

--- crates/e2e/tests/e2e/place_order_with_quote.rs
+++ crates/e2e/tests/e2e/place_order_with_quote.rs
@@ -25,12 +25,6 @@ async fn local_node_place_order_with_quote_basic() {
 
 #[tokio::test]
 #[ignore]
-async fn local_node_place_order_with_quote_same_token_pair() {
-    run_test(place_order_with_quote_same_token_pair).await;
-}
-
-#[tokio::test]
-#[ignore]
 async fn local_node_place_order_with_quote_same_token_pair_error() {
     run_test(place_order_with_quote_same_token_pair_error).await;
 }
@@ -171,93 +165,4 @@ async fn place_order_with_quote_same_token_pair_error(web3: Web3) {
     assert!(
         matches!(services.submit_quote(&quote_request).await, Err((reqwest::StatusCode::BAD_REQUEST, response)) if response.contains("SameBuyAndSellToken"))
     );
-}
-
-async fn place_order_with_quote_same_token_pair(web3: Web3) {
-    let mut onchain = OnchainComponents::deploy(web3.clone()).await;
-
-    let [solver] = onchain.make_solvers(eth(10)).await;
-    let [trader] = onchain.make_accounts(eth(10)).await;
-    let [token] = onchain
-        .deploy_tokens_with_weth_uni_v2_pools(to_wei(1_000), to_wei(1_000))
-        .await;
-
-    token.mint(trader.address(), eth(10)).await;
-
-    token
-        .approve(onchain.contracts().allowance.into_alloy(), eth(10))
-        .from(trader.address())
-        .send_and_watch()
-        .await
-        .unwrap();
-
-    tracing::info!("Starting services.");
-    let services = Services::new(&onchain).await;
-    services
-        .start_protocol_with_args(
-            ExtraServiceArgs {
-                api: vec!["--allow-same-sell-and-buy-token=true".to_string()],
-                ..Default::default()
-            },
-            solver.clone(),
-        )
-        .await;
-
-    // Disable auto-mine so we don't accidentally mine a settlement
-    web3.api::<TestNodeApi<_>>()
-        .set_automine_enabled(false)
-        .await
-        .expect("Must be able to disable automine");
-
-    tracing::info!("Quoting");
-    let quote_sell_amount = to_wei(1);
-    let quote_request = OrderQuoteRequest {
-        from: trader.address(),
-        sell_token: *token.address(),
-        buy_token: *token.address(),
-        side: OrderQuoteSide::Sell {
-            sell_amount: SellAmount::BeforeFee {
-                value: NonZeroU256::try_from(quote_sell_amount).unwrap(),
-            },
-        },
-        ..Default::default()
-    };
-    let quote_response = services.submit_quote(&quote_request).await.unwrap();
-    tracing::debug!(?quote_response);
-    assert!(quote_response.id.is_some());
-    assert!(quote_response.verified);
-
-    let quote_metadata =
-        crate::database::quote_metadata(services.db(), quote_response.id.unwrap()).await;
-    assert!(quote_metadata.is_some());
-    tracing::debug!(?quote_metadata);
-
-    tracing::info!("Placing order");
-    let order = OrderCreation {
-        quote_id: quote_response.id,
-        sell_token: *token.address(),
-        sell_amount: quote_sell_amount.into_alloy(),
-        buy_token: *token.address(),
-        buy_amount: quote_response.quote.buy_amount,
-        valid_to: model::time::now_in_epoch_seconds() + 300,
-        kind: OrderKind::Sell,
-        ..Default::default()
-    }
-    .sign(
-        EcdsaSigningScheme::Eip712,
-        &onchain.contracts().domain_separator,
-        SecretKeyRef::from(&SecretKey::from_slice(trader.private_key()).unwrap()),
-    );
-    let order_uid = services.create_order(&order).await.unwrap();
-
-    tracing::info!("Order quote verification");
-    let order_quote = database::orders::read_quote(
-        services.db().acquire().await.unwrap().deref_mut(),
-        &database::byte_array::ByteArray(order_uid.0),
-    )
-    .await
-    .unwrap()
-    .unwrap();
-    assert_eq!(quote_response.verified, order_quote.verified);
-    assert_eq!(quote_metadata.unwrap().0, order_quote.metadata);
 }
