75273a245af08cfc628f403c1a919ae1fe83db35
Marcin Szymczak <mail@marszy.com>
2025-12-11 15:14:41 +0100

    Update crates/shared/src/order_validation.rs
    Co-authored-by: José Duarte <duarte.gmj@gmail.com>

--- crates/shared/src/order_validation.rs
+++ crates/shared/src/order_validation.rs
@@ -1303,7 +1303,7 @@ mod tests {
 
     #[tokio::test]
     async fn pre_validate_same_tokens_allow_sell() {
-        let native_token = WETH9::Instance::new([0xef; 20].into(), ethrpc::mock::web3().alloy);
+        let native_token = WETH9::Instance::new(Address::repeat_byte(0xef), ethrpc::mock::web3().alloy);
         let validity_configuration = OrderValidPeriodConfiguration {
             min: Duration::from_secs(1),
             max_market: Duration::from_secs(100),
