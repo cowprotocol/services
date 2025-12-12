a18d1d95211f50f240f49e1629b2c1fb32ff7203
ilya <ilya@cow.fi>
2025-12-12 10:13:22 +0300

    [TRIVIAL] Fix clippy warnings (#3974)
    This PR fixes clippy warnings due to the latest update.
    Failed CI:
    https://github.com/cowprotocol/services/actions/runs/20142706161/job/57814513611?pr=3973

--- crates/shared/src/order_quoting.rs
+++ crates/shared/src/order_quoting.rs
@@ -263,7 +263,7 @@ pub trait OrderQuoting: Send + Sync {
 
 #[derive(Error, Debug)]
 pub enum CalculateQuoteError {
-    #[error("sell amount does not cover fee")]
+    #[error("sell amount does not cover fee {fee_amount:?}")]
     SellAmountDoesNotCoverFee { fee_amount: U256 },
 
     #[error("{estimator_kind:?} estimator failed: {source}")]
