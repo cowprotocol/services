4e74ce3ba89d486ec54cee76c973a75b41a3e564
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2026-01-15 18:10:45 +0200

    adjust pod-sdk and protocol

--- crates/e2e/src/setup/config/pod.rs
+++ crates/e2e/src/setup/config/pod.rs
@@ -1,2 +1,2 @@
-pub(crate) static POD_ENDPOINT: &str = "ws://cow.pod.network:8545";
+pub(crate) static POD_ENDPOINT: &str = "http://cow.pod.network:8545";
 pub(crate) static POD_AUCTION_CONTRACT: &str = "0xeDD0670497E00ded712a398563Ea938A29dD28c7";
