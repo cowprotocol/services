676d843ab3ea6f02467d0f6060c13ecbabe90094
Marcin Szymczak <mail@marszy.com>
2026-01-16 13:44:49 +0100

    Adjust migration for 2 step deployment

--- database/sql/V095__add_confirmed_valid_to_to_orders.sql
+++ database/sql/V095__add_confirmed_valid_to_to_orders.sql
@@ -1,11 +1,21 @@
 ALTER TABLE orders ADD COLUMN confirmed_valid_to bigint;
-UPDATE orders SET confirmed_valid_to = COALESCE(
+
+/*
+The confirmed_valid_to will have to be backfilled manually to ensure manageable load on db
+using the following:
+UPDATE orders
+SET confirmed_valid_to = COALESCE(
     (SELECT ethflow_orders.valid_to FROM ethflow_orders WHERE ethflow_orders.uid = orders.uid),
     orders.valid_to
+)
+WHERE uid IN (
+    SELECT uid FROM orders
+    WHERE confirmed_valid_to IS NULL
+    LIMIT 10000
 );
-ALTER TABLE orders ALTER COLUMN confirmed_valid_to SET NOT NULL;
+*/
 
-CREATE INDEX orders_owner_live_limit 
-ON orders USING btree (owner, confirmed_valid_to)
-WHERE cancellation_timestamp IS NULL 
-  AND class = 'limit';
+--index on `confirmed_valid_to` for quickly discarding expired ethflow orders
+CREATE INDEX orders_owner_live_limit ON orders USING btree (confirmed_valid_to)
+-- further drops the query from 100ms to 80ms (warmed cache)
+CREATE INDEX okay_onchain_orders ON onchain_placed_orders USING btree (uid) WHERE placement_error IS NOT NULL
