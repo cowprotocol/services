f962e3ff2fd2a98bf17c88cea16d3668102c4522
Marcin Szymczak <mail@marszy.com>
2026-01-22 17:56:25 +0100

    Create indexes concurrently

--- database/sql/V096__set_confirmed_valid_to_not_null.sql
+++ database/sql/V096__set_confirmed_valid_to_not_null.sql
@@ -6,8 +6,3 @@ SET true_valid_to = COALESCE(
 );
 -- at this point every order has the true_valid_to filled in
 ALTER TABLE orders ALTER COLUMN true_valid_to SET NOT NULL;
-
---index on `true_valid_to` for quickly discarding expired orders
-CREATE INDEX orders_true_valid_to ON orders USING btree (true_valid_to);
--- further drops the query from 100ms to 80ms (warmed cache)
-CREATE INDEX okay_onchain_orders ON onchain_placed_orders USING btree (uid) WHERE placement_error IS NOT NULL;
