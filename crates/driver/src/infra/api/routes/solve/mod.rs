3ea30da6faf22fc423130c88aed38cc00be07bb1
MartinquaXD <martin.beckmann@protonmail.com>
2026-01-23 15:14:56 +0000

    Use metrics and trace logs

--- crates/driver/src/infra/api/routes/solve/mod.rs
+++ crates/driver/src/infra/api/routes/solve/mod.rs
@@ -11,6 +11,7 @@ use {
     },
     axum::{body::Body, http::Request},
     hyper::body::Bytes,
+    std::time::{Duration, Instant},
     tracing::Instrument,
 };
 
@@ -25,14 +26,16 @@ async fn route(
     // logging how long the raw data transfer takes.
     request: Request<Body>,
 ) -> Result<axum::Json<dto::SolveResponse>, (hyper::StatusCode, axum::Json<Error>)> {
+    let solver = state.solver().name().as_str();
+
     let handle_request = async {
-        let body_bytes = collect_request_body(request).await?;
+        let body_bytes = collect_request_body(request, solver).await?;
         let competition = state.competition();
         let result = competition.solve(body_bytes).await;
         // Solving takes some time, so there is a chance for the settlement queue to
         // have capacity again.
         competition.ensure_settle_queue_capacity()?;
-        observe::solved(state.solver().name(), &result);
+        observe::solved(solver, &result);
         Ok(axum::Json(dto::SolveResponse::new(
             result?,
             &competition.solver,
@@ -40,13 +43,20 @@ async fn route(
     };
 
     handle_request
-        .instrument(tracing::info_span!("/solve", solver = %state.solver().name(), auction_id = tracing::field::Empty))
+        .instrument(tracing::info_span!(
+            "/solve",
+            solver,
+            auction_id = tracing::field::Empty
+        ))
         .await
 }
 
-async fn collect_request_body(request: Request<Body>) -> Result<Bytes, competition::Error> {
-    tracing::trace!("received request");
-    let start = std::time::Instant::now();
+async fn collect_request_body(
+    request: Request<Body>,
+    solver: &str,
+) -> Result<Bytes, competition::Error> {
+    tracing::trace!("start streaming request body");
+    let start = Instant::now();
 
     // accepting the raw request bypasses axum's request body limiting layer
     // so we have to manually ensure the body has a reasonable size.
@@ -56,6 +66,30 @@ async fn collect_request_body(request: Request<Body>) -> Result<Bytes, competiti
         competition::Error::MalformedRequest
     })?;
 
-    tracing::debug!(time = ?start.elapsed(), "finished streaming body");
+    let duration = start.elapsed();
+    Metrics::measure_solve_transfer_time(solver, duration);
+    tracing::trace!(?duration, "finished streaming request body");
     Ok(body_bytes)
+}
+
+#[derive(prometheus_metric_storage::MetricStorage)]
+struct Metrics {
+    /// Tracks how long the network transfer of the solve request takes.
+    #[metric(labels("solver"))]
+    #[metric(buckets(0.0001, 0.0005, 0.002, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 1, 1.5))]
+    solve_network_transfer: prometheus::HistogramVec,
+}
+
+impl Metrics {
+    fn get() -> &'static Metrics {
+        Metrics::instance(::observe::metrics::get_storage_registry())
+            .expect("unexpected error getting metrics instance")
+    }
+
+    fn measure_solve_transfer_time(solver: &str, time: Duration) {
+        Self::get()
+            .solve_network_transfer
+            .with_label_values(&[solver])
+            .observe(time.as_secs_f64());
+    }
 }
