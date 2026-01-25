736523b383d9fe04e9a7b0c976a4de60a28b71aa
MartinquaXD <martin.beckmann@protonmail.com>
2026-01-25 00:50:12 +0000

    fix typo in migration

--- database/sql/V098__add_auction_deadline_index.sql
+++ database/sql/V098__add_auction_deadline_index.sql
@@ -1,2 +1,2 @@
 -- adds index on the deadline of an auction to quickly look up inflight orders from the db
-CREATE INDEX CONCURENTLY competition_auction_deadline ON auction_competitions USING BTREE(deadline);
+CREATE INDEX CONCURRENTLY competition_auction_deadline ON auction_competitions USING BTREE(deadline);
