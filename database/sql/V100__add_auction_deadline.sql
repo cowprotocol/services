d2e2e2d5b6eaab7c9bd869b1e64bab361e59eb82
Marcin Szymczak <mail@marszy.com>
2026-01-28 15:50:38 +0100

    Rewrite migration V100 to be optional (#4098)
    # Description
    The migration V100 creates index on competition_auction_deadline on
    competition_auctions. To make the prod deployment viable it needs to be
    optional (IF NOT EXISTS) which will enable to apply it manually
    beforehand.
    
    # Changes
    Update the V100 migration to specify IF NOT EXISTS.
    
    ## How to test
    Will apply the migration manually and deploy on staging to verify.

--- database/sql/V100__add_auction_deadline.sql
+++ database/sql/V100__add_auction_deadline.sql
@@ -1,2 +1,2 @@
 -- adds index on the deadline of an auction to quickly look up inflight orders from the db
-CREATE INDEX CONCURRENTLY competition_auction_deadline ON competition_auctions USING BTREE(deadline);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS competition_auction_deadline ON competition_auctions USING BTREE(deadline);
