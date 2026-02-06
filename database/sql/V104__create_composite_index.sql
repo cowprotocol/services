5a52f184d453ab37ea2a27e462d9874b8942dc4f
José Duarte <duarte.gmj@gmail.com>
2026-02-06 15:28:07 +0000

    Tighter index + if not exists

--- database/sql/V104__create_composite_index.sql
+++ database/sql/V104__create_composite_index.sql
@@ -1,2 +1,2 @@
 -- composite index to speed up query for user orders with quote
-CREATE INDEX CONCURRENTLY orders_owner_class_valid_composite ON orders (owner, class, true_valid_to DESC);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS orders_owner_class_valid_composite ON orders (owner, class, true_valid_to DESC) WHERE cancellation_timestamp IS NULL;
