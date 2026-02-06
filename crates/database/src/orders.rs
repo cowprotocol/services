f8087e5b4c1904059741d91a8c93e687ce4d446e
Martin Magnus <martin.beckmann@protonmail.com>
2026-02-06 12:18:55 +0100

    Filter refunded ethflow orders from all solvable orders query (#4129)
    # Description
    In a recent change the query to return ALL solvable orders suddenly
    returned ~45K orders more. This caused an increased memory usage on the
    start of the autopilot and made the DB query slower than it could be.
    
    # Changes
    Filter out refunded ethflow orders from solvable orders query.
    
    ## How to test
    confirmed with a manual query execution that the number of returned
    orders is in line with the final_auction_size + filtered_out_queries
    indicating that the all_solvable_orders query and the incremental logic
    afterwards agree with each other.
    
    increase in memory usage
    <img width="1474" height="376" alt="Screenshot 2026-02-06 at 10 45 44"
    src="https://github.com/user-attachments/assets/feb15290-d848-485e-8526-523d40605dcf"
    />
    
    first auction after restart is huge
    <img width="857" height="331" alt="Screenshot 2026-02-06 at 10 53 45"
    src="https://github.com/user-attachments/assets/e486f253-d9ce-44a1-b64b-8b03b31172e3"
    />

--- crates/database/src/orders.rs
+++ crates/database/src/orders.rs
@@ -729,6 +729,7 @@ pub fn solvable_orders(
             AND NOT EXISTS (SELECT 1 FROM invalidations i WHERE i.order_uid = o.uid)
             AND NOT EXISTS (SELECT 1 FROM onchain_order_invalidations oi WHERE oi.uid = o.uid)
             AND NOT EXISTS (SELECT 1 FROM onchain_placed_orders op WHERE op.uid = o.uid AND op.placement_error IS NOT NULL)
+            AND NOT EXISTS (SELECT 1 FROM ethflow_refunds r WHERE r.order_uid = o.uid)
     ),
     trades_agg AS (
         SELECT t.order_uid,
