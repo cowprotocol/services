6296b59b23a7c06611628173b52b43d078c99f60
MartinquaXD <martin.beckmann@protonmail.com>
2026-02-06 11:16:35 +0000

    Also add refund check to open user orders query

--- crates/database/src/orders.rs
+++ crates/database/src/orders.rs
@@ -967,6 +967,7 @@ pub async fn user_orders_with_quote(
             AND NOT EXISTS (SELECT 1 FROM invalidations i WHERE i.order_uid = o.uid)
             AND NOT EXISTS (SELECT 1 FROM onchain_order_invalidations oi WHERE oi.uid = o.uid)
             AND NOT EXISTS (SELECT 1 FROM onchain_placed_orders op WHERE op.uid = o.uid AND op.placement_error IS NOT NULL)
+            AND NOT EXISTS (SELECT 1 FROM ethflow_refunds r WHERE r.order_uid = o.uid)
             AND  o.owner = $2
             AND  o.class = 'limit'
     )
