f37a7fda971c402a1d6c5f3a1288e48bd910c988
José Duarte <duarte.gmj@gmail.com>
2026-02-10 15:10:46 +0000

    Bound cancellation batch size

--- crates/orderbook/src/api/cancel_orders.rs
+++ crates/orderbook/src/api/cancel_orders.rs
@@ -1,5 +1,6 @@
 use {
     crate::{api::AppState, orderbook::OrderCancellationError},
+    anyhow::anyhow,
     axum::{Json, extract::State},
     model::order::SignedOrderCancellations,
     std::sync::Arc,
@@ -9,6 +10,15 @@ pub async fn cancel_orders_handler(
     State(state): State<Arc<AppState>>,
     Json(cancellations): Json<SignedOrderCancellations>,
 ) -> Result<Json<&'static str>, OrderCancellationError> {
+    // Explicitly limit the number of orders cancelled in a batch as the request
+    // size limit *does not* provide a proper bound for this
+    if cancellations.data.order_uids.len() > 1024 {
+        return Err(OrderCancellationError::Other(anyhow!(
+            "too many orders ({} > 1024)",
+            cancellations.data.order_uids.len()
+        )));
+    }
+
     state
         .orderbook
         .cancel_orders(cancellations)
