6676e1800c95121ba181fad30ec418d31a6e47a7
Marcin Szymczak <mail@marszy.com>
2026-02-10 19:30:55 +0100

    doc-test fix

--- crates/driver/src/domain/competition/auction.rs
+++ crates/driver/src/domain/competition/auction.rs
@@ -180,7 +180,8 @@ impl Price {
     /// Converting 1 ETH expressed in `eth::TokenAmount` into `eth::Ether`
     ///
     /// ```
-    /// use driver::domain::{competition::auction::Price, eth};
+    /// use driver::domain::competition::auction::Price;
+    /// use shared::domain::eth;
     ///
     /// let amount = eth::TokenAmount::from(eth::U256::from(10).pow(eth::U256::from(18)));
     /// let price = Price::try_new(eth::Ether::from(
@@ -204,7 +205,8 @@ impl Price {
     ///
     /// # Examples
     /// ```
-    /// use driver::domain::{competition::auction::Price, eth};
+    /// use driver::domain::competition::auction::Price;
+    /// use shared::domain::eth;
     ///
     /// let amount = eth::Ether::from(eth::U256::from(10).pow(eth::U256::from(18)));
     /// let price = Price::try_new(eth::Ether::from(
