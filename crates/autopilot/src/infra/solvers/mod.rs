69061415d1d154ddfdae6dc14120fd362ad12bfb
Martin Magnus <martin.beckmann@protonmail.com>
2026-02-17 11:37:51 +0100

    autopilot: send `/solve` requests with ref-counted body (#4159)
    # Description
    Currently the way the autopilot serializes `/solve` requests is still
    sub optimal. While it does indeed only serialize the `/solve` requests
    once it still allocates memory for every HTTP request it ultimately
    sends.
    Since `reqwest` also supports `Bytes` as the request body which are
    already references counted we can save a bunch of allocating and copying
    data by serializing the auction data into a `Bytes` instance and cloning
    that into the individual `/solve` HTTP requests.
    
    # Changes
    - changes internal members of `solve::Request` from
    `serde_json::RawValue` to `Bytes`
    - adds a new `X-Auction-Id` header which is important for another
    optimization but that has to be merged before said optimization
    - moved original serialization from the executor thread to a background
    task that's intended for blocking operations since serialization takes
    quite a bit of time
    
    ## How to test
    existing e2e tests

--- crates/autopilot/src/infra/solvers/mod.rs
+++ crates/autopilot/src/infra/solvers/mod.rs
@@ -4,8 +4,8 @@ use {
     alloy::signers::{Signer, aws::AwsSigner},
     anyhow::{Context, Result, anyhow},
     observe::tracing::tracing_headers,
-    reqwest::{Client, StatusCode},
-    std::time::Duration,
+    reqwest::{Client, RequestBuilder, StatusCode},
+    std::{borrow::Cow, time::Duration},
     thiserror::Error,
     tracing::instrument,
     url::Url,
@@ -122,28 +122,22 @@ impl Driver {
     async fn request_response<Response, Request>(
         &self,
         path: &str,
-        request: Request,
+        payload: Request,
     ) -> Result<Response>
     where
         Response: serde::de::DeserializeOwned,
-        Request: serde::Serialize + Send + Sync + 'static,
+        Request: InjectIntoHttpRequest,
     {
         let url = util::join(&self.url, path);
+
         tracing::trace!(
-            path=&url.path(),
-            body=%serde_json::to_string_pretty(&request).unwrap(),
+            path = &url.path(),
+            body = %payload.body_to_string(),
             "solver request",
         );
-        let mut request = {
-            let builder = self.client.post(url.clone()).headers(tracing_headers());
-            // If the payload is very big then serializing it will block the
-            // executor a long time (mostly relevant for solve requests).
-            // That's why we always do it on a thread specifically for
-            // running blocking tasks.
-            tokio::task::spawn_blocking(move || builder.json(&request))
-                .await
-                .context("failed to build request")?
-        };
+
+        let request = self.client.post(url.clone()).headers(tracing_headers());
+        let mut request = payload.inject(request);
 
         if let Some(request_id) = observe::distributed_tracing::request_id::from_current_span() {
             request = request.header("X-REQUEST-ID", request_id);
@@ -180,4 +174,23 @@ pub async fn response_body_with_size_limit(
         bytes.extend_from_slice(slice);
     }
     Ok(bytes)
+}
+
+trait InjectIntoHttpRequest {
+    fn inject(&self, request: RequestBuilder) -> RequestBuilder;
+    fn body_to_string(&self) -> Cow<'_, str>;
+}
+
+impl<T> InjectIntoHttpRequest for T
+where
+    T: serde::ser::Serialize + Sized,
+{
+    fn inject(&self, request: RequestBuilder) -> RequestBuilder {
+        request.json(&self)
+    }
+
+    fn body_to_string(&self) -> Cow<'_, str> {
+        let serialized = serde_json::to_string(&self).expect("type should be JSON serializable");
+        Cow::Owned(serialized)
+    }
 }
