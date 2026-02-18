22e54cc4df156fe986144df21d716330a5179aff
José Duarte <duarte.gmj@gmail.com>
2026-02-18 10:49:27 +0000

    Add wrongfully removed commit

--- crates/orderbook/src/api/get_solver_competition.rs
+++ crates/orderbook/src/api/get_solver_competition.rs
@@ -16,6 +16,10 @@ pub async fn get_solver_competition_by_id_handler(
     State(state): State<Arc<AppState>>,
     Path(auction_id): Path<u64>,
 ) -> Response {
+    // We use u64 to ensure that negative numbers are returned as BAD_REQUEST
+    // however, there's a gap between u64::MAX and i64::MAX, numbers beyond i64::MAX
+    // will be marked as NOT_FOUND as they're positive (and as such, valid) but
+    // they are not covered by our system
     if auction_id >= AuctionId::MAX.cast_unsigned() {
         return crate::solver_competition::LoadSolverCompetitionError::NotFound.into_response();
     }
