f930e92d4f6964da431a5d3eea0e56570d871a09
MartinquaXD <martin.beckmann@protonmail.com>
2026-03-06 21:00:10 +0000

    cherry-picked changed from other branch

--- crates/autopilot/src/main.rs
+++ crates/autopilot/src/main.rs
@@ -1,11 +1,3 @@
-#[cfg(feature = "mimalloc-allocator")]
-#[global_allocator]
-static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
-
-#[cfg(not(feature = "mimalloc-allocator"))]
-#[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
-
 #[tokio::main]
 async fn main() {
     autopilot::start(std::env::args()).await;
