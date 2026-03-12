5dc4b2a8b508020e7b1cbd63c5474619bedfbe09
ilya <ilya@cow.fi>
2026-03-12 17:44:38 +0000

    [TRIVIAL] Update BitGet solver chain names (#4257)
    Some BitGet chain names were inconsistent and don't align with [the
    official docs](https://web3.bitget.com/en/docs/swap/). This PR fixes it.
    
    Tested on staging on each chain.

--- crates/solvers/src/infra/dex/bitget/dto.rs
+++ crates/solvers/src/infra/dex/bitget/dto.rs
@@ -13,13 +13,13 @@ use {
 pub enum ChainName {
     #[serde(rename = "eth")]
     Mainnet,
-    #[serde(rename = "bsc")]
+    #[serde(rename = "bnb")]
     Bnb,
     #[serde(rename = "base")]
     Base,
-    #[serde(rename = "polygon")]
+    #[serde(rename = "matic")]
     Polygon,
-    #[serde(rename = "arb")]
+    #[serde(rename = "arbitrum")]
     ArbitrumOne,
 }
 
