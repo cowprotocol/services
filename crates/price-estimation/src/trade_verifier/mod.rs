c0759c2e4e84782da6b705fdf63d3083350e229b
Marcin Szymczak <mail@marszy.com>
2026-03-19 17:10:08 +0100

    Revert "Add debug logs"
    This reverts commit f61af42f88744361246b974f657cc4d1d7c3f715.

--- crates/price-estimation/src/trade_verifier/mod.rs
+++ crates/price-estimation/src/trade_verifier/mod.rs
@@ -153,8 +153,6 @@ impl TradeVerifier {
             clearing_prices,
         };
 
-        tracing::debug!(?query, "SWAP SIMULATION QUERY");
-
         let mut swap = self
             .simulator
             .fake_swap(simulator_query)
@@ -205,12 +203,7 @@ impl TradeVerifier {
                 &self.simulator.domain_separator,
             )?);
         }
-
-        tracing::debug!(?swap, "SWAP SIMULATION");
-
         let output = self.simulator.simulate_swap(swap).await?;
-
-        tracing::debug!(?output, "SWAP SIMULATION OUTPUT");
 
         if let Some(tenderly) = &self.tenderly
             && let Err(err) = tenderly.log_simulation_command(output.tx, output.overrides, None)
--- crates/simulator/src/swap_simulator.rs
+++ crates/simulator/src/swap_simulator.rs
@@ -53,7 +53,6 @@ pub struct SwapSimulator {
     pub gas_limit: u64,
 }
 
-#[derive(Debug)]
 pub struct EncodedSwap {
     pub settlement: EncodedSettlement,
     pub overrides: StateOverride,
@@ -61,7 +60,7 @@ pub struct EncodedSwap {
     pub receiver: Address,
 }
 
-#[derive(Debug)]
+// Look into driver encoding logic for wrappers
 pub struct SwapSimulation {
     pub tx: TransactionRequest,
     pub overrides: StateOverride,
