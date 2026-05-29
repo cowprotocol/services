a0850dc71569246506ad385cdd60346bfa5823ad
José Duarte <15343819+jmg-duarte@users.noreply.github.com>
2026-05-29 11:26:45 +0100

    Apply suggestion from @jmg-duarte

--- crates/autopilot/src/run_loop.rs
+++ crates/autopilot/src/run_loop.rs
@@ -494,7 +494,7 @@ impl RunLoop {
                     .collect(),
                 // Always empty — kept to avoid breaking the solver competition
                 // API (`/api/v1/solver_competition`).
-                // NOTE: since the v1 is being removed,
+                // NOTE: since the v1 has been removed,
                 // we'll probably be able to remove this soon too
                 clearing_prices: Default::default(),
                 is_winner: bid.is_winner(),
