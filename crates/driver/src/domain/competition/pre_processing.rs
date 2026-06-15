639a114ccce1b12b1d5572272600914620b63b76
MartinquaXD <martin.beckmann@protonmail.com>
2026-06-15 06:53:43 +0000

    log only cow-amm order uids

--- crates/driver/src/domain/competition/pre_processing.rs
+++ crates/driver/src/domain/competition/pre_processing.rs
@@ -25,6 +25,7 @@ use {
         order::{OrderKind, SellTokenSource},
         signature::Signature,
     },
+    observe::tracing::lazy::Lazy,
     signature_validator::SignatureValidating,
     std::{
         collections::HashMap,
@@ -529,7 +530,9 @@ impl Utilities {
             .collect();
 
         if !orders.is_empty() {
-            tracing::debug!(?orders, "generated cow amm template orders");
+            tracing::trace!(?orders, "generated cow amm template orders");
+            let only_uids = Lazy(|| orders.iter().map(|o| format!("{:?}", o.uid)).collect_vec());
+            tracing::debug!(orders = ?only_uids, "generated cow amm template orders");
         }
 
         Arc::new(orders)
