1d815596335ed1865ef9569197efecffa2ea183d
José Duarte <15343819+jmg-duarte@users.noreply.github.com>
2026-07-21 20:21:42 +0100

    Downgrade more logs

--- crates/observe/src/tracing/distributed/axum.rs
+++ crates/observe/src/tracing/distributed/axum.rs
@@ -27,7 +27,7 @@ pub fn make_span<B>(request: &Request<B>) -> Span {
 
     let span = info_span!(SPAN_NAME, request_id = request_id, trace_id = field::Empty);
     if let Err(err) = span.set_parent(parent_context) {
-        tracing::debug!(?err, "failed to set request parent span");
+        tracing::trace!(?err, "failed to set request parent span");
     }
     {
         let _span = span.enter();
