d911493885e64a4ae2b8d0e10e65136ef1fea914
Tiago Guimarães <tiago@cow.fi>
2026-08-11 12:00:27 -0300

    fmt

--- crates/solana-driver/src/run.rs
+++ crates/solana-driver/src/run.rs
@@ -7,7 +7,11 @@ use {crate::infra::observe as infra_observe, clap::Parser};
 #[command(author, version, about)]
 pub struct Args {
     /// Log filter for the tracing framework.
-    #[arg(long, env = "RUST_LOG", default_value = "info,warn,solana_driver=debug")]
+    #[arg(
+        long,
+        env = "RUST_LOG",
+        default_value = "info,warn,solana_driver=debug"
+    )]
     log: String,
 }
 
