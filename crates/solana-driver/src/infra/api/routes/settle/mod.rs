aeab8f61b7dcf4c8f1bdb9ef4e8e820271d31568
Tiago Guimarães <tiago@cow.fi>
2026-08-13 11:15:20 -0300

    remove stub comment from endpoints

--- crates/solana-driver/src/infra/api/routes/settle/mod.rs
+++ crates/solana-driver/src/infra/api/routes/settle/mod.rs
@@ -1,6 +1,5 @@
 use crate::infra::api::State;
 
-/// Stub handler.
 pub async fn settle(_state: axum::extract::State<State>) -> axum::http::StatusCode {
     axum::http::StatusCode::NOT_IMPLEMENTED
 }
