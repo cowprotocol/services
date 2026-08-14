db510a4f347827d712c45512b26e5cf0972ede33
Tiago Guimarães <tiago@cow.fi>
2026-08-13 21:47:50 -0300

    chore(solana-driver): set the rpc commitment level to confirmed

--- crates/solana-driver/src/run.rs
+++ crates/solana-driver/src/run.rs
@@ -4,6 +4,7 @@ use {
     crate::infra::{Api, config, observe as infra_observe},
     clap::Parser,
     solana_client::nonblocking::rpc_client::RpcClient,
+    solana_commitment_config::CommitmentConfig,
     std::{path::PathBuf, sync::Arc, time::Duration},
 };
 
@@ -35,9 +36,10 @@ pub async fn run(args: Args) {
     }
 
     let shutdown_token = tokio_util::sync::CancellationToken::new();
-    let rpc = Arc::new(RpcClient::new_with_timeout(
+    let rpc = Arc::new(RpcClient::new_with_timeout_and_commitment(
         config.rpc.endpoint.to_string(),
         config.rpc.request_timeout,
+        CommitmentConfig::confirmed(),
     ));
     let api = Api {
         addr: config.http.bind_address,
