161b6fb9d2714e5b219eb1916efa543b2c6b22d3
José Duarte <15343819+jmg-duarte@users.noreply.github.com>
2026-08-31 14:42:29 +0100

    Remove fetch_latest_prices dependencies on auction_prices

--- crates/database/src/auction_prices.rs
+++ crates/database/src/auction_prices.rs
@@ -54,10 +54,14 @@ pub async fn fetch(
 #[instrument(skip_all)]
 pub async fn fetch_latest_prices(ex: &mut PgConnection) -> Result<Vec<AuctionPrice>, sqlx::Error> {
     const QUERY: &str = r#"
-SELECT * FROM auction_prices WHERE auction_id = (
-    SELECT MAX(auction_id)
-    FROM auction_prices
-)
+    SELECT
+        c.id,
+        unnest(c.price_tokens) AS price_token,
+        unnest(c.price_values) AS price_value
+    FROM competition_auctions c
+    WHERE c.id = (
+        SELECT MAX(id) FROM competition_auctions
+    )
     "#;
     sqlx::query_as(QUERY).fetch_all(ex).await
 }
